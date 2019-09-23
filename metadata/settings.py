387da2c4b8738ec07a03e9327643750edce69ba6
zxenia <ksenia1zaytseva@gmail.com>
2019-09-23 12:03:19 -0400

    postgres settings added requirements updated

--- metadata/settings.py
+++ metadata/settings.py
@@ -73,10 +73,22 @@ WSGI_APPLICATION = 'metadata.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'metadata',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
