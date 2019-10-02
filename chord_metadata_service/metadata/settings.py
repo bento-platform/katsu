c0adb269e576a7aaec28eef516680ebd8395742f
David Lougheed <david.lougheed@gmail.com>
2019-10-02 16:18:12 -0400

    Try to fix import issues with patients app

--- chord_metadata_service/metadata/settings.py
+++ chord_metadata_service/metadata/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'patients',
+
+    'chord_metadata_service.patients',
+
     'rest_framework',
 ]
 
