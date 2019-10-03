3ad1fa4419d2a54a3ee8a27471d15e727143ddf5
David Lougheed <david.lougheed@gmail.com>
2019-10-03 14:21:14 -0400

    Fix manage.py running dev server

--- manage.py
+++ manage.py
@@ -5,7 +5,8 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'metadata.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                          'chord_metadata_service.metadata.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
