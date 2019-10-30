268f6a48ad0c4b557403a0646e264ce0cbec4016
zxenia <ksenia1zaytseva@gmail.com>
2019-10-30 16:30:47 -0400

    add Biosample and Phenopacket as read_only to Individual

--- chord_metadata_service/patients/serializers.py
+++ chord_metadata_service/patients/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from .models import *
 from chord_metadata_service.restapi.schemas import ONTOLOGY_CLASS
 from chord_metadata_service.restapi.validators import JsonSchemaValidator
+from chord_metadata_service.phenopackets.serializers import (
+	BiosampleSerializer, PhenopacketSerializer)
 
 
 class IndividualSerializer(serializers.ModelSerializer):
@@ -9,6 +11,8 @@ class IndividualSerializer(serializers.ModelSerializer):
 		validators=[JsonSchemaValidator(schema=ONTOLOGY_CLASS)],
 		allow_null=True
 		)
+	biosamples = BiosampleSerializer(read_only=True, many=True)
+	phenopackets = PhenopacketSerializer(read_only=True, many=True)
 
 	class Meta:
 		model = Individual
