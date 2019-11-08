f15bf027abf18bb2f18ca7eb1aa559a09817762c
zxenia <ksenia1zaytseva@gmail.com>
2019-11-08 17:54:22 -0500

    add more mappings to PF

--- chord_metadata_service/restapi/utils.py
+++ chord_metadata_service/restapi/utils.py
@@ -48,11 +48,11 @@ def convert_to_fhir(individual_data):
 				biosample_record['phenotypicFeatures'] = []
 				for feature in sample.get('phenotypic_features'):
 					feature_record = {}
-					feature_record['resourceType'] = 'Condition'
+					feature_record['resourceType'] = 'Observation'
 					if 'id' in feature.keys():
 						feature_record['identifier'] = feature.get('id', None)
 					if 'description' in feature.keys():
-						feature_record['text'] = feature.get('description', None)
+						feature_record['note'] = feature.get('description', None)
 					if 'type' in feature.keys():
 						feature_record['code'] = {}
 						feature_record['code']['coding'] = []
@@ -60,6 +60,44 @@ def convert_to_fhir(individual_data):
 						ftype['code'] = feature.get('type').get('id', None)
 						ftype['display'] = feature.get('type').get('label', None)
 						feature_record['code']['coding'].append(ftype)
+					if 'onset' in feature.keys():
+						feature_record['onsetAge'] = {}
+						feature_record['onsetAge']['value'] = feature.get('onset').get('label', None)
+						feature_record['onsetAge']['code'] = feature.get('onset').get('id', None)
+					if 'modifier' in feature.keys():
+						feature_record['modifier'] = {}
+						# TODO store all profile references in separate dict, should be treated as a context
+						feature_record['modifier']['url'] = "http://ga4gh.org/fhir/phenopackets/StructureDefinition/phenotypic-feature-modifier"
+						feature_record['modifier']['coding'] = []
+						for item in feature.get('modifier'):
+							mod = {}
+							mod['system'] = ""
+							mod['code'] = item.get('id')
+							mod['display'] = item.get('label')
+							feature_record['modifier']['coding'].append(mod)
+					if 'evidence' in feature.keys():
+						evidence = feature.get('evidence')
+						feature_record['evidence'] = []
+						evidence_code = {}
+						evidence_code['code'] = []
+						code = {}
+						code['coding'] = []
+						coding = {}
+						coding['system'] = ""
+						coding['code'] = evidence.get('evidence_code').get('id')
+						coding['display'] = evidence.get('evidence_code').get('label')
+						code['coding'].append(coding)
+						evidence_code['code'].append(code)
+						feature_record['evidence'].append(evidence_code)
+						if 'reference' in evidence.keys():
+							# feature_record['evidence_detail'] = []
+							evidence_detail = {}
+							evidence_detail['detail'] = []
+							detail = {}
+							detail['reference'] = evidence.get('reference').get('id')
+							detail['display'] = evidence.get('reference').get('description', None)
+							evidence_detail['detail'].append(detail)
+							feature_record['evidence'].append(evidence_detail)
 					biosample_record['phenotypicFeatures'].append(feature_record)
 			# mapping for procedure relared to each biosample
 			if 'procedure' in sample.keys():
