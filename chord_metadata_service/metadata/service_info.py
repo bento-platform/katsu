1f01dcf9f58343d308942027a87e948bf429afc9
David Lougheed <david.lougheed@gmail.com>
2019-11-13 12:25:01 -0500

    Update service-info

--- chord_metadata_service/metadata/service_info.py
+++ chord_metadata_service/metadata/service_info.py
@@ -3,13 +3,13 @@ from .. import __version__
 # Service info according to spec https://github.com/ga4gh-discovery/ga4gh-service-info
 
 SERVICE_INFO = {
-	"id": "ca.distributedgenomics.chord_metadata_service",
-	"name": "Metadata Service",
-	"type": "data service",  # TODO
+	"id": "ca.c3g.chord:metadata",  # TODO: Globally unique
+	"name": "Metadata Service",  # TODO: Globally unique?
+	"type": "ca.c3g.chord:metadata:{}".format(__version__),
 	"description": "Metadata service implementation based on Phenopackets schema",
 	"organization": {
 		"name": "C3G",
-		"url": "http://www.computationalgenomics.ca/"
+		"url": "http://www.computationalgenomics.ca"
 	},
 	"contactUrl": "mailto:ksenia.zaytseva@mcgill.ca",
 	"version": __version__
