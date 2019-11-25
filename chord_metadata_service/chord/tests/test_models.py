2aa317e46e1f5382a300361561203b3b4a847484
David Lougheed <david.lougheed@gmail.com>
2019-11-25 11:48:08 -0500

    Add table ownership model test

--- chord_metadata_service/chord/tests/test_models.py
+++ chord_metadata_service/chord/tests/test_models.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from uuid import uuid4
 from ..models import *
 from .constants import VALID_DATA_USE_1
 
@@ -18,6 +19,8 @@ class ProjectTest(TestCase):
         self.assertEqual(p1.description, "")
         self.assertEqual(p2.description, P2_DESC)
 
+        self.assertEqual(str(p1), f"Project 1 (ID: {str(p1.project_id)})")
+
         self.assertDictEqual(p1.data_use, VALID_DATA_USE_1)
         self.assertDictEqual(p2.data_use, VALID_DATA_USE_1)
 
@@ -34,4 +37,36 @@ class DatasetTest(TestCase):
         self.assertEqual(d.description, "Some dataset")
         self.assertEqual(d.project, p)
 
+        self.assertEqual(str(d), f"Dataset 1 (ID: {str(d.dataset_id)})")
+
         self.assertIn(d.dataset_id, set(d2.dataset_id for d2 in p.datasets.all()))
+
+
+TABLE_ID = str(uuid4())
+SERVICE_ID = str(uuid4())
+
+
+class TableOwnershipTest(TestCase):
+    def setUp(self) -> None:
+        p = Project.objects.create(name="Project 1", description="", data_use=VALID_DATA_USE_1)
+        d = Dataset.objects.create(name="Dataset 1", description="", project=p)
+        TableOwnership.objects.create(
+            table_id=TABLE_ID,
+            service_id=SERVICE_ID,
+            service_artifact="variant",
+            data_type="variant",
+
+            dataset=d
+        )
+
+    def test_table_ownership(self):
+        d = Dataset.objects.get(name="Dataset 1")
+        t = TableOwnership.objects.get(table_id=TABLE_ID, service_id=SERVICE_ID)
+
+        self.assertEqual(t.service_artifact, "variant")
+        self.assertEqual(t.data_type, "variant")
+        self.assertEqual(t.dataset, d)
+
+        self.assertIn(t, d.tableownership_set.all())
+
+        self.assertEqual(str(t), f"{str(d)} -> {t.table_id}")
