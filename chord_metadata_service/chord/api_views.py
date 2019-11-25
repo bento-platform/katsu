2e80c48feba2f9250826eef04a31034179991b98
David Lougheed <david.lougheed@gmail.com>
2019-11-25 14:24:43 -0500

    Remove unused imports

--- chord_metadata_service/chord/api_views.py
+++ chord_metadata_service/chord/api_views.py
@@ -1,6 +1,4 @@
 from rest_framework import viewsets
-from rest_framework.decorators import action
-from rest_framework.response import Response
 
 from chord_metadata_service.phenopackets.api_views import LargeResultsSetPagination
 from .models import *
