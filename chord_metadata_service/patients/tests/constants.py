c8862967f772f1746b45206e095b2752ec09dd38
zxenia <ksenia1zaytseva@gmail.com>
2019-12-16 17:27:50 -0500

    fix tests for patients api

---
+++ chord_metadata_service/patients/tests/constants.py
@@ -0,0 +1,40 @@
+VALID_INDIVIDUAL = {
+    "id": "patient:1",
+    "taxonomy": {
+        "id": "NCBITaxon:9606",
+        "label": "human"
+    },
+    "date_of_birth": "1960-01-01",
+    "age": {
+        "age": {
+            "start": {
+                "age": "P45Y"
+            },
+            "end": {
+                "age": "P49Y"
+            }
+        }
+    },
+    "sex": "FEMALE",
+    "active": True
+}
+
+INVALID_INDIVIDUAL = {
+    "id": "patient:1",
+    "taxonomy": {
+        "id": "NCBITaxon:9606"
+    },
+    "date_of_birth": "1960-01-01",
+    "age": {
+        "age": {
+            "start": {
+                "age": "P45Y"
+            },
+            "end": {
+                "age": "P49Y"
+            }
+        }
+    },
+    "sex": "FEM",
+    "active": True
+}
