73dd7023e3f7d87a00ab20dc444219cee76add8f
zxenia <ksenia1zaytseva@gmail.com>
2020-01-08 11:56:20 -0500

    split Phenopackets structural definition fixed values in a sep dict

--- chord_metadata_service/restapi/ga4gh_fhir_profiles.py
+++ chord_metadata_service/restapi/ga4gh_fhir_profiles.py
@@ -1,5 +1,55 @@
-# Allf fixed values provided by Pheno_FHIR Mapping guide
+# All fixed values provided by Pheno_FHIR Mapping guide
 # TODO split by some logic
+PHENOPACKETS_ON_FHIR_MAPPING = {
+    "individual": {
+        "title": "Individual",
+        "url": "http://ga4gh.org/fhir/phenopackets/StructureDefinition/Individual",
+        "age": "http://ga4gh.org/fhir/phenopackets/StructureDefinition/individual-age",
+        "karyotypic_sex": {
+            "url": "http://ga4gh.org/fhir/phenopackets/StructureDefinition/individual-karyotypic-sex",
+            "system": "http://ga4gh.org/fhir/phenopackets/CodeSystem/karyotypic-sex"
+        },
+        "taxonomy": "http://ga4gh.org/fhir/phenopackets/StructureDefinition/individual-taxonomy"
+    },
+    "phenopacket": {
+        "title": "Phenopacket",
+        "code": {
+            "system": "http://ga4gh.org/fhir/phenopackets/CodeSystem/document-type",
+            "code": "phenopacket"
+        },
+        "url": "http://ga4gh.org/fhir/phenopackets/StructureDefinition/Phenopacket"
+    },
+    "biosample": {
+        "title": "Biosample",
+        "url": "http://ga4gh.org/fhir/phenopackets/StructureDefinition/Biosample",
+        "individual_age_at_collection": "http://ga4gh.org/fhir/phenopackets/StructureDefinition/biosample-individual-age-at-collection",
+        "histological_diagnosis": "http://ga4gh.org/fhir/phenopackets/StructureDefinition/biosample-histological-diagnosis",
+        "tumor_progression": "http://ga4gh.org/fhir/phenopackets/StructureDefinition/biosample-tumor-progression",
+        "tumor_grade": "http://ga4gh.org/fhir/phenopackets/StructureDefinition/biosample-tumor-grade",
+        "diagnostic_markers": "http://ga4gh.org/fhir/phenopackets/StructureDefinition/biosample-diagnostic-markers",
+        "is_control_sample": "http://ga4gh.org/fhir/phenopackets/StructureDefinition/biosample-control"
+    },
+    "phenotypic_feature": {
+        "title": "Phenotypic Feature",
+        "url": "http://ga4gh.org/fhir/phenopackets/StructureDefinition/PhenotypicFeature",
+        "severity": "http://ga4gh.org/fhir/phenopackets/StructureDefinition/phenotypic-feature-severity",
+        "modifier": "http://ga4gh.org/fhir/phenopackets/StructureDefinition/phenotypic-feature-modifier",
+        "onset": "http://ga4gh.org/fhir/phenopackets/StructureDefinition/phenotypic-feature-onset",
+        "evidence": {
+            "url": "http://ga4gh.org/fhir/phenopackets/StructureDefinition/evidence",
+            # fixed value
+            "evidence_code": "evidenceCode"
+
+        }
+
+
+    }
+    
+
+
+}
+
+
 GA4GH_FHIR_PROFILES = {
     "individual-age": "http://ga4gh.org/fhir/phenopackets/StructureDefinition/individual-age",
     "individual-karyotypic-sex": {
