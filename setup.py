9347d622de8a44aece045a5d8d9bd2b80ce4b4fd
David Lougheed <david.lougheed@gmail.com>
2020-01-13 10:15:39 -0500

    Fix license classifier in setup.py

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setuptools.setup(
     license="LGPLv3",
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
         "Operating System :: OS Independent"
     ]
 )
