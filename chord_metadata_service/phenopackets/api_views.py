189678a281f33db6a2b29a820deb87ea60cda3e8
Simon Chénard <chenard.simon@gmail.com>
2020-01-27 17:02:42 -0500

    translated every tab to 4 spaces

--- chord_metadata_service/phenopackets/api_views.py
+++ chord_metadata_service/phenopackets/api_views.py
@@ -6,183 +6,183 @@ from chord_metadata_service.restapi.api_renderers import *
 
 
 class LargeResultsSetPagination(pagination.PageNumberPagination):
-	page_size = 25
-	page_size_query_param = 'page_size'
-	max_page_size = 10000
+    page_size = 25
+    page_size_query_param = 'page_size'
+    max_page_size = 10000
 
 
 class PhenopacketsModelViewSet(viewsets.ModelViewSet):
-	renderer_classes = tuple(api_settings.DEFAULT_RENDERER_CLASSES) + (PhenopacketsRenderer,)
-	pagination_class = LargeResultsSetPagination
+    renderer_classes = tuple(api_settings.DEFAULT_RENDERER_CLASSES) + (PhenopacketsRenderer,)
+    pagination_class = LargeResultsSetPagination
 
 
 class ExtendedPhenopacketsModelViewSet(PhenopacketsModelViewSet):
-	renderer_classes = tuple(PhenopacketsModelViewSet.renderer_classes) + (FHIRRenderer,)
+    renderer_classes = tuple(PhenopacketsModelViewSet.renderer_classes) + (FHIRRenderer,)
 
 
 class PhenotypicFeatureViewSet(ExtendedPhenopacketsModelViewSet):
-	"""
-	get:
-	Return a list of all existing phenotypic features
+    """
+    get:
+    Return a list of all existing phenotypic features
 
-	post:
-	Create a new phenotypic feature
+    post:
+    Create a new phenotypic feature
 
-	"""
-	queryset = PhenotypicFeature.objects.all().order_by("id")
-	serializer_class = PhenotypicFeatureSerializer
+    """
+    queryset = PhenotypicFeature.objects.all().order_by("id")
+    serializer_class = PhenotypicFeatureSerializer
 
 
 class ProcedureViewSet(ExtendedPhenopacketsModelViewSet):
-	"""
-	get:
-	Return a list of all existing procedures
+    """
+    get:
+    Return a list of all existing procedures
 
-	post:
-	Create a new procedure
+    post:
+    Create a new procedure
 
-	"""
-	queryset = Procedure.objects.all().order_by("id")
-	serializer_class = ProcedureSerializer
+    """
+    queryset = Procedure.objects.all().order_by("id")
+    serializer_class = ProcedureSerializer
 
 
 class HtsFileViewSet(ExtendedPhenopacketsModelViewSet):
-	"""
-	get:
-	Return a list of all existing HTS files
+    """
+    get:
+    Return a list of all existing HTS files
 
-	post:
-	Create a new HTS file
+    post:
+    Create a new HTS file
 
-	"""
-	queryset = HtsFile.objects.all().order_by("uri")
-	serializer_class = HtsFileSerializer
+    """
+    queryset = HtsFile.objects.all().order_by("uri")
+    serializer_class = HtsFileSerializer
 
 
 class GeneViewSet(ExtendedPhenopacketsModelViewSet):
-	"""
-	get:
-	Return a list of all existing genes
+    """
+    get:
+    Return a list of all existing genes
 
-	post:
-	Create a new gene
+    post:
+    Create a new gene
 
-	"""
-	queryset = Gene.objects.all().order_by("id")
-	serializer_class = GeneSerializer
+    """
+    queryset = Gene.objects.all().order_by("id")
+    serializer_class = GeneSerializer
 
 
 class VariantViewSet(ExtendedPhenopacketsModelViewSet):
-	"""
-	get:
-	Return a list of all existing variants
+    """
+    get:
+    Return a list of all existing variants
 
-	post:
-	Create a new variant
+    post:
+    Create a new variant
 
-	"""
-	queryset = Variant.objects.all().order_by("id")
-	serializer_class = VariantSerializer
+    """
+    queryset = Variant.objects.all().order_by("id")
+    serializer_class = VariantSerializer
 
 
 class DiseaseViewSet(ExtendedPhenopacketsModelViewSet):
-	"""
-	get:
-	Return a list of all existing diseases
+    """
+    get:
+    Return a list of all existing diseases
 
-	post:
-	Create a new disease
+    post:
+    Create a new disease
 
-	"""
-	queryset = Disease.objects.all().order_by("id")
-	serializer_class = DiseaseSerializer
+    """
+    queryset = Disease.objects.all().order_by("id")
+    serializer_class = DiseaseSerializer
 
 
 class ResourceViewSet(PhenopacketsModelViewSet):
-	"""
-	get:
-	Return a list of all existing resources
+    """
+    get:
+    Return a list of all existing resources
 
-	post:
-	Create a new resource
+    post:
+    Create a new resource
 
-	"""
-	queryset = Resource.objects.all().order_by("id")
-	serializer_class = ResourceSerializer
+    """
+    queryset = Resource.objects.all().order_by("id")
+    serializer_class = ResourceSerializer
 
 
 class MetaDataViewSet(PhenopacketsModelViewSet):
-	"""
-	get:
-	Return a list of all existing metadata records
+    """
+    get:
+    Return a list of all existing metadata records
 
-	post:
-	Create a new metadata record
+    post:
+    Create a new metadata record
 
-	"""
-	queryset = MetaData.objects.all().order_by("id")
-	serializer_class = MetaDataSerializer
+    """
+    queryset = MetaData.objects.all().order_by("id")
+    serializer_class = MetaDataSerializer
 
 
 class BiosampleViewSet(ExtendedPhenopacketsModelViewSet):
-	"""
-	get:
-	Return a list of all existing biosamples
+    """
+    get:
+    Return a list of all existing biosamples
 
-	post:
-	Create a new biosample
-	"""
-	queryset = Biosample.objects.all().order_by("id")
-	serializer_class = BiosampleSerializer
+    post:
+    Create a new biosample
+    """
+    queryset = Biosample.objects.all().order_by("id")
+    serializer_class = BiosampleSerializer
 
 
 class PhenopacketViewSet(ExtendedPhenopacketsModelViewSet):
-	"""
-	get:
-	Return a list of all existing phenopackets
+    """
+    get:
+    Return a list of all existing phenopackets
 
-	post:
-	Create a new phenopacket
+    post:
+    Create a new phenopacket
 
-	"""
-	queryset = Phenopacket.objects.all().order_by("id")
-	serializer_class = PhenopacketSerializer
+    """
+    queryset = Phenopacket.objects.all().order_by("id")
+    serializer_class = PhenopacketSerializer
 
 
 class GenomicInterpretationViewSet(PhenopacketsModelViewSet):
-	"""
-	get:
-	Return a list of all existing genomic interpretations
+    """
+    get:
+    Return a list of all existing genomic interpretations
 
-	post:
-	Create a new genomic interpretation
+    post:
+    Create a new genomic interpretation
 
-	"""
-	queryset = GenomicInterpretation.objects.all().order_by("id")
-	serializer_class = GenomicInterpretationSerializer
+    """
+    queryset = GenomicInterpretation.objects.all().order_by("id")
+    serializer_class = GenomicInterpretationSerializer
 
 
 class DiagnosisViewSet(PhenopacketsModelViewSet):
-	"""
-	get:
-	Return a list of all existing diagnoses
+    """
+    get:
+    Return a list of all existing diagnoses
 
-	post:
-	Create a new diagnosis
+    post:
+    Create a new diagnosis
 
-	"""
-	queryset = Diagnosis.objects.all().order_by("id")
-	serializer_class = DiagnosisSerializer
+    """
+    queryset = Diagnosis.objects.all().order_by("id")
+    serializer_class = DiagnosisSerializer
 
 
 class InterpretationViewSet(PhenopacketsModelViewSet):
-	"""
-	get:
-	Return a list of all existing interpretations
+    """
+    get:
+    Return a list of all existing interpretations
 
-	post:
-	Create a new interpretation
+    post:
+    Create a new interpretation
 
-	"""
-	queryset = Interpretation.objects.all().order_by("id")
-	serializer_class = InterpretationSerializer
+    """
+    queryset = Interpretation.objects.all().order_by("id")
+    serializer_class = InterpretationSerializer
--- chord_metadata_service/restapi/api_renderers.py
+++ chord_metadata_service/restapi/api_renderers.py
@@ -3,32 +3,32 @@ from djangorestframework_camel_case.render import CamelCaseJSONRenderer
 
 
 class FHIRRenderer(JSONRenderer):
-	media_type = 'application/json'
-	format = 'fhir'
+    media_type = 'application/json'
+    format = 'fhir'
 
-	def render(self, data, media_type=None, renderer_context=None):
-		fhir_datatype_plural = getattr(
-				renderer_context.get('view').get_serializer().Meta,
-				'fhir_datatype_plural', 'objects'
-				)
-		class_converter = getattr(
-				renderer_context.get('view').get_serializer().Meta,
-				'class_converter', 'objects'
-				)
-		if 'results' in data:
-			final_data = {}
-			final_data[fhir_datatype_plural] = []
-			for item in data.get('results'):
-				item_data = class_converter(item)
-				final_data[fhir_datatype_plural].append(item_data)
-		else:
-			final_data = class_converter(data)
-		return super(FHIRRenderer, self).render(final_data, media_type, renderer_context)
+    def render(self, data, media_type=None, renderer_context=None):
+        fhir_datatype_plural = getattr(
+                renderer_context.get('view').get_serializer().Meta,
+                'fhir_datatype_plural', 'objects'
+                )
+        class_converter = getattr(
+                renderer_context.get('view').get_serializer().Meta,
+                'class_converter', 'objects'
+                )
+        if 'results' in data:
+            final_data = {}
+            final_data[fhir_datatype_plural] = []
+            for item in data.get('results'):
+                item_data = class_converter(item)
+                final_data[fhir_datatype_plural].append(item_data)
+        else:
+            final_data = class_converter(data)
+        return super(FHIRRenderer, self).render(final_data, media_type, renderer_context)
 
 
 class PhenopacketsRenderer(CamelCaseJSONRenderer):
-	media_type = 'application/json'
-	format = 'phenopackets'
+    media_type = 'application/json'
+    format = 'phenopackets'
 
-	def render(self, data, media_type=None, renderer_context=None):
-		return super(PhenopacketsRenderer, self).render(data, media_type, renderer_context)
+    def render(self, data, media_type=None, renderer_context=None):
+        return super(PhenopacketsRenderer, self).render(data, media_type, renderer_context)
