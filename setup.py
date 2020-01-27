818b851de945ad7393b7408679525cf7b58b09f7
David Lougheed <david.lougheed@gmail.com>
2020-01-27 16:04:06 -0500

    Bump version number

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as rf:
 
 setuptools.setup(
     name="chord_metadata_service",
-    version="0.1.0",
+    version="0.2.0",
 
     python_requires=">=3.6",
     install_requires=[
