990813499c2310dd8ab9d8b62c6f89694a11d466
David Lougheed <david.lougheed@gmail.com>
2020-01-28 14:23:36 -0500

    Use prefetch tuples in individual viewset

--- chord_metadata_service/patients/api_views.py
+++ chord_metadata_service/patients/api_views.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets, pagination
 from .serializers import *
 from .models import *
 from rest_framework.settings import api_settings
+from chord_metadata_service.phenopackets.api_views import BIOSAMPLE_PREFETCH, PHENOPACKET_PREFETCH
 from chord_metadata_service.restapi.api_renderers import (
     FHIRRenderer, PhenopacketsRenderer)
 
@@ -22,17 +23,9 @@ class IndividualViewSet(viewsets.ModelViewSet):
 
     """
     queryset = Individual.objects.all().prefetch_related(
-        "biosamples",
-        "phenopackets__meta_data",
-        "phenopackets__biosamples__phenotypic_features",
-        "phenopackets__genes",
-        "phenopackets__variants",
-        "phenopackets__diseases",
-        "phenopackets__hts_files",
-        "phenopackets__phenotypic_features",
+        *(f"biosamples__{p}" for p in BIOSAMPLE_PREFETCH),
+        *(f"phenopackets__{p}" for p in PHENOPACKET_PREFETCH if p != "subject"),
     ).order_by("id")
     serializer_class = IndividualSerializer
     pagination_class = LargeResultsSetPagination
-    renderer_classes = tuple(
-        api_settings.DEFAULT_RENDERER_CLASSES
-        ) + (FHIRRenderer, PhenopacketsRenderer)
+    renderer_classes = tuple(api_settings.DEFAULT_RENDERER_CLASSES) + (FHIRRenderer, PhenopacketsRenderer)
