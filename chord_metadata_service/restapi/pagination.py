8c801159e7778ee13163d0021a013ef3962ad10d
David Lougheed <david.lougheed@gmail.com>
2020-02-20 14:22:46 -0500

    Fix errors with pagination, make code shorter

--- chord_metadata_service/restapi/pagination.py
+++ chord_metadata_service/restapi/pagination.py
@@ -14,7 +14,7 @@ class LargeResultsSetPagination(pagination.PageNumberPagination):
     page_size_query_param = 'page_size'
     max_page_size = 10000
 
-    # Fix next/previous links inside sub-path-mounted reverse proxies in the CHORD context
+    # Fix next/previous links inside sub-path-mounted reverse proxies in the CHORD context:
 
     def _get_chord_absolute_uri(self):
         full_path = self.request.get_full_path()
@@ -24,25 +24,19 @@ class LargeResultsSetPagination(pagination.PageNumberPagination):
         return urljoin(settings.CHORD_URL, full_path)
 
     def get_next_link(self):
-        if not self.page.has_next():
-            return None
-
         if settings.CHORD_URL is not None:
-            url = self._get_chord_absolute_uri()
-            page_number = self.page.next_page_number()
-            return replace_query_param(url, self.page_query_param, page_number)
-
+            # Monkey-patch rewrite build_absolute_uri
+            self.request.build_absolute_uri = self._get_chord_absolute_uri
         return super(LargeResultsSetPagination, self).get_next_link()
 
     def get_previous_link(self):
-        if not self.page.has_previous():
-            return None
-
         if settings.CHORD_URL is not None:
-            url = self._get_chord_absolute_uri()
-            page_number = self.page.next_page_number()
-            if page_number == 1:
-                return remove_query_param(url, self.page_query_param)
-            return replace_query_param(url, self.page_query_param, page_number)
-
+            # Monkey-patch rewrite build_absolute_uri
+            self.request.build_absolute_uri = self._get_chord_absolute_uri
         return super(LargeResultsSetPagination, self).get_previous_link()
+
+    def get_html_context(self):
+        if settings.CHORD_URL is not None:
+            # Monkey-patch rewrite build_absolute_uri
+            self.request.build_absolute_uri = self._get_chord_absolute_uri
+        super(LargeResultsSetPagination, self).get_html_context()
