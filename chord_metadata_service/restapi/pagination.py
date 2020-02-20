fff4e935fc1f35278835dbc553dfdd43b788cdbd
David Lougheed <david.lougheed@gmail.com>
2020-02-20 14:23:27 -0500

    Remove unused imports

--- chord_metadata_service/restapi/pagination.py
+++ chord_metadata_service/restapi/pagination.py
@@ -1,6 +1,5 @@
 from django.conf import settings
 from rest_framework import pagination
-from rest_framework.utils.urls import remove_query_param, replace_query_param
 from urllib.parse import urljoin
 
 
