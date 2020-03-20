84132c4a7a0a3b456fbe8065ad519ca93d2cfa08
Rom Grk <romgrk.cc@gmail.com>
2020-03-20 14:24:10 -0400

    experiments: remove unused files

--- chord_metadata_service/experiments/models.py
+++ chord_metadata_service/experiments/models.py
@@ -52,31 +52,5 @@ class Experiment(models.Model, IndexableMixin):
     library_strategy = models.CharField(choices=LIBRARY_STRATEGY, max_length=25, null=False, blank=False,
             help_text="(Controlled Vocabulary) The assay used. These are defined within the SRA metadata specifications with a controlled vocabulary (e.g. ‘Bisulfite-Seq’, ‘RNA-Seq’, ‘ChIP-Seq’). For a complete list, see https://www.ebi.ac.uk/ena/submit/reads-library-strategy.")
 
-
-
-
-    # TODO check for CURIE
-    alternate_ids = ArrayField(models.CharField(max_length=200), blank=True, null=True,
-                               help_text='A list of alternative identifiers for the individual.')
-    date_of_birth = models.DateField(null=True, blank=True, help_text='A timestamp either exact or imprecise.')
-    # An ISO8601 string represent age
-    age = JSONField(blank=True, null=True, help_text='The age or age range of the individual.')
-    sex = models.CharField(choices=SEX, max_length=200,  blank=True, null=True,
-                           help_text='Observed apparent sex of the individual.')
-    karyotypic_sex = models.CharField(choices=KARYOTYPIC_SEX, max_length=200, default='UNKNOWN_KARYOTYPE',
-                                      help_text='The karyotypic sex of the individual.')
-    taxonomy = JSONField(blank=True, null=True,
-                         help_text='Ontology resource representing the species (e.g., NCBITaxon:9615).')
-    # FHIR specific
-    active = models.BooleanField(default=False, help_text='Whether this patient\'s record is in active use.')
-    deceased = models.BooleanField(default=False, help_text='Indicates if the individual is deceased or not.')
-    # mCode specific
-    race = models.CharField(max_length=200, blank=True, help_text='A code for the person\'s race.')
-    ethnicity = models.CharField(max_length=200, blank=True, help_text='A code for the person\'s ethnicity.')
-    extra_properties = JSONField(blank=True, null=True,
-                                 help_text='Extra properties that are not supported by current schema')
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
-
     def __str__(self):
         return str(self.id)
