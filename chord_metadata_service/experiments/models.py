bcd2f7388ce4a650b8b8764d5e7a6aebef759cc0
Rom Grk <romgrk.cc@gmail.com>
2020-03-24 16:52:36 -0400

    experiments: update model

--- chord_metadata_service/experiments/models.py
+++ chord_metadata_service/experiments/models.py
@@ -1,8 +1,16 @@
 from django.db import models
+from django.db.models import CharField
 from django.core.validators import RegexValidator
+from django.core.exceptions import ValidationError
 from django.contrib.postgres.fields import JSONField, ArrayField
 from chord_metadata_service.restapi.models import IndexableMixin
+from chord_metadata_service.restapi.description_utils import rec_help
+from chord_metadata_service.restapi.validators import JsonSchemaValidator
+from chord_metadata_service.restapi.schemas import ONTOLOGY_CLASS_LIST, KEY_VALUE_OBJECT
+import chord_metadata_service.experiments.descriptions as d
 
+ontologyListValidator = JsonSchemaValidator(ONTOLOGY_CLASS_LIST)
+keyValueValidator     = JsonSchemaValidator(KEY_VALUE_OBJECT)
 
 class Experiment(models.Model, IndexableMixin):
     """ Class to store Experiment information """
@@ -31,32 +39,18 @@ class Experiment(models.Model, IndexableMixin):
         ('other', 'other'),
     )
 
-    # FIXME(validate qc_flags.max_length)
-    # FIXME(validate experiment_ontology_curie.max_length)
-    # FIXME(validate molecule_ontology_curie.max_length)
-    # FIXME(validation for minSize/maxSize for ArrayField)
+    id = CharField(primary_key=True, max_length=200, help_text=rec_help(d.EXPERIMENT, 'id'))
 
-    id = models.CharField(primary_key=True, max_length=200, help_text='An arbitrary identifier for the individual.')
+    reference_registry_id = CharField(max_length=30, null=True, blank=True, help_text=rec_help(d.EXPERIMENT, 'reference_registry_id'))
+    qc_flags = ArrayField(CharField(max_length=100, help_text=rec_help(d.EXPERIMENT, 'qc_flags')), null=True, default=list)
+    experiment_type = CharField(max_length=30, null=True, blank=True, help_text=rec_help(d.EXPERIMENT, 'experiment_type'))
+    experiment_ontology = JSONField(null=True, blank=True, validators=[ontologyListValidator], help_text=rec_help(d.EXPERIMENT, 'experiment_ontology'))
+    molecule_ontology = JSONField(null=True, blank=True, validators=[ontologyListValidator], help_text=rec_help(d.EXPERIMENT, 'molecule_ontology'))
+    molecule = CharField(choices=MOLECULE, max_length=20, null=True, blank=True, help_text=rec_help(d.EXPERIMENT, 'molecule'))
 
-    reference_registry_id = models.CharField(max_length=30, null=True, blank=True,
-            help_text='The IHEC EpiRR ID for this dataset, only for IHEC Reference Epigenome datasets. Otherwise leave empty.')
-    qc_flags = ArrayField(models.CharField(max_length=30, null=True, blank=True,
-            help_text='Any quanlity control observations can be noted here. This field can be omitted if empty'))
-    experiment_type = models.CharField(max_length=30, null=True, blank=True,
-            help_text="(Controlled Vocabulary) The assay target (e.g. ‘DNA Methylation’, ‘mRNA-Seq’, ‘smRNA-Seq’, 'Histone H3K4me1').")
-    experiment_ontology_curie = ArrayField(models.CharField(max_length=30, null=True, blank=True,
-            validators=[RegexValidator(regex="^[A-Za-z]*:[A-Za-z0-9]*")],
-            help_text="(Ontology: OBI) links to experiment ontology information."))
-    molecule_ontology_curie = ArrayField(models.CharField(max_length=30, null=True, blank=True,
-            validators=[RegexValidator(regex="^[A-Za-z]*:[A-Za-z0-9]*")],
-            help_text="(Ontology: SO) links to molecule ontology information."))
-    molecule = models.CharField(choices=MOLECULE, max_length=20, null=True, blank=True,
-            help_text="(Controlled Vocabulary) The type of molecule that was extracted from the biological material. Include one of the following: total RNA, polyA RNA, cytoplasmic RNA, nuclear RNA, small RNA, genomic DNA, protein, or other.")
+    library_strategy = CharField(choices=LIBRARY_STRATEGY, max_length=25, null=False, blank=False, help_text=rec_help(d.EXPERIMENT, 'library_strategy'))
 
-    library_strategy = models.CharField(choices=LIBRARY_STRATEGY, max_length=25, null=False, blank=False,
-            help_text="(Controlled Vocabulary) The assay used. These are defined within the SRA metadata specifications with a controlled vocabulary (e.g. ‘Bisulfite-Seq’, ‘RNA-Seq’, ‘ChIP-Seq’). For a complete list, see https://www.ebi.ac.uk/ena/submit/reads-library-strategy.")
-
-    other_fields = JSONField(blank=True, null=True, help_text='The other fields for the experiment')
+    other_fields = JSONField(blank=True, null=True, validators=[keyValueValidator], help_text=rec_help(d.EXPERIMENT, 'other_fields'))
 
     def __str__(self):
         return str(self.id)
