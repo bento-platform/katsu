0568a30ca2c99be1cf64b8518219a11338d06d41
Rom Grk <romgrk.cc@gmail.com>
2020-03-27 13:22:25 -0400

    experiments: add api routes

---
+++ chord_metadata_service/experiments/api_views.py
@@ -0,0 +1,25 @@
+from rest_framework import viewsets
+from rest_framework.settings import api_settings
+from .serializers import ExperimentSerializer
+from .models import Experiment
+from chord_metadata_service.phenopackets.api_views import BIOSAMPLE_PREFETCH, PHENOPACKET_PREFETCH
+from chord_metadata_service.restapi.api_renderers import (
+    FHIRRenderer,
+    PhenopacketsRenderer
+)
+from chord_metadata_service.restapi.pagination import LargeResultsSetPagination
+
+
+class ExperimentViewSet(viewsets.ModelViewSet):
+    """
+    get:
+    Return a list of all existing experiments
+
+    post:
+    Create a new experiment
+    """
+
+    queryset = Experiment.objects.all().order_by("id")
+    serializer_class = ExperimentSerializer
+    pagination_class = LargeResultsSetPagination
+    renderer_classes = tuple(api_settings.DEFAULT_RENDERER_CLASSES)
