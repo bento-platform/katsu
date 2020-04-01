7505e8b9773c117ece49524e0b41b96f2fad0dcc
ksenia1zaytseva <ksenia1zaytseva@gmail.com>
2020-04-01 15:06:10 -0400

    move validation to models in patients

--- chord_metadata_service/patients/models.py
+++ chord_metadata_service/patients/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.postgres.fields import JSONField, ArrayField
 from chord_metadata_service.restapi.models import IndexableMixin
+from chord_metadata_service.restapi.validators import (
+    ontology_validator, age_or_age_range_validator, comorbid_condition_validator
+)
 
 
 class Individual(models.Model, IndexableMixin):
@@ -33,24 +36,28 @@ class Individual(models.Model, IndexableMixin):
                                help_text='A list of alternative identifiers for the individual.')
     date_of_birth = models.DateField(null=True, blank=True, help_text='A timestamp either exact or imprecise.')
     # An ISO8601 string represent age
-    age = JSONField(blank=True, null=True, help_text='The age or age range of the individual.')
+    age = JSONField(blank=True, null=True, validators=[age_or_age_range_validator],
+                    help_text='The age or age range of the individual.')
     sex = models.CharField(choices=SEX, max_length=200,  blank=True, null=True,
                            help_text='Observed apparent sex of the individual.')
     karyotypic_sex = models.CharField(choices=KARYOTYPIC_SEX, max_length=200, default='UNKNOWN_KARYOTYPE',
                                       help_text='The karyotypic sex of the individual.')
-    taxonomy = JSONField(blank=True, null=True,
+    taxonomy = JSONField(blank=True, null=True, validators=[ontology_validator],
                          help_text='Ontology resource representing the species (e.g., NCBITaxon:9615).')
     # FHIR specific
     active = models.BooleanField(default=False, help_text='Whether this patient\'s record is in active use.')
     deceased = models.BooleanField(default=False, help_text='Indicates if the individual is deceased or not.')
     # mCode specific
     # this field should be complex Ontology - clinical status and code - two Codeable concept - single, cl status has enum list of values
-    comorbid_condition = JSONField(blank=True, null=True, help_text='One or more conditions that occur with primary'
-                                                                    ' condition.')
-    #TODO decide use ONTOLOGY_CLASS vs. CODEABLE_CONCEPT
-    ecog_performance_status = JSONField(blank=True, null=True, help_text='Value representing the Eastern Cooperative '
-                                                                         'Oncology Group performance status.')
-    karnofsky = JSONField(blank=True, null=True, help_text='Value representing the Karnofsky Performance status.')
+    # TODO add these fields to FHIR converter ?
+    comorbid_condition = JSONField(blank=True, null=True, validators=[comorbid_condition_validator],
+                                   help_text='One or more conditions that occur with primary condition.')
+    #TODO decide use ONTOLOGY_CLASS vs. CODEABLE_CONCEPT - currently Ontology class
+    ecog_performance_status = JSONField(blank=True, null=True, validators=[ontology_validator],
+                                        help_text='Value representing the Eastern Cooperative '
+                                                  'Oncology Group performance status.')
+    karnofsky = JSONField(blank=True, null=True, validators=[ontology_validator],
+                          help_text='Value representing the Karnofsky Performance status.')
     race = models.CharField(max_length=200, blank=True, help_text='A code for the person\'s race.')
     ethnicity = models.CharField(max_length=200, blank=True, help_text='A code for the person\'s ethnicity.')
     # extra
