e29b66ddd0b752037e8917287af49d57bd9ea464
David Lougheed <david.lougheed@gmail.com>
2020-05-19 15:36:18 -0400

    Fix experiment tests for model updates

--- chord_metadata_service/experiments/tests/test_models.py
+++ chord_metadata_service/experiments/tests/test_models.py
@@ -2,14 +2,18 @@ from django.test import TestCase
 from django.core.exceptions import ValidationError
 from rest_framework import serializers
 from chord_metadata_service.patients.models import Individual
+from chord_metadata_service.phenopackets.models import Biosample, Procedure
 from ..models import Experiment
+from chord_metadata_service.phenopackets.tests.constants import VALID_PROCEDURE_1, VALID_INDIVIDUAL_1, valid_biosample_1
 
 
 class ExperimentTest(TestCase):
     """ Test module for Experiment model """
 
     def setUp(self):
-        Individual.objects.create(id='patient:1', sex='FEMALE', age={"age": "P25Y3M2D"})
+        i = Individual.objects.create(**VALID_INDIVIDUAL_1)
+        p = Procedure.objects.create(**VALID_PROCEDURE_1)
+        self.biosample = Biosample.objects.create(**valid_biosample_1(i, p))
         Experiment.objects.create(
             id='experiment:1',
             reference_registry_id='some_id',
@@ -19,47 +23,50 @@ class ExperimentTest(TestCase):
             molecule_ontology=[{"id": "ontology:1", "label": "Ontology term 1"}],
             molecule='total RNA',
             library_strategy='Bisulfite-Seq',
-            other_fields={"some_field": "value"}
+            other_fields={"some_field": "value"},
+            biosample=self.biosample
         )
 
     def create(self, **kwargs):
-        e = Experiment(**kwargs)
+        e = Experiment(id="experiment:2", **kwargs)
         e.full_clean()
         e.save()
 
     def test_validation(self):
-        individual_one = Individual.objects.get(id='patient:1')
-
         # Invalid experiment_ontology
-        self.assertRaises(serializers.ValidationError, self.create,
-                id='experiment:2',
-                library_strategy='Bisulfite-Seq',
-                experiment_type='Chromatin Accessibility',
-                experiment_ontology=["invalid_value"],
-                individual=individual_one
+        self.assertRaises(
+            serializers.ValidationError,
+            self.create,
+            library_strategy='Bisulfite-Seq',
+            experiment_type='Chromatin Accessibility',
+            experiment_ontology=["invalid_value"],
+            biosample=self.biosample
         )
 
         # Invalid molecule_ontology
-        self.assertRaises(serializers.ValidationError, self.create,
-                id='experiment:2',
-                library_strategy='Bisulfite-Seq',
-                experiment_type='Chromatin Accessibility',
-                molecule_ontology=[{"id": "some_id"}],
-                individual=individual_one
+        self.assertRaises(
+            serializers.ValidationError,
+            self.create,
+            library_strategy='Bisulfite-Seq',
+            experiment_type='Chromatin Accessibility',
+            molecule_ontology=[{"id": "some_id"}],
+            biosample=self.biosample
         )
 
         # Invalid value in other_fields
-        self.assertRaises(serializers.ValidationError, self.create,
-                id='experiment:2',
-                library_strategy='Bisulfite-Seq',
-                experiment_type='Chromatin Accessibility',
-                other_fields={"some_field": "value", "invalid_value": 42},
-                individual=individual_one
+        self.assertRaises(
+            serializers.ValidationError,
+            self.create,
+            library_strategy='Bisulfite-Seq',
+            experiment_type='Chromatin Accessibility',
+            other_fields={"some_field": "value", "invalid_value": 42},
+            biosample=self.biosample
         )
 
-        # Missing individual or biosamples
-        self.assertRaises(ValidationError, self.create,
-                id='experiment:2',
-                library_strategy='Bisulfite-Seq',
-                experiment_type='Chromatin Accessibility'
+        # Missing biosample
+        self.assertRaises(
+            ValidationError,
+            self.create,
+            library_strategy='Bisulfite-Seq',
+            experiment_type='Chromatin Accessibility'
         )
