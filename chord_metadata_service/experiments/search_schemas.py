1d8bedc6b1c57f485503e19afa9577c96950742a
David Lougheed <david.lougheed@gmail.com>
2020-05-21 10:58:18 -0400

    Add ordering to experiment search schema

--- chord_metadata_service/experiments/search_schemas.py
+++ chord_metadata_service/experiments/search_schemas.py
@@ -13,37 +13,37 @@ __all__ = ["EXPERIMENT_SEARCH_SCHEMA"]
 EXPERIMENT_SEARCH_SCHEMA = tag_schema_with_search_properties(schemas.EXPERIMENT_SCHEMA, {
     "properties": {
         "id": {
-            "search": {"database": {"field": models.Experiment._meta.pk.column}}
+            "search": {"order": 0, "database": {"field": models.Experiment._meta.pk.column}}
         },
         "reference_registry_id": {
-            "search": search_optional_str(0, queryable="internal"),
+            "search": search_optional_str(1, queryable="internal"),
         },
         "qc_flags": {
             "items": {
                 "search": search_optional_str(0),
             },
-            "search": {"database": {"type": "array"}}
+            "search": {"order": 2, "database": {"type": "array"}}
         },
         "experiment_type": {
-            "search": search_optional_str(1, queryable="internal"),
+            "search": search_optional_str(3),
         },
         "experiment_ontology": {
             "items": ONTOLOGY_SEARCH_SCHEMA,  # TODO: Specific ontology?
-            "search": {"database": {"type": "jsonb"}}
+            "search": {"order": 4, "database": {"type": "jsonb"}}
         },
         "molecule": {
-            "search": search_optional_eq(2),
+            "search": search_optional_eq(5),
         },
         "molecule_ontology": {
             "items": ONTOLOGY_SEARCH_SCHEMA,  # TODO: Specific ontology?
-            "search": {"database": {"type": "jsonb"}}
+            "search": {"order": 6, "database": {"type": "jsonb"}}
         },
         "library_strategy": {
-            "search": search_optional_eq(3),
+            "search": search_optional_eq(7),
         },
         # TODO: other_fields: ?
         "biosample": {
-            "search": search_optional_eq(4, queryable="internal"),
+            "search": search_optional_eq(8, queryable="internal"),
         },
     },
     "search": {
