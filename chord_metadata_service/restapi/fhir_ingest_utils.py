ffe0cb871581cf95915fcbfbc01d7747b8f916e1
ksenia1zaytseva <ksenia1zaytseva@gmail.com>
2020-05-26 18:38:37 -0400

    add schema check

--- chord_metadata_service/restapi/fhir_ingest_utils.py
+++ chord_metadata_service/restapi/fhir_ingest_utils.py
@@ -72,50 +72,59 @@ def _parse_reference(ref):
     return ref.split('/')[-1]
 
 
-def _check_fhir_schema(schema, obj):
-    return
+def _check_schema(schema, obj, additional_info=None):
+    """ Validates schema and catches errors. """
+    try:
+        jsonschema.validate(obj, schema)
+    except jsonschema.exceptions.ValidationError:
+        v = jsonschema.Draft7Validator(schema)
+        errors = [e for e in v.iter_errors(obj)]
+        error_messages = []
+        for i, error in enumerate(errors, 1):
+            error_messages.append(f"{i} validation error {'.'.join(str(v) for v in error.path)}: {error.message}")
+        raise ValidationError(f"{additional_info+' ' if additional_info else None}errors: {error_messages}")
 
 
 @api_view(["POST"])
 @permission_classes([AllowAny])
 def ingest_fhir(request):
-    try:
-        jsonschema.validate(request.data, FHIR_INGEST_SCHEMA)
-    except jsonschema.exceptions.ValidationError:
-        v = jsonschema.Draft7Validator(FHIR_INGEST_SCHEMA)
-        errors = [e for e in v.iter_errors(request.data)]
-        for i, error in enumerate(errors, 1):
-            return Response(bad_request_error(
-                f"{i} Validation error in {'.'.join(str(v) for v in error.path)}: {error.message}"
-            ))
+    """
+    View to ingest FHIR data.
+    Takes FHIR Bundles (collections of resources) of the following types:
+    Patient, Observation, Condition, Specimen.
+    """
+
+    # check schema of ingest body
+    _check_schema(FHIR_INGEST_SCHEMA, request.data, 'ingest body')
+
+    # check if dataset exists
     if not Dataset.objects.filter(identifier=request.data["dataset_id"]).exists():
         return Response(bad_request_error(f"Dataset with ID {request.data['dataset_id']} does not exist"),
                         status=400)
-
-    # create new phenopacket for each individual
-    # fake metadata
-    meta_data_obj = MetaData.objects.create(
-        created_by=request.data["metadata"]["created_by"],
-        phenopacket_schema_version="1.0.0-RC3",
-        external_references=[]
-    )
 
     # patients-individuals
     with open(request.data["patients"], "r") as p_file:
         try:
             patients_data = json.load(p_file)
-            if isinstance(patients_data, dict):
-                # List of FHIR resources is of ResourceType "Bundle"
-                for item in patients_data["entry"]:
-                    individual_data = patient_to_individual(item["resource"])
-                    individual, _ = Individual.objects.get_or_create(**individual_data)
-                    phenopacket = Phenopacket.objects.create(
-                        id=str(uuid.uuid4()),
-                        subject=individual,
-                        meta_data=meta_data_obj,
-                        dataset=Dataset.objects.get(identifier=request.data["dataset_id"])
-                    )
-                    print(f'Phenopacket {phenopacket.id} created')
+            # check if Patients data follows FHIR Bundle schema
+            _check_schema(FHIR_BUNDLE_SCHEMA, patients_data, 'patients data')
+            for item in patients_data["entry"]:
+                individual_data = patient_to_individual(item["resource"])
+                individual, _ = Individual.objects.get_or_create(**individual_data)
+                # create metadata for Phenopacket
+                meta_data_obj, _ = MetaData.objects.get_or_create(
+                    created_by=request.data["metadata"]["created_by"],
+                    phenopacket_schema_version="1.0.0-RC3",
+                    external_references=[]
+                )
+                # create new phenopacket for each individual
+                phenopacket = Phenopacket.objects.create(
+                    id=str(uuid.uuid4()),
+                    subject=individual,
+                    meta_data=meta_data_obj,
+                    dataset=Dataset.objects.get(identifier=request.data["dataset_id"])
+                )
+                print(f'Phenopacket {phenopacket.id} created')
         except json.decoder.JSONDecodeError as e:
             return Response(bad_request_error(f"Invalid JSON provided (message: {e})"), status=400)
 
@@ -124,10 +133,13 @@ def ingest_fhir(request):
         with open(request.data["observations"], "r") as obs_file:
             try:
                 observations_data = json.load(obs_file)
+                # check if Observations data follows FHIR Bundle schema
+                _check_schema(FHIR_BUNDLE_SCHEMA, observations_data, 'observations data')
                 for item in observations_data["entry"]:
                     phenotypic_feature_data = observation_to_phenotypic_feature(item["resource"])
+                    # Observation must have a subject
                     if not item["resource"]["subject"]:
-                        return Response(bad_request_error(f"Subject is required."), status=404)
+                        return Response(bad_request_error(f"Observation's subject is required."), status=404)
 
                     subject = _parse_reference(item["resource"]["subject"]["reference"])
                     phenotypic_feature, _ = PhenotypicFeature.objects.get_or_create(
@@ -142,9 +154,12 @@ def ingest_fhir(request):
         with open(request.data["conditions"]) as c_file:
             try:
                 conditions_data = json.load(c_file)
+                # check if Conditions data follows FHIR Bundle schema
+                _check_schema(FHIR_BUNDLE_SCHEMA, conditions_data, 'conditions data')
                 for item in conditions_data["entry"]:
                     disease_data = condition_to_disease(item["resource"])
-                    disease = Disease.objects.create(**disease_data)
+                    disease= Disease.objects.create(**disease_data)
+                    # Condition must have a subject
                     if not item["resource"]["subject"]:
                         return Response(bad_request_error(f"Subject is required."), status=404)
                     subject = _parse_reference(item["resource"]["subject"]["reference"])
@@ -159,11 +174,17 @@ def ingest_fhir(request):
         with open(request.data["specimens"], "r") as s_file:
             try:
                 specimens_data = json.load(s_file)
+                # check if Specimens data follows FHIR Bundle schema
+                _check_schema(FHIR_BUNDLE_SCHEMA, specimens_data, 'specimens data')
                 for item in specimens_data["entry"]:
                     biosample_data = specimen_to_biosample(item["resource"])
                     procedure, _ = Procedure.objects.get_or_create(**biosample_data["procedure"])
+                    # Specimen must have a subject
+                    if not biosample_data["individual"]:
+                        return Response(bad_request_error(f"Specimen's subject is required."), status=404)
+
                     individual_id = _parse_reference(biosample_data["individual"])
-                    Biosample.objects.create(
+                    biosample, _ = Biosample.objects.get_or_create(
                         id=biosample_data["id"],
                         procedure=procedure,
                         individual=Individual.objects.get(id=individual_id),
