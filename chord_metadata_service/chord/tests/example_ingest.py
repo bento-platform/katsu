4c32489575a76911c8363ac630c5129bcc2d29cd
ksenia1zaytseva <ksenia1zaytseva@gmail.com>
2020-06-09 11:58:31 -0400

    fix path error in travis

--- chord_metadata_service/chord/tests/example_ingest.py
+++ chord_metadata_service/chord/tests/example_ingest.py
@@ -1,9 +1,13 @@
 import json
 import os
+import sys
+
 
 __all__ = ["EXAMPLE_INGEST_PHENOPACKET", "EXAMPLE_INGEST_OUTPUTS"]
 
-with open(os.path.join(os.path.dirname(__file__), "example_phenopacket.json"), "r") as pf:
+current_dir = os.path.join(os.path.dirname(sys.path[0]), os.path.dirname(__file__))
+
+with open(os.path.join(current_dir, "example_phenopacket.json"), "r") as pf:
     EXAMPLE_INGEST_PHENOPACKET = json.load(pf)
 
 EXAMPLE_INGEST_OUTPUTS = {
