6c6f3c30fb7473d4e54a0cded3562ebb7d52d152
ksenia1zaytseva <ksenia1zaytseva@gmail.com>
2020-06-09 16:59:08 -0400

    change path back

--- chord_metadata_service/chord/tests/example_ingest.py
+++ chord_metadata_service/chord/tests/example_ingest.py
@@ -1,13 +1,10 @@
 import json
 import os
-import sys
 
 
 __all__ = ["EXAMPLE_INGEST_PHENOPACKET", "EXAMPLE_INGEST_OUTPUTS"]
 
-current_dir = os.path.join(os.path.dirname(sys.path[0]), os.path.dirname(__file__))
-
-with open(os.path.join(current_dir, "example_phenopacket.json"), "r") as pf:
+with open(os.path.join(os.path.dirname(__file__), "example_phenopacket.json"), "r") as pf:
     EXAMPLE_INGEST_PHENOPACKET = json.load(pf)
 
 EXAMPLE_INGEST_OUTPUTS = {
