3b3614564617873522a242c0bb78a6b6eb8cfaf6
ksenia1zaytseva <ksenia1zaytseva@gmail.com>
2020-06-10 23:37:51 -0400

    add the rest of mcode profiles v1.0.0

--- chord_metadata_service/mcode/mappings/mcode_profiles.py
+++ chord_metadata_service/mcode/mappings/mcode_profiles.py
@@ -5,20 +5,21 @@ MCODE_COMORBID_CONDITION = "http://hl7.org/fhir/us/mcode/StructureDefinition/mco
 MCODE_ECOG_PERFORMANCE_STATUS = "http://hl7.org/fhir/us/mcode/StructureDefinition/mcode-ecog-performance-status"
 MCODE_KARNOFSKY = "http://hl7.org/fhir/us/mcode/StructureDefinition/mcode-karnofsky-performance-status"
 
+# GeneticSpecimen
+MCODE_GENETIC_SPECIMEN = "http://hl7.org/fhir/us/mcode/StructureDefinition/mcode-genetic-specimen"
+
 # GeneticVariant
 MCODE_GENETIC_VARIANT = "http://hl7.org/fhir/us/mcode/StructureDefinition/mcode-cancer-genetic-variant"
+
+# GenomicRegionStudied
+MCODE_GENOMIC_REGION_STUDIED = "http://hl7.org/fhir/us/mcode/StructureDefinition/mcode-genomic-region-studied"
 
 # GenomicsReport
 MCODE_GENOMICS_REPORT = "http://hl7.org/fhir/us/mcode/StructureDefinition/mcode-cancer-genomics-report"
 
 # LabsVital
 # the following are present in Ballout 1 version but not in 1.0.0 version
-MCODE_BODY_HEIGHT = ""
-MCODE_BODY_WEIGHT = ""
-MCODE_CBC_WITH_AUTO_DIFFERENTIAL_PANEL = ""
-MCODE_COMPREHENSIVE_METABOLIC_2000 = ""
-MCODE_BLOOD_PRESSURE = ""
-MCODE_TUMOR_MARKER_TEST = "http://hl7.org/fhir/us/mcode/StructureDefinition/mcode-tumor-marker"
+MCODE_TUMOR_MARKER = "http://hl7.org/fhir/us/mcode/StructureDefinition/mcode-tumor-marker"
 
 # CancerCondition
 MCODE_PRIMARY_CANCER_CONDITION = "http://hl7.org/fhir/us/mcode/StructureDefinition/mcode-primary-cancer-condition"
@@ -26,16 +27,16 @@ MCODE_SECONDARY_CANCER_CONDITION = "http://hl7.org/fhir/us/mcode/StructureDefini
 
 # TNMStaging
 # CLINICAL
-MCODE_CLINICAL_STAGE_GROUP = "http://hl7.org/fhir/us/mcode/StructureDefinition/mcode-tnm-clinical-stage-group"
-MCODE_CLINICAL_PRIMARY_TUMOR_CATEGORY = "http://hl7.org/fhir/us/mcode/StructureDefinition/mcode-tnm-clinical-primary-tumor-category"
-MCODE_CLINICAL_REGIONAL_NODES_CATEGORY = "http://hl7.org/fhir/us/mcode/StructureDefinition/mcode-tnm-clinical-regional-nodes-category"
-MCODE_CLINICAL_DISTANT_METASTASES_CATEGORY = "http://hl7.org/fhir/us/mcode/StructureDefinition/mcode-tnm-clinical-distant-metastases-category"
+MCODE_TNM_CLINICAL_STAGE_GROUP = "http://hl7.org/fhir/us/mcode/StructureDefinition/mcode-tnm-clinical-stage-group"
+MCODE_TNM_CLINICAL_PRIMARY_TUMOR_CATEGORY = "http://hl7.org/fhir/us/mcode/StructureDefinition/mcode-tnm-clinical-primary-tumor-category"
+MCODE_TNM_CLINICAL_REGIONAL_NODES_CATEGORY = "http://hl7.org/fhir/us/mcode/StructureDefinition/mcode-tnm-clinical-regional-nodes-category"
+MCODE_TNM_CLINICAL_DISTANT_METASTASES_CATEGORY = "http://hl7.org/fhir/us/mcode/StructureDefinition/mcode-tnm-clinical-distant-metastases-category"
 
 # PATHOLOGIC
-MCODE_PATHOLOGIC_STAGE_GROUP = "http://hl7.org/fhir/us/mcode/StructureDefinition/mcode-tnm-pathological-stage-group"
-MCODE_PATHOLOGIC_PRIMARY_TUMOR_CATEGORY = "http://hl7.org/fhir/us/mcode/StructureDefinition/mcode-tnm-pathological-primary-tumor-category"
-MCODE_PATHOLOGIC_REGIONAL_NODES_CATEGORY = "http://hl7.org/fhir/us/mcode/StructureDefinition/mcode-tnm-pathological-regional-nodes-category"
-MCODE_PATHOLOGIC_DISTANT_METASTASES_CATEGORY = "http://hl7.org/fhir/us/mcode/StructureDefinition/mcode-tnm-pathological-distant-metastases-category"
+MCODE_TNM_PATHOLOGIC_STAGE_GROUP = "http://hl7.org/fhir/us/mcode/StructureDefinition/mcode-tnm-pathological-stage-group"
+MCODE_TNM_PATHOLOGIC_PRIMARY_TUMOR_CATEGORY = "http://hl7.org/fhir/us/mcode/StructureDefinition/mcode-tnm-pathological-primary-tumor-category"
+MCODE_TNM_PATHOLOGIC_REGIONAL_NODES_CATEGORY = "http://hl7.org/fhir/us/mcode/StructureDefinition/mcode-tnm-pathological-regional-nodes-category"
+MCODE_TNM_PATHOLOGIC_DISTANT_METASTASES_CATEGORY = "http://hl7.org/fhir/us/mcode/StructureDefinition/mcode-tnm-pathological-distant-metastases-category"
 
 # CancerRelatedProcedure
 # CancerRelatedRadiationProcedure
@@ -46,5 +47,15 @@ MCODE_CANCER_RELATED_SURGICAL_PROCEDURE = "http://hl7.org/fhir/us/mcode/Structur
 # MedicationStatement
 MCODE_MEDICATION_STATEMENT = "http://hl7.org/fhir/us/mcode/StructureDefinition/mcode-cancer-related-medication-statement"
 
-# add it to mCodepacket
+# mCodePacket
 MCODE_CANCER_DISEASE_STATUS = "http://hl7.org/fhir/us/mcode/StructureDefinition/mcode-cancer-disease-status"
+
+# Extension definitions
+MCODE_LATERALITY = "http://hl7.org/fhir/us/mcode/StructureDefinition/mcode-laterality"
+
+# CancerCondition histology_morphology_behavior
+MCODE_HISTOLOGY_MORPHOLOGY_BEHAVIOR = "http://hl7.org/fhir/us/mcode/StructureDefinition/mcode-histology-morphology-behavior"
+
+# MedicationStatement
+MCODE_TERMINATION_REASON = "http://hl7.org/fhir/us/mcode/StructureDefinition/mcode-termination-reason"
+MCODE_TREATMENT_INTENT = "http://hl7.org/fhir/us/mcode/StructureDefinition/mcode-treatment-intent"
