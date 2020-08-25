fa8d2344edaa2469b056c0e076789fbcf99ebee8
zxenia <ksenia1zaytseva@gmail.com>
2020-08-25 16:05:12 -0400

    add http

--- chord_metadata_service/metadata/settings.py
+++ chord_metadata_service/metadata/settings.py
@@ -104,11 +104,11 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    "localhost:8000",
-    "localhost:8080"
-    "127.0.0.1",
-    "127.0.0.1:8000",
-    "127.0.0.1:8080",
+    "http://localhost:8000",
+    "http://localhost:8080",
+    "http://127.0.0.1",
+    "http://127.0.0.1:8000",
+    "http://127.0.0.1:8080",
 ]
 
 ROOT_URLCONF = 'chord_metadata_service.metadata.urls'
