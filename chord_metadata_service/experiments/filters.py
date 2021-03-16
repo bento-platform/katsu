53aeba9587161346b116d4b7f3568f7db3ecce1a
zxenia <ksenia1zaytseva@gmail.com>
2021-03-16 15:12:36 -0400

    change charfields filtering for icontains add new fields to filtering

--- chord_metadata_service/experiments/filters.py
+++ chord_metadata_service/experiments/filters.py
@@ -3,12 +3,12 @@ from .models import Experiment
 
 
 class ExperimentFilter(django_filters.rest_framework.FilterSet):
-    experiment_type = django_filters.CharFilter(lookup_expr='iexact')
-    molecule = django_filters.CharFilter(lookup_expr='iexact')
-    library_strategy = django_filters.CharFilter(lookup_expr='iexact')
+    experiment_type = django_filters.CharFilter(lookup_expr='icontains')
+    molecule = django_filters.CharFilter(lookup_expr='icontains')
+    library_strategy = django_filters.CharFilter(lookup_expr='icontains')
+    extraction_protocol = django_filters.CharFilter(lookup_expr='icontains')
+    file_location = django_filters.CharFilter(lookup_expr='icontains')
 
     class Meta:
         model = Experiment
-        fields = ["id", "reference_registry_id",
-                  "experiment_type", "molecule",
-                  "library_strategy", "biosample"]
+        fields = ["id", "reference_registry_id", "biosample"]
