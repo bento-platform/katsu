ab78192fddcfd9755df999bc9b87a63d5cf0662d
zxenia <ksenia1zaytseva@gmail.com>
2021-06-01 16:04:20 -0400

    update descriptions

--- chord_metadata_service/experiments/descriptions.py
+++ chord_metadata_service/experiments/descriptions.py
@@ -3,11 +3,27 @@
 
 from chord_metadata_service.restapi.description_utils import EXTRA_PROPERTIES, ontology_class
 
-
 EXPERIMENT = {
     "description": "Experiment related metadata.",
     "properties": {
         "id": "An arbitrary identifier for the experiment.",
+        "experiment_type": "(Controlled Vocabulary) The assay target (e.g. ‘DNA Methylation’, ‘mRNA-Seq’, ‘smRNA-Seq’, "
+                           "'Histone H3K4me1').",
+        "experiment_ontology": {
+            "description": "Links to experiment ontology information (e.g. via the OBI ontology.)",
+            "items": ontology_class("describing the experiment"),
+        },
+        "molecule": "(Controlled Vocabulary) The type of molecule that was extracted from the biological material."
+                    "Include one of the following: total RNA, polyA RNA, cytoplasmic RNA, nuclear RNA, small RNA, "
+                    "genomic DNA, protein, or other.",
+        "molecule_ontology": {
+            "description": "Links to molecule ontology information (e.g. via the SO ontology.)",
+            "items": ontology_class("describing a molecular property"),
+        },
+        "library_strategy": "(Controlled Vocabulary) The assay used. These are defined within the SRA metadata "
+                            "specifications with a controlled vocabulary (e.g. ‘Bisulfite-Seq’, ‘RNA-Seq’, ‘ChIP-Seq’)."
+                            " For a complete list, see https://www.ebi.ac.uk/ena/submit/reads-library-strategy.",
+        "extraction_protocol": "The protocol used to isolate the extract material.",
 
         "reference_registry_id": "The IHEC EpiRR ID for this dataset, only for IHEC Reference Epigenome datasets. "
                                  "Otherwise leave empty.",
@@ -15,26 +31,8 @@ EXPERIMENT = {
             "description": "Any quality control observations can be noted here. This field can be omitted if empty",
             "items": "A quality control observation.",
         },
-        "experiment_type": "(Controlled Vocabulary) The assay target (e.g. ‘DNA Methylation’, ‘mRNA-Seq’, ‘smRNA-Seq’, "
-                           "'Histone H3K4me1').",
-        "experiment_ontology": {
-            "description": "Links to experiment ontology information (e.g. via the OBI ontology.)",
-            "items": ontology_class("describing the experiment"),
-        },
-        "molecule_ontology": {
-            "description": "Links to molecule ontology information (e.g. via the SO ontology.)",
-            "items": ontology_class("describing a molecular property"),
-        },
-        "molecule": "(Controlled Vocabulary) The type of molecule that was extracted from the biological material."
-                    "Include one of the following: total RNA, polyA RNA, cytoplasmic RNA, nuclear RNA, small RNA, "
-                    "genomic DNA, protein, or other.",
-        "library_strategy": "(Controlled Vocabulary) The assay used. These are defined within the SRA metadata "
-                            "specifications with a controlled vocabulary (e.g. ‘Bisulfite-Seq’, ‘RNA-Seq’, ‘ChIP-Seq’)."
-                            " For a complete list, see https://www.ebi.ac.uk/ena/submit/reads-library-strategy.",
-        "extraction_protocol": "The protocol used to isolate the extract material.",
-        "file_location": "The location of the file that contains the analysis of sequencing data.",
+        "experiment_results": "Related experiments files containing the analysis of sequencing data.",
         "biosample": "Biosample on which this experiment was done.",
-
         **EXTRA_PROPERTIES
     }
 }
@@ -47,6 +45,7 @@ EXPERIMENT_RESULT = {
         "filename": "The name of the file containing the result.",
         "file_format": "(Controlled Vocabulary) File format.",
         "creation_date": "The date when the experiment result file was created.",
+        "created_by": "Name/Username/Code of the person who prepared the sequncing data.",
         **EXTRA_PROPERTIES
     }
 }
