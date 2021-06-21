6f3b887065072c5f8d835ee707c94b3fac186929
zxenia <ksenia1zaytseva@gmail.com>
2021-06-21 15:02:48 -0400

    add experiment result schema; update experiment schema

--- chord_metadata_service/experiments/schemas.py
+++ chord_metadata_service/experiments/schemas.py
@@ -1,9 +1,44 @@
-from .descriptions import EXPERIMENT
+from .descriptions import EXPERIMENT, EXPERIMENT_RESULT
 from chord_metadata_service.restapi.description_utils import describe_schema
 from chord_metadata_service.restapi.schemas import ONTOLOGY_CLASS_LIST, KEY_VALUE_OBJECT
 
 
 __all__ = ["EXPERIMENT_SCHEMA"]
+
+
+EXPERIMENT_RESULT_SCHEMA = describe_schema({
+    "$schema": "http://json-schema.org/draft-07/schema#",
+    "$id": "chord_metadata_service:experiment_result_schema",
+    "title": "Experiment result schema",
+    "description": "Schema for describing information about analysis of sequencing data in a file format.",
+    "type": "object",
+    "properties": {
+        "identifier": {
+            "type": "string"
+        },
+        "description": {
+            "type": "string"
+        },
+        "filename": {
+            "type": "string"
+        },
+        "file_format": {
+            "type": "string",
+            "enum": []
+        },
+        "data_output_type": {
+            "type": "string",
+            "enum": []
+        },
+        "creation_date": {
+            "type": "string"
+        },
+        "created_by": {
+            "type": "string"
+        },
+        "extra_properties": KEY_VALUE_OBJECT,
+    }
+}, EXPERIMENT_RESULT)
 
 
 EXPERIMENT_SCHEMA = describe_schema({
@@ -16,14 +51,8 @@ EXPERIMENT_SCHEMA = describe_schema({
         "id": {
             "type": "string"
         },
-        "reference_registry_id": {
+        "study_type": {
             "type": "string"
-        },
-        "qc_flags": {
-            "type": "array",
-            "items": {
-                "type": "string"
-            }
         },
         "experiment_type": {
             "type": "string"
@@ -36,15 +65,34 @@ EXPERIMENT_SCHEMA = describe_schema({
         "library_strategy": {
             "type": "string"
         },
+        "library_source": {
+            "type": "string"
+        },
+        "library_selection": {
+            "type": "string"
+        },
+        "library_layout": {
+            "type": "string"
+        },
         "extraction_protocol": {
             "type": "string"
         },
-        "file_location": {
+        "reference_registry_id": {
             "type": "string"
+        },
+        "qc_flags": {
+            "type": "array",
+            "items": {
+                "type": "string"
+            }
         },
         "extra_properties": KEY_VALUE_OBJECT,
         "biosample": {
             "type": "string"
+        },
+        "experiment_results":{
+            "type": "array",
+            "items": EXPERIMENT_RESULT_SCHEMA
         },
     },
     "required": ["id", "experiment_type", "library_strategy"]
