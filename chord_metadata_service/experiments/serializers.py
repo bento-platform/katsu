03d076d58280dedea3613d354d6bacd6d38a8522
zxenia <ksenia1zaytseva@gmail.com>
2021-06-22 18:39:27 -0400

    add serializers fro ExperimentResult and Instrument

--- chord_metadata_service/experiments/serializers.py
+++ chord_metadata_service/experiments/serializers.py
@@ -1,11 +1,26 @@
 from chord_metadata_service.restapi.serializers import GenericSerializer
-from .models import Experiment
+from .models import Experiment, ExperimentResult, Instrument
 
 
 __all__ = ["ExperimentSerializer"]
 
 
+class ExperimentResultSerializer(GenericSerializer):
+    class Meta:
+        model = ExperimentResult
+        fields = "__all__"
+
+
+class InstrumentSerializer(GenericSerializer):
+    class Meta:
+        model = Instrument
+        fields = "__all__"
+
+
 class ExperimentSerializer(GenericSerializer):
+    experiment_results = ExperimentResultSerializer(read_only=True, many=True)
+    instrument = InstrumentSerializer()
+
     class Meta:
         model = Experiment
-        fields = '__all__'
+        fields = "__all__"
