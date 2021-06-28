320c538b46f7621b70fca0c74c74f6cbf57a2095
zxenia <ksenia1zaytseva@gmail.com>
2021-06-28 10:15:25 -0400

    add new char fields for search schemas

--- chord_metadata_service/experiments/search_schemas.py
+++ chord_metadata_service/experiments/search_schemas.py
@@ -48,6 +48,18 @@ EXPERIMENT_SEARCH_SCHEMA = tag_schema_with_search_properties(schemas.EXPERIMENT_
         "extraction_protocol": {
             "search": search_optional_str(9),
         },
+        "study_type": {
+            "search": search_optional_str(10),
+        },
+        "library_source": {
+            "search": search_optional_str(11),
+        },
+        "library_selection": {
+            "search": search_optional_str(12),
+        },
+        "library_layout": {
+            "search": search_optional_str(13),
+        },
     },
     "search": {
         "database": {
