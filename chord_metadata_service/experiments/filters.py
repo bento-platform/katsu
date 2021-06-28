04998d121797c5753a2613053839115dce745eda
zxenia <ksenia1zaytseva@gmail.com>
2021-06-28 10:06:54 -0400

    add filters for new char fields

--- chord_metadata_service/experiments/filters.py
+++ chord_metadata_service/experiments/filters.py
@@ -3,9 +3,13 @@ from .models import Experiment
 
 
 class ExperimentFilter(django_filters.rest_framework.FilterSet):
+    study_type = django_filters.CharFilter(lookup_expr='icontains')
     experiment_type = django_filters.CharFilter(lookup_expr='icontains')
     molecule = django_filters.CharFilter(lookup_expr='icontains')
     library_strategy = django_filters.CharFilter(lookup_expr='icontains')
+    library_source = django_filters.CharFilter(lookup_expr='icontains')
+    library_selection = django_filters.CharFilter(lookup_expr='icontains')
+    library_layout = django_filters.CharFilter(lookup_expr='icontains')
     extraction_protocol = django_filters.CharFilter(lookup_expr='icontains')
     extra_properties = django_filters.CharFilter(method="filter_extra_properties", label="Extra properties")
 
