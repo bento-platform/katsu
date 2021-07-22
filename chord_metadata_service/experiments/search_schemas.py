51fc999dd7237df0c0b3af9133859161090b0cdb
zxenia <ksenia1zaytseva@gmail.com>
2021-07-21 21:45:46 -0400

    add search schemas for experiment result and instrument

--- chord_metadata_service/experiments/search_schemas.py
+++ chord_metadata_service/experiments/search_schemas.py
@@ -7,8 +7,59 @@ from chord_metadata_service.restapi.schema_utils import (
 )
 from chord_metadata_service.restapi.search_schemas import ONTOLOGY_SEARCH_SCHEMA
 
-
 __all__ = ["EXPERIMENT_SEARCH_SCHEMA"]
+
+EXPERIMENT_RESULT_SEARCH_SCHEMA = tag_schema_with_search_properties(schemas.EXPERIMENT_RESULT, {
+    "properties": {
+        "identifier": {
+            "search": search_optional_eq(0)
+        },
+        "description": {
+            "search": search_optional_str(1)
+        },
+        "filename": {
+            "search": search_optional_str(2)
+        },
+        "file_format": {
+            "search": search_optional_eq(3)
+        },
+        "data_output_type": {
+            "search": search_optional_eq(4)
+        },
+        "usage": {
+            "search": search_optional_eq(5)
+        },
+    },
+    "search": {
+        "database": {
+            "primary_key": models.ExperimentResult._meta.pk.column,
+            "relation": models.ExperimentResult._meta.db_table,
+        }
+    }
+})
+
+INSTRUMENT_SEARCH_SCHEMA = tag_schema_with_search_properties(schemas.INSTRUMENT_SCHEMA, {
+    "properties": {
+        "identifier": {
+            "search": search_optional_eq(0)
+        },
+        "platform": {
+            "search": search_optional_str(1)
+        },
+        "description": {
+            "search": search_optional_str(2)
+        },
+        "model": {
+            "search": search_optional_str(3)
+        },
+    },
+    "search": {
+        "database": {
+            "primary_key": models.Instrument._meta.pk.column,
+            "relation": models.Instrument._meta.db_table,
+        }
+    }
+})
 
 EXPERIMENT_SEARCH_SCHEMA = tag_schema_with_search_properties(schemas.EXPERIMENT_SCHEMA, {
     "properties": {
@@ -62,6 +113,14 @@ EXPERIMENT_SEARCH_SCHEMA = tag_schema_with_search_properties(schemas.EXPERIMENT_
         "library_layout": {
             "search": search_optional_str(13),
         },
+        "instrument": merge_schema_dictionaries(
+            INSTRUMENT_SEARCH_SCHEMA,
+            {"search": {"database": {
+                "relationship": {
+                    "type": "MANY_TO_ONE",
+                    "foreign_key": models.Experiment._meta.get_field("instrument").column
+                }
+            }}}),
     },
     "search": {
         "database": {
