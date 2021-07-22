a8c86ddcc61da662d07c50cb7a9c57f28d910598
zxenia <ksenia1zaytseva@gmail.com>
2021-07-21 22:40:52 -0400

    add search for experiment results

--- chord_metadata_service/experiments/search_schemas.py
+++ chord_metadata_service/experiments/search_schemas.py
@@ -9,7 +9,7 @@ from chord_metadata_service.restapi.search_schemas import ONTOLOGY_SEARCH_SCHEMA
 
 __all__ = ["EXPERIMENT_SEARCH_SCHEMA"]
 
-EXPERIMENT_RESULT_SEARCH_SCHEMA = tag_schema_with_search_properties(schemas.EXPERIMENT_RESULT, {
+EXPERIMENT_RESULT_SEARCH_SCHEMA = tag_schema_with_search_properties(schemas.EXPERIMENT_RESULT_SCHEMA, {
     "properties": {
         "identifier": {
             "search": search_optional_eq(0)
@@ -27,7 +27,7 @@ EXPERIMENT_RESULT_SEARCH_SCHEMA = tag_schema_with_search_properties(schemas.EXPE
             "search": search_optional_eq(4)
         },
         "usage": {
-            "search": search_optional_eq(5)
+            "search": search_optional_str(5)
         },
     },
     "search": {
@@ -113,6 +113,7 @@ EXPERIMENT_SEARCH_SCHEMA = tag_schema_with_search_properties(schemas.EXPERIMENT_
         "library_layout": {
             "search": search_optional_str(13),
         },
+        # query example: ["#ico", ["#resolve", "instrument", "model"], "Illumina"]
         "instrument": merge_schema_dictionaries(
             INSTRUMENT_SEARCH_SCHEMA,
             {"search": {"database": {
@@ -121,6 +122,29 @@ EXPERIMENT_SEARCH_SCHEMA = tag_schema_with_search_properties(schemas.EXPERIMENT_
                     "foreign_key": models.Experiment._meta.get_field("instrument").column
                 }
             }}}),
+        # query example: ["#ico", ["#resolve", "experiment_results", "[item]", "description"], "VCF file"]
+        "experiment_results": {
+            "items": merge_schema_dictionaries(
+                EXPERIMENT_RESULT_SEARCH_SCHEMA,
+                {"search": {"database": {
+                    "relationship": {
+                        "type": "MANY_TO_ONE",
+                        "foreign_key": "experimentresult_id"  # TODO: No hard-code, from M2M
+                    }
+                }}}
+            ),
+            "search": {
+                "database": {
+                    "relation": models.Experiment._meta.get_field(
+                        "experiment_results").remote_field.through._meta.db_table,
+                    "relationship": {
+                        "type": "ONE_TO_MANY",
+                        "parent_foreign_key": "experiment_id",  # TODO: No hard-code
+                        "parent_primary_key": models.Experiment._meta.pk.column
+                    }
+                }
+            }
+        },
     },
     "search": {
         "database": {
