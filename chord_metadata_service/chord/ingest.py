0b287115bf2a970e94d050267740ed98734252b2
zxenia <ksenia1zaytseva@gmail.com>
2021-09-16 12:22:08 -0400

    edit info message

--- chord_metadata_service/chord/ingest.py
+++ chord_metadata_service/chord/ingest.py
@@ -715,7 +715,7 @@ def ingest_mcode_workflow(workflow_outputs, table_id):
 # it can be any existing table_id which can be validated
 # the workflow only performs copying files over to the DRS
 def ingest_readset_workflow(workflow_outputs, table_id):
-    logger.info(f"Debugging : {workflow_outputs}")
+    logger.info(f"Current workflow outputs : {workflow_outputs}")
     for readset_file in _get_output_or_raise(workflow_outputs, "readset_files"):
         with _workflow_file_output_to_path(readset_file) as readset_file_path:
             logger.info(f"Attempting ingestion of Readset file from path: {readset_file_path}")
