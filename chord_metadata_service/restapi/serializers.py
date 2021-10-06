ef1d3c83a60a43c0e02f9164fa826a9a1e0778ec
zxenia <ksenia1zaytseva@gmail.com>
2021-10-06 15:22:38 -0400

    add fields option to GenericSerializer

--- chord_metadata_service/restapi/serializers.py
+++ chord_metadata_service/restapi/serializers.py
@@ -8,11 +8,22 @@ class GenericSerializer(serializers.ModelSerializer):
     always_include: Tuple[str, ...] = ()
 
     def __init__(self, *args, **kwargs):
+        # fields to exclude when nested
         exclude_when_nested = kwargs.pop('exclude_when_nested', None)
+        # fields to include
+        fields = kwargs.pop('fields', None)
+
         super(GenericSerializer, self).__init__(*args, **kwargs)
 
         if exclude_when_nested:
             for field_name in exclude_when_nested:
+                self.fields.pop(field_name)
+
+        if fields is not None:
+            # Drop any fields that are not specified in the `fields` argument.
+            allowed = set(fields)
+            existing = set(self.fields)
+            for field_name in existing - allowed:
                 self.fields.pop(field_name)
 
     def to_representation(self, instance):
