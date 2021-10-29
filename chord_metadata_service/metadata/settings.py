fd419132e7f186015e6aa3fcfc89777c941e2e56
daisie_local <daisieh@gmail.com>
2021-10-29 16:17:51 -0700

    check secrets file

--- chord_metadata_service/metadata/settings.py
+++ chord_metadata_service/metadata/settings.py
@@ -23,7 +23,8 @@ load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-if os.environ.get('POSTGRES_PASSWORD_FILE') is not None:
+POSTGRES_PASSWORD_FILE = os.environ.get('POSTGRES_PASSWORD_FILE')
+if POSTGRES_PASSWORD_FILE is not None:
     with open(os.environ.get('POSTGRES_PASSWORD_FILE'), "r") as f:
         POSTGRES_PASSWORD_FILE = f.read()
 
