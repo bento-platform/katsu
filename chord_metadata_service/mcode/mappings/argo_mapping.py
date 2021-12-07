e4d2b353072449f4704f65859ed58d7db1956553
zxenia <ksenia1zaytseva@gmail.com>
2021-12-07 17:02:00 -0500

    update argo-mapping

--- chord_metadata_service/mcode/mappings/argo_mapping.py
+++ chord_metadata_service/mcode/mappings/argo_mapping.py
@@ -27,22 +27,26 @@ ARGO_MAPPING = {
         "code": "cancer_type_code",
         "date_of_diagnosis": "age_at_diagnosis",
         "histology_morphology_behavior": "",
-        "verification_status": "",
-        "extra_properties": [
-            "cancer_type_additional_information",
-        ]
+        "verification_status": ""
     },
-    "tnm_staging": {
-        "title": "PrimaryDiagnosis",
-        "id": "",
-        "tnm_type": "clinical",
-        "stage_group": "clinical_tumour_staging_system",
-        "primary_tumor_category": "clinical_t_category",
-        "regional_nodes_category": "clinical_n_category",
-        "distant_metastases_category": "clinical_m_category",
-        "cancer_condition": "submitter_primary_diagnosis_id",
-        "extra_properties": [
-            "clinical_stage_group",
-        ]
-    }
+    "tnm_staging": [
+        {
+            "title": "PrimaryDiagnosis",
+            "tnm_type": "clinical",
+            "stage_group": "clinical_stage_group",
+            "primary_tumor_category": "clinical_t_category",
+            "regional_nodes_category": "clinical_n_category",
+            "distant_metastases_category": "clinical_m_category",
+            "cancer_condition": "submitter_primary_diagnosis_id"
+        },
+        {
+            "title": "Specimen",
+            "tnm_type": "pathologic",
+            "stage_group": "pathological_stage_group",
+            "primary_tumor_category": "pathological_t_category",
+            "regional_nodes_category": "pathological_n_category",
+            "distant_metastases_category": "pathological_m_category",
+            "cancer_condition": "submitter_primary_diagnosis_id"
+        }
+    ]
 }
