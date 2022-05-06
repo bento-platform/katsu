873ee2a5736b631112fc2b7125d4ec3abb081c9d
Daisie Huang <daisieh@gmail.com>
2022-05-06 12:09:25 -0700

    add key

--- chord_metadata_service/metadata/settings.py
+++ chord_metadata_service/metadata/settings.py
@@ -77,6 +77,7 @@ DRS_URL = os.environ.get("DRS_URL", f"http+unix://{NGINX_INTERNAL_SOCKET}/api/dr
 CANDIG_AUTHORIZATION = os.environ.get("CANDIG_AUTHORIZATION")
 CANDIG_OPA_URL = os.environ.get("CANDIG_OPA_URL")
 CANDIG_OPA_SECRET = os.getenv("CANDIG_OPA_SECRET", "my-secret-beacon-token")
+CANDIG_OPA_SITE_ADMIN_KEY = os.environ.get("CANDIG_OPA_SITE_ADMIN_KEY")
 
 # Application definition
 
