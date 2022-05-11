b4cbdcfdab5589a867e1ebc39b63a7915e469a8e
zxenia <ksenia1zaytseva@gmail.com>
2022-05-11 13:39:25 -0400

    use list serializer for search results

--- chord_metadata_service/chord/views_search.py
+++ chord_metadata_service/chord/views_search.py
@@ -29,7 +29,7 @@ from chord_metadata_service.patients.models import Individual
 
 from chord_metadata_service.phenopackets.api_views import PHENOPACKET_SELECT_REL, PHENOPACKET_PREFETCH
 from chord_metadata_service.phenopackets.models import Phenopacket
-from chord_metadata_service.phenopackets.serializers import PhenopacketSerializer
+from chord_metadata_service.phenopackets.serializers import PhenopacketSerializer, ListPhenopacketSerializer
 
 from .data_types import DATA_TYPE_EXPERIMENT, DATA_TYPE_MCODEPACKET, DATA_TYPE_PHENOPACKET, DATA_TYPES
 from .models import Dataset, TableOwnership, Table
@@ -306,7 +306,7 @@ QUERY_RESULTS_FN: Dict[str, Callable] = {
 QUERY_RESULT_SERIALIZERS = {
     DATA_TYPE_EXPERIMENT: ExperimentSerializer,
     DATA_TYPE_MCODEPACKET: MCodePacketSerializer,
-    DATA_TYPE_PHENOPACKET: PhenopacketSerializer,
+    DATA_TYPE_PHENOPACKET: ListPhenopacketSerializer,
 }
 
 
