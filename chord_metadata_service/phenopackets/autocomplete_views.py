a5cb7daf004e68ba99a94aea58d1fb48f8aed23d
Paul Pillot <paul.pillot@mcgill.ca>
2022-09-06 17:49:41 +0000

    fix: order autocomplete suggestions
    This silences warnings by Django

--- chord_metadata_service/phenopackets/autocomplete_views.py
+++ chord_metadata_service/phenopackets/autocomplete_views.py
@@ -18,7 +18,7 @@ class DiseaseTermAutocomplete(autocomplete.Select2QuerySetView):
         # if not self.request.user.is_authenticated:
         #     return Disease.objects.none()
 
-        qs = Disease.objects.all()
+        qs = Disease.objects.all().order_by("term__label")
         if self.q:
             # looks for a matching string everywhere in term label field
             qs = qs.filter(term__label__icontains=self.q)
@@ -37,7 +37,7 @@ class PhenotypicFeatureTypeAutocomplete(autocomplete.Select2QuerySetView):
         return item.pftype["label"]
 
     def get_queryset(self):
-        qs = PhenotypicFeature.objects.all().distinct("pftype__label")
+        qs = PhenotypicFeature.objects.all().distinct("pftype__label").order_by("pftype__label")
         if self.q:
             # looks for a matching string everywhere in pf type label field
             qs = qs.filter(pftype__label__icontains=self.q)
@@ -56,7 +56,7 @@ class BiosampleSampledTissueAutocomplete(autocomplete.Select2QuerySetView):
         return item.sampled_tissue["label"]
 
     def get_queryset(self):
-        qs = Biosample.objects.all().distinct("sampled_tissue__label")
+        qs = Biosample.objects.all().distinct("sampled_tissue__label").order_by("sampled_tissue__label")
         if self.q:
             # looks for a matching string everywhere in sampled_tissue label field
             qs = qs.filter(sampled_tissue__label__icontains=self.q)
