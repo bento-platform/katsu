ae41f7340ed28f476705fc3192c1628801cafa23
David Lougheed <david.lougheed@gmail.com>
2022-11-05 14:22:47 -0400

    one more

--- chord_metadata_service/chord/ingest/__init__.py
+++ chord_metadata_service/chord/ingest/__init__.py
@@ -1,18 +1,18 @@
 from chord_metadata_service.chord.workflows import metadata as wm
 
-from typing import Callable
-
 from .experiments import ingest_experiments_workflow, ingest_maf_derived_from_vcf_workflow
 from .fhir import ingest_fhir_workflow
 from .mcode import ingest_mcode_fhir_workflow, ingest_mcode_workflow
 from .phenopackets import ingest_phenopacket_workflow
 from .readsets import ingest_readset_workflow
 
+from typing import Callable, Dict
+
 __all__ = [
     "WORKFLOW_INGEST_FUNCTION_MAP",
 ]
 
-WORKFLOW_INGEST_FUNCTION_MAP: dict[str, Callable] = {
+WORKFLOW_INGEST_FUNCTION_MAP: Dict[str, Callable] = {
     wm.WORKFLOW_EXPERIMENTS_JSON: ingest_experiments_workflow,
     wm.WORKFLOW_PHENOPACKETS_JSON: ingest_phenopacket_workflow,
     wm.WORKFLOW_FHIR_JSON: ingest_fhir_workflow,
