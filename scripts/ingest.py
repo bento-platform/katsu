782cf2c9e2b809093ac853b71ecf791ff135e16b
Julian <ornatus30@gmail.com>
2022-11-23 13:14:10 -0500

    string length modified

--- scripts/ingest.py
+++ scripts/ingest.py
@@ -172,10 +172,8 @@ def ingest_phenopackets(katsu_server_url, table_id, phenopackets_json_location):
         sys.exit()
     else:
         print(
-            "Something else went wrong when ingesting phenopackets, possibly due to duplications."
-        )
-        print(
-            "Check phenopackets_json_location config, or remove duplicated individuals from the database and try again."
+            """Something else went wrong when ingesting phenopackets, possibly due to duplications.
+            Check phenopackets_json_location config, or remove duplicated individuals from the database and try again."""
         )
         sys.exit()
 
