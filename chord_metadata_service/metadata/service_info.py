a3a7d5548a29a721416c368a00d598da68d1691e
Julian <ornatus30@gmail.com>
2022-12-02 16:38:22 -0500

    linter: spaces

--- chord_metadata_service/metadata/service_info.py
+++ chord_metadata_service/metadata/service_info.py
@@ -31,3 +31,27 @@ SERVICE_INFO = {
     "contactUrl": "mailto:ksenia.zaytseva@mcgill.ca",
     "version": __version__
 }
+
+
+def service_info_git():
+    info = {
+        **SERVICE_INFO,
+        "environment": "dev",
+    }
+    try:
+        res_tag = subprocess.check_output(["git", "describe", "--tags", "--abbrev=0"])
+        if res_tag:
+            info["git_tag"] = res_tag.decode().rstrip()
+        res_branch = subprocess.check_output(["git", "branch", "--show-current"])
+        if res_branch:
+            info["git_branch"] = res_branch.decode().rstrip()
+
+    except Exception as e:
+        except_name = type(e).__name__
+        print("Error in dev-mode retrieving git information", except_name)
+
+    return info  # updated service info with the git info
+
+
+if DEBUG:
+    SERVICE_INFO = service_info_git()
