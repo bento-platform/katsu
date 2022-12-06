ae9eb050d7da26c4feff86560719006aa9ce2a3a
Son Chau <soncqb@gmail.com>
2022-12-06 11:04:04 -0800

    Create serializers.py
    Add Serializer class for the following models:
            - Program
        - Donor
        - Specimen
        - SampleRegistration
        - PrimaryDiagnosis
        - Treatment
        - Chemotherapy
        - HormoneTherapy
        - Radiation
        - Immunotherapy
        - Surgery
        - FollowUp
        - Biomarker
        - Comorbidity

---
+++ chord_metadata_service/mohpackets/serializers.py
@@ -0,0 +1,115 @@
+from rest_framework import serializers
+from .models import (
+    Program,
+    Donor,
+    Specimen,
+    SampleRegistration,
+    PrimaryDiagnosis,
+    Treatment,
+    Chemotherapy,
+    HormoneTherapy,
+    Radiation,
+    Immunotherapy,
+    Surgery,
+    FollowUp,
+    Biomarker,
+    Comorbidity,
+)
+
+"""
+    This module contains the SERIALIZERS for the models in the mohpackets app.
+    It converting Python objects or Django model instances into a JSON string,
+    and back again for API transmissions.
+    
+    We use the ModelSerializer class, which provides a way to create a Serializer
+    directly from a Django model. All that needs to be done to create a ModelSerializer 
+    is to specify a model on its Meta attribute. For reference, see:
+    https://www.django-rest-framework.org/api-guide/serializers/#modelserializer
+    
+    NOTES: There is no validation in the serializers yet. Currently, it will be 
+    handled by the ingest process. Additional validations can be added here if needed.
+"""
+
+
+class ProgramSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Program
+        fields = "__all__"
+
+
+class DonorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Donor
+        fields = "__all__"
+
+
+class SpecimenSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Specimen
+        fields = "__all__"
+
+
+class SampleRegistrationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SampleRegistration
+        fields = "__all__"
+
+
+class PrimaryDiagnosisSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PrimaryDiagnosis
+        fields = "__all__"
+
+
+class TreatmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Treatment
+        fields = "__all__"
+
+
+class ChemotherapySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Chemotherapy
+        fields = "__all__"
+
+
+class HormoneTherapySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = HormoneTherapy
+        fields = "__all__"
+
+
+class RadiationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Radiation
+        fields = "__all__"
+
+
+class ImmunotherapySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Immunotherapy
+        fields = "__all__"
+
+
+class SurgerySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Surgery
+        fields = "__all__"
+
+
+class FollowUpSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FollowUp
+        fields = "__all__"
+
+
+class BiomarkerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Biomarker
+        fields = "__all__"
+
+
+class ComorbiditySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comorbidity
+        fields = "__all__"
