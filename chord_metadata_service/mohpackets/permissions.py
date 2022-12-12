ebfccc8c5cf32f61dad715a00b271cdade403a10
Son Chau <soncqb@gmail.com>
2022-12-12 14:33:31 -0800

    Update permissions.py
    switch to CanDIGAdminOrReadOnly

--- chord_metadata_service/mohpackets/permissions.py
+++ chord_metadata_service/mohpackets/permissions.py
@@ -8,23 +8,15 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
     can make them a bit more secure by only allow superusers to make changes, and
     non-superusers to only read.
 """
-
-        
-class CanDIGUser(BasePermission):
-    """
-        Allow access to anyone only for the “read-only” methods: GET, HEAD or OPTIONS.
-        Assumed that the user is already authenticated through Tyk since this is 
-        a protected endpoint.
-    """
-    def has_permission(self, request, view):
-        return bool(
-            request.method in SAFE_METHODS 
-        )
-
-class CanDIGAdmin(BasePermission):
+    
+class CanDIGAdminOrReadOnly(BasePermission):
     """
         Allow full access to CanDIG admins. This means that they can make changes.
+        Allow access to anyone only for the “read-only” methods: GET, HEAD or OPTIONS.
     """
     def has_permission(self, request, view):
-        # TODO: How do we check for admin status from the request?
-        return True
+        if request.method in SAFE_METHODS:
+            return True
+        else:
+            # TODO: How do we check for admin status from the request?
+            return True
