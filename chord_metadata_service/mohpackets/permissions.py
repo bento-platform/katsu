840aa609237f11e7c3f568371c584f71022d3749
Son Chau <soncqb@gmail.com>
2022-12-12 16:20:59 -0800

    Update permissions.py

--- chord_metadata_service/mohpackets/permissions.py
+++ chord_metadata_service/mohpackets/permissions.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from rest_framework.permissions import BasePermission, SAFE_METHODS
+from authx.auth import is_site_admin
 
 """
     This module contains custom permission classes for the API.
@@ -8,15 +9,26 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
     can make them a bit more secure by only allow superusers to make changes, and
     non-superusers to only read.
 """
-    
+
+
 class CanDIGAdminOrReadOnly(BasePermission):
     """
-        Allow full access to CanDIG admins. This means that they can make changes.
-        Allow access to anyone only for the “read-only” methods: GET, HEAD or OPTIONS.
+    Allow full access to CanDIG admins. This means that they can make changes.
+    Allow access to anyone only for the “read-only” methods: GET, HEAD or OPTIONS.
     """
+
     def has_permission(self, request, view):
         if request.method in SAFE_METHODS:
             return True
         else:
-            # TODO: How do we check for admin status from the request?
-            return True
+            if settings.CANDIG_AUTHORIZATION == "OPA":
+                opa_url = settings.CANDIG_OPA_URL
+                opa_secret = settings.CANDIG_OPA_SECRET
+                opa_site_admin = settings.CANDIG_OPA_SITE_ADMIN_KEY
+                return is_site_admin(
+                    request,
+                    opa_url=opa_url,
+                    admin_secret=opa_secret,
+                    site_admin_key=opa_site_admin,
+                )
+            return False
