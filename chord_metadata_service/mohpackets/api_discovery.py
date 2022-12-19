f3e147b6e620c64de5245c96d9a62a5b1089a00c
Son Chau <soncqb@gmail.com>
2022-12-19 11:38:58 -0800

    Update api_discovery.py
    add model endpoints discovery

--- chord_metadata_service/mohpackets/api_discovery.py
+++ chord_metadata_service/mohpackets/api_discovery.py
@@ -1,13 +1,9 @@
-from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
 from rest_framework.response import Response
 from django_filters.rest_framework import DjangoFilterBackend
-from django.conf import settings
-from authx.auth import get_opa_datasets, is_site_admin
-from rest_framework import generics, mixins, views
-import requests
+from rest_framework import mixins
+from chord_metadata_service.mohpackets.permissions import CanDIGAdminOrReadOnly
 from chord_metadata_service.mohpackets.filters import (
-    ProgramFilter,
     DonorFilter,
     SpecimenFilter,
     SampleRegistrationFilter,
@@ -22,10 +18,7 @@ from chord_metadata_service.mohpackets.filters import (
     BiomarkerFilter,
     ComorbidityFilter,
 )
-from chord_metadata_service.mohpackets.permissions import CanDIGAdminOrReadOnly
-
 from chord_metadata_service.mohpackets.serializers import (
-    ProgramSerializer,
     DonorSerializer,
     SpecimenSerializer,
     SampleRegistrationSerializer,
@@ -41,7 +34,6 @@ from chord_metadata_service.mohpackets.serializers import (
     ComorbiditySerializer,
 )
 from chord_metadata_service.mohpackets.models import (
-    Program,
     Donor,
     Specimen,
     SampleRegistration,
@@ -57,32 +49,178 @@ from chord_metadata_service.mohpackets.models import (
     Comorbidity,
 )
 
+"""
+    This Views module based on the api_views.py module, but only contains
+    the ListModelMixin, meaning the user cannot use any create, update,
+    or view details APIs.
+    It overrides the list function to return the count of patients for discovery purposes.
+    NOTE: This is a temporary solution until we implement Beacon discovery features.
+"""
+
+##########################################
+#                                        #
+#           HELPER FUNCTIONS             #
+#                                        #
+##########################################
+
+
+def get_discovery_response(self):
+    """
+    This function returns the count of unique submitter_donor_ids
+    (aka the number of patients the queryset has).
+    """
+    queryset = self.filter_queryset(self.get_queryset())
+    count = queryset.values_list("submitter_donor_id").distinct().count()
+    return Response({"discovery_count": count})
+
+
 ###############################################
 #                                             #
-#           DISCOVERY API ENDPOINTS           #
+#           DISCOVERY API VIEWS               #
 #                                             #
 ###############################################
 
-class DonorDiscoverViewSet(mixins.ListModelMixin, viewsets.GenericViewSet): 
+
+class DiscoveryDonorViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
     serializer_class = DonorSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_class = DonorFilter
     permission_classes = [CanDIGAdminOrReadOnly]
     queryset = Donor.objects.all()
-    
-    def list(self, request, *args, **kwargs):
-        queryset = self.filter_queryset(self.get_queryset())
-        count = queryset.values_list('submitter_donor_id').distinct().count()
-        return Response({"number of patients":count})
 
-class SpecimenDiscoveryViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+    def list(self, request, *args, **kwargs):
+        return get_discovery_response(self)
+
+
+class DiscoverySpecimenViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
     serializer_class = SpecimenSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_class = SpecimenFilter
     permission_classes = [CanDIGAdminOrReadOnly]
     queryset = Specimen.objects.all()
+
+    def list(self, request, *args, **kwargs):
+        return get_discovery_response(self)
+
+
+class DiscoverySampleRegistrationViewSet(
+    mixins.ListModelMixin, viewsets.GenericViewSet
+):
+    serializer_class = SampleRegistrationSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = SampleRegistrationFilter
+    permission_classes = [CanDIGAdminOrReadOnly]
+    queryset = SampleRegistration.objects.all()
     
     def list(self, request, *args, **kwargs):
-        queryset = self.filter_queryset(self.get_queryset())
-        count = queryset.values_list('submitter_donor_id').distinct().count()
-        return Response({"number of patients":count})
+        return get_discovery_response(self)
+
+
+class DiscoveryPrimaryDiagnosisViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+    serializer_class = PrimaryDiagnosisSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = PrimaryDiagnosisFilter
+    permission_classes = [CanDIGAdminOrReadOnly]
+    queryset = PrimaryDiagnosis.objects.all()
+    
+    def list(self, request, *args, **kwargs):
+        return get_discovery_response(self)
+
+
+class DiscoveryTreatmentViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+    serializer_class = TreatmentSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = TreatmentFilter
+    permission_classes = [CanDIGAdminOrReadOnly]
+    queryset = Treatment.objects.all()
+    
+    def list(self, request, *args, **kwargs):
+        return get_discovery_response(self)
+
+
+class DiscoveryChemotherapyViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+    serializer_class = ChemotherapySerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = ChemotherapyFilter
+    permission_classes = [CanDIGAdminOrReadOnly]
+    queryset = Chemotherapy.objects.all()
+    
+    def list(self, request, *args, **kwargs):
+        return get_discovery_response(self)
+
+
+class DiscoveryHormoneTherapyViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+    serializer_class = HormoneTherapySerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = HormoneTherapyFilter
+    permission_classes = [CanDIGAdminOrReadOnly]
+    queryset = HormoneTherapy.objects.all()
+    
+    def list(self, request, *args, **kwargs):
+        return get_discovery_response(self)
+
+
+class DiscoveryRadiationViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+    serializer_class = RadiationSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = RadiationFilter
+    permission_classes = [CanDIGAdminOrReadOnly]
+    queryset = Radiation.objects.all()
+    
+    def list(self, request, *args, **kwargs):
+        return get_discovery_response(self)
+
+
+class DiscoveryImmunotherapyViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+    serializer_class = ImmunotherapySerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = ImmunotherapyFilter
+    permission_classes = [CanDIGAdminOrReadOnly]
+    queryset = Immunotherapy.objects.all()
+    
+    def list(self, request, *args, **kwargs):
+        return get_discovery_response(self)
+
+
+class DiscoverySurgeryViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+    serializer_class = SurgerySerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = SurgeryFilter
+    permission_classes = [CanDIGAdminOrReadOnly]
+    queryset = Surgery.objects.all()
+    
+    def list(self, request, *args, **kwargs):
+        return get_discovery_response(self)
+
+
+class DiscoveryFollowUpViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+    serializer_class = FollowUpSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = FollowUpFilter
+    permission_classes = [CanDIGAdminOrReadOnly]
+    queryset = FollowUp.objects.all()
+    
+    def list(self, request, *args, **kwargs):
+        return get_discovery_response(self)
+
+
+class DiscoveryBiomarkerViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+    serializer_class = BiomarkerSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = BiomarkerFilter
+    permission_classes = [CanDIGAdminOrReadOnly]
+    queryset = Biomarker.objects.all()
+    
+    def list(self, request, *args, **kwargs):
+        return get_discovery_response(self)
+
+
+class DiscoveryComorbidityViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+    serializer_class = ComorbiditySerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = ComorbidityFilter
+    permission_classes = [CanDIGAdminOrReadOnly]
+    queryset = Comorbidity.objects.all()
+    
+    def list(self, request, *args, **kwargs):
+        return get_discovery_response(self)
