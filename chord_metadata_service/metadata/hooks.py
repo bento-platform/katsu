aa598a045b0eb0a493732ccb33a9d8395231d315
Son Chau <soncqb@gmail.com>
2022-12-19 11:37:32 -0800

    Update hooks.py
    add description

--- chord_metadata_service/metadata/hooks.py
+++ chord_metadata_service/metadata/hooks.py
@@ -1,4 +1,8 @@
 def preprocessing_filter_path(endpoints):
+    """
+        This function used by drf_spectacular to filter out unwanted endpoints.
+        For reference, see https://drf-spectacular.readthedocs.io/en/latest/customization.html?highlight=hook#step-7-preprocessing-hooks
+    """
     filtered = []
     for (path, path_regex, method, callback) in endpoints:
         # Remove all but DRF API endpoints
