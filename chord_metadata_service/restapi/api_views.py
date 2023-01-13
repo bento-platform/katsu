389f344d03861765481b072b493eff98b44e3a28
Son Chau <soncqb@gmail.com>
2023-01-13 14:48:43 -0800

    Update api_views.py

--- chord_metadata_service/restapi/api_views.py
+++ chord_metadata_service/restapi/api_views.py
@@ -3,20 +3,18 @@ import logging
 from collections import Counter
 
 from django.conf import settings
-from django.views.decorators.cache import cache_page
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 
 from chord_metadata_service.restapi.utils import (
     get_field_options,
-    parse_individual_age,
     stats_for_field,
     compute_binned_ages,
     get_field_bins,
     get_categorical_stats,
     get_date_stats,
-    get_range_stats
+    get_range_stats,
 )
 from chord_metadata_service.chord.permissions import OverrideOrSuperUserOnly
 from chord_metadata_service.metadata.service_info import SERVICE_INFO
@@ -50,13 +48,13 @@ def service_info(_request):
     description="Overview of all Phenopackets in the database",
     responses={
         200: inline_serializer(
-            name='overview_response',
+            name="overview_response",
             fields={
-                'phenopackets': serializers.IntegerField(),
-                'data_type_specific': serializers.JSONField(),
-            }
+                "phenopackets": serializers.IntegerField(),
+                "data_type_specific": serializers.JSONField(),
+            },
         )
-    }
+    },
 )
 @api_view(["GET"])
 @permission_classes([OverrideOrSuperUserOnly])
@@ -71,7 +69,9 @@ def overview(_request):
     experiments_count = experiments_models.Experiment.objects.all().count()
     experiment_results_count = experiments_models.ExperimentResult.objects.all().count()
     instruments_count = experiments_models.Instrument.objects.all().count()
-    phenotypic_features_count = pheno_models.PhenotypicFeature.objects.all().distinct('pftype').count()
+    phenotypic_features_count = (
+        pheno_models.PhenotypicFeature.objects.all().distinct("pftype").count()
+    )
 
     # Sex related fields stats are precomputed here and post processed later
     # to include missing values inferred from the schema
@@ -84,12 +84,16 @@ def overview(_request):
     # age_numeric is computed at ingestion time of phenopackets. On some instances
     # it might be unavailable and as a fallback must be computed from the age JSON field which
     # has two alternate formats (hence more complex and slower to process)
-    individuals_age = get_field_bins(patients_models.Individual, "age_numeric", OVERVIEW_AGE_BIN_SIZE)
+    individuals_age = get_field_bins(
+        patients_models.Individual, "age_numeric", OVERVIEW_AGE_BIN_SIZE
+    )
     if None in individuals_age:  # fallback
         del individuals_age[None]
         individuals_age = Counter(individuals_age)
         individuals_age.update(
-            compute_binned_ages(OVERVIEW_AGE_BIN_SIZE)   # single update instead of creating iterables in a loop
+            compute_binned_ages(
+                OVERVIEW_AGE_BIN_SIZE
+            )  # single update instead of creating iterables in a loop
         )
 
     r = {
@@ -98,51 +102,83 @@ def overview(_request):
             "biosamples": {
                 "count": biosamples_count,
                 "taxonomy": stats_for_field(pheno_models.Biosample, "taxonomy__label"),
-                "sampled_tissue": stats_for_field(pheno_models.Biosample, "sampled_tissue__label"),
+                "sampled_tissue": stats_for_field(
+                    pheno_models.Biosample, "sampled_tissue__label"
+                ),
             },
             "diseases": {
                 # count is a number of unique disease terms (not all diseases in the database)
                 "count": diseases_count,
-                "term": diseases_stats
+                "term": diseases_stats,
             },
             "individuals": {
                 "count": individuals_count,
-                "sex": {k: individuals_sex.get(k, 0) for k in (s[0] for s in pheno_models.Individual.SEX)},
-                "karyotypic_sex": {
-                    k: individuals_k_sex.get(k, 0) for k in (s[0] for s in pheno_models.Individual.KARYOTYPIC_SEX)
+                "sex": {
+                    k: individuals_sex.get(k, 0)
+                    for k in (s[0] for s in pheno_models.Individual.SEX)
                 },
-                "taxonomy": stats_for_field(patients_models.Individual, "taxonomy__label"),
+                "karyotypic_sex": {
+                    k: individuals_k_sex.get(k, 0)
+                    for k in (s[0] for s in pheno_models.Individual.KARYOTYPIC_SEX)
+                },
+                "taxonomy": stats_for_field(
+                    patients_models.Individual, "taxonomy__label"
+                ),
                 "age": individuals_age,
                 "ethnicity": stats_for_field(patients_models.Individual, "ethnicity"),
             },
             "phenotypic_features": {
                 # count is a number of unique phenotypic feature types (not all pfs in the database)
                 "count": phenotypic_features_count,
-                "type": stats_for_field(pheno_models.PhenotypicFeature, "pftype__label")
+                "type": stats_for_field(
+                    pheno_models.PhenotypicFeature, "pftype__label"
+                ),
             },
             "experiments": {
                 "count": experiments_count,
-                "study_type": stats_for_field(experiments_models.Experiment, "study_type"),
-                "experiment_type": stats_for_field(experiments_models.Experiment, "experiment_type"),
+                "study_type": stats_for_field(
+                    experiments_models.Experiment, "study_type"
+                ),
+                "experiment_type": stats_for_field(
+                    experiments_models.Experiment, "experiment_type"
+                ),
                 "molecule": stats_for_field(experiments_models.Experiment, "molecule"),
-                "library_strategy": stats_for_field(experiments_models.Experiment, "library_strategy"),
-                "library_source": stats_for_field(experiments_models.Experiment, "library_source"),
-                "library_selection": stats_for_field(experiments_models.Experiment, "library_selection"),
-                "library_layout": stats_for_field(experiments_models.Experiment, "library_layout"),
-                "extraction_protocol": stats_for_field(experiments_models.Experiment, "extraction_protocol"),
+                "library_strategy": stats_for_field(
+                    experiments_models.Experiment, "library_strategy"
+                ),
+                "library_source": stats_for_field(
+                    experiments_models.Experiment, "library_source"
+                ),
+                "library_selection": stats_for_field(
+                    experiments_models.Experiment, "library_selection"
+                ),
+                "library_layout": stats_for_field(
+                    experiments_models.Experiment, "library_layout"
+                ),
+                "extraction_protocol": stats_for_field(
+                    experiments_models.Experiment, "extraction_protocol"
+                ),
             },
             "experiment_results": {
                 "count": experiment_results_count,
-                "file_format": stats_for_field(experiments_models.ExperimentResult, "file_format"),
-                "data_output_type": stats_for_field(experiments_models.ExperimentResult, "data_output_type"),
-                "usage": stats_for_field(experiments_models.ExperimentResult, "usage")
+                "file_format": stats_for_field(
+                    experiments_models.ExperimentResult, "file_format"
+                ),
+                "data_output_type": stats_for_field(
+                    experiments_models.ExperimentResult, "data_output_type"
+                ),
+                "usage": stats_for_field(experiments_models.ExperimentResult, "usage"),
             },
             "instruments": {
                 "count": instruments_count,
-                "platform": stats_for_field(experiments_models.Experiment, "instrument__platform"),
-                "model": stats_for_field(experiments_models.Experiment, "instrument__model")
+                "platform": stats_for_field(
+                    experiments_models.Experiment, "instrument__platform"
+                ),
+                "model": stats_for_field(
+                    experiments_models.Experiment, "instrument__model"
+                ),
             },
-        }
+        },
     }
 
     return Response(r)
@@ -152,12 +188,12 @@ def overview(_request):
     description="Public search fields with their configuration",
     responses={
         200: inline_serializer(
-            name='public_search_fields_response',
+            name="public_search_fields_response",
             fields={
-                'sections': serializers.JSONField(),
-            }
+                "sections": serializers.JSONField(),
+            },
         )
-    }
+    },
 )
 @api_view(["GET"])
 @permission_classes([AllowAny])
@@ -181,10 +217,12 @@ def public_search_fields(_request):
                     {
                         **field_conf[f],
                         "id": f,
-                        "options": get_field_options(field_conf[f])
-                    } for f in section["fields"]
-                ]
-            } for section in search_conf
+                        "options": get_field_options(field_conf[f]),
+                    }
+                    for f in section["fields"]
+                ],
+            }
+            for section in search_conf
         ]
     }
     return Response(r)
@@ -194,12 +232,12 @@ def public_search_fields(_request):
     description="Overview of all public data in the database",
     responses={
         200: inline_serializer(
-            name='public_overview_response',
+            name="public_overview_response",
             fields={
-                'datasets': serializers.CharField(),
-            }
+                "datasets": serializers.CharField(),
+            },
         )
-    }
+    },
 )
 @api_view(["GET"])
 @permission_classes([AllowAny])
@@ -222,28 +260,41 @@ def public_overview(_request):
 
     # Datasets provenance metadata
     datasets = chord_models.Dataset.objects.values(
-        "title", "description", "contact_info",
-        "dates", "stored_in", "spatial_coverage",
-        "types", "privacy", "distributions",
-        "dimensions", "primary_publications", "citations",
-        "produced_by", "creators", "licenses",
-        "acknowledges", "keywords", "version",
-        "extra_properties"
+        "title",
+        "description",
+        "contact_info",
+        "dates",
+        "stored_in",
+        "spatial_coverage",
+        "types",
+        "privacy",
+        "distributions",
+        "dimensions",
+        "primary_publications",
+        "citations",
+        "produced_by",
+        "creators",
+        "licenses",
+        "acknowledges",
+        "keywords",
+        "version",
+        "extra_properties",
     )
 
     response = {
         "datasets": datasets,
         "layout": settings.CONFIG_PUBLIC["overview"],
         "fields": {},
-        "counts": {
-            "individuals": individuals_count,
-            "experiments": experiments_count
-        },
+        "counts": {"individuals": individuals_count, "experiments": experiments_count},
     }
 
     # Parse the public config to gather data for each field defined in the
     # overview
-    fields = [chart["field"] for section in settings.CONFIG_PUBLIC["overview"] for chart in section["charts"]]
+    fields = [
+        chart["field"]
+        for section in settings.CONFIG_PUBLIC["overview"]
+        for chart in section["charts"]
+    ]
 
     for field in fields:
         field_props = settings.CONFIG_PUBLIC["fields"][field]
@@ -254,11 +305,7 @@ def public_overview(_request):
         elif field_props["datatype"] == "date":
             stats = get_date_stats(field_props)
 
-        response["fields"][field] = {
-            **field_props,
-            "id": field,
-            "data": stats
-        }
+        response["fields"][field] = {**field_props, "id": field, "data": stats}
 
     return Response(response)
 
@@ -267,17 +314,25 @@ def public_overview(_request):
 @permission_classes([AllowAny])
 def moh_overview(_request):
     """
-        Return a summary of the statistics for the database:
-        - cohort_count: number of datasets
-        - individual_count: number of individuals
-        - ethnicity: the count of each ethenicity
-        - gender: the count of each gender
+    Return a summary of the statistics for the database:
+    - cohort_count: number of datasets
+    - individual_count: number of individuals
+    - ethnicity: the count of each ethenicity
+    - gender: the count of each gender
     """
-    return Response({
-        "cohort_count": chord_models.Dataset.objects.all().count(),
-        "individual_count": patients_models.Individual.objects.count(),
-        "ethnicity": list(patients_models.Individual.objects.values('ethnicity').annotate(
-            count=Count('ethnicity')).order_by()),
-        "gender": list(patients_models.Individual.objects.values('sex').annotate(
-            count=Count('sex')).order_by()),
-    })
+    return Response(
+        {
+            "cohort_count": chord_models.Dataset.objects.all().count(),
+            "individual_count": patients_models.Individual.objects.count(),
+            "ethnicity": list(
+                patients_models.Individual.objects.values("ethnicity")
+                .annotate(count=Count("ethnicity"))
+                .order_by()
+            ),
+            "gender": list(
+                patients_models.Individual.objects.values("sex")
+                .annotate(count=Count("sex"))
+                .order_by()
+            ),
+        }
+    )
