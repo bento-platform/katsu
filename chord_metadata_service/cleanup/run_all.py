bd96926fc46556390929d636c8af3d0629a145e4
David Lougheed <david.lougheed@gmail.com>
2023-01-19 22:37:05 -0500

    feat: resources cleanup

--- chord_metadata_service/cleanup/run_all.py
+++ chord_metadata_service/cleanup/run_all.py
@@ -1,6 +1,7 @@
 from chord_metadata_service.experiments.cleanup import clean_experiment_results, clean_instruments
 from chord_metadata_service.patients.cleanup import clean_individuals
 from chord_metadata_service.phenopackets.cleanup import clean_biosamples, clean_phenotypic_features, clean_procedures
+from chord_metadata_service.resources.cleanup import clean_resources
 
 __all__ = [
     "run_all_cleanup",
@@ -8,7 +9,8 @@ __all__ = [
 
 
 def run_all_cleanup() -> int:
-    # Specific order: biosamples, then experiment artifacts (results/instruments), then patients
+    # Specific order: biosamples, then experiment artifacts (results/instruments), then patients,
+    # then resources (where order is less important)
 
     n_removed: int = 0
 
@@ -23,6 +25,9 @@ def run_all_cleanup() -> int:
 
     # Patients
     n_removed += clean_individuals()
+
+    # Resources
+    n_removed += clean_resources()
 
     # Return final removed object count
     return n_removed
--- chord_metadata_service/resources/cleanup.py
+++ chord_metadata_service/resources/cleanup.py
@@ -1,8 +1,37 @@
 import chord_metadata_service.chord.models as cm
 import chord_metadata_service.phenopackets.models as pm
+from chord_metadata_service.resources.models import Resource
+
+from chord_metadata_service.logger import logger
+from chord_metadata_service.utils import dict_first_val
+
+__all__ = [
+    "clean_resources",
+]
 
 
-def clean_resources():
-    # TODO
-    cm.Dataset.objects.values("additional_resources__id")
-    pm.MetaData.objects.values("resources__id")
+def clean_resources() -> int:
+    """
+    Removes any resources not referenced by any datasets/phenopackets.
+    """
+
+    resources_referenced = set()
+
+    resources_referenced.update(map(dict_first_val, cm.Dataset.objects.values("additional_resources__id")))
+    resources_referenced.update(map(dict_first_val, pm.MetaData.objects.values("resources__id")))
+
+    # Remove null from set
+    resources_referenced.discard(None)
+
+    # Remove resources NOT in set
+    resources_to_remove = set(
+        map(dict_first_val, Resource.objects.exclude(id__in=resources_referenced).values("id")))
+    n_to_remove = len(resources_to_remove)
+
+    if n_to_remove:
+        logger.info(f"Automatically cleaning up {n_to_remove} resources: {str(resources_to_remove)}")
+        Resource.objects.filter(id__in=resources_to_remove).delete()
+    else:
+        logger.info("No resources set for auto-removal")
+
+    return n_to_remove
