5b36a90caa375a7b7eabea639a1d95e6d5393b39
yavyx <javiercastilloar@gmail.com>
2023-01-20 15:39:19 -0800

    created program, donor and specimen validators

--- chord_metadata_service/mohpackets/serializers.py
+++ chord_metadata_service/mohpackets/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from chord_metadata_service.mohpackets.validators import ID_VALIDATOR
+import chord_metadata_service.mohpackets.validators as val
 
 from .models import (
     Program,
@@ -37,8 +37,11 @@ from .models import (
 
 class ProgramSerializer(serializers.ModelSerializer):
     program_id = serializers.PrimaryKeyRelatedField(
-        queryset=Program.objects.all(), validators=[ID_VALIDATOR]
+        queryset=Program.objects.all(), validators=[val.ID]
     )
+    name=serializers.CharField(validators=[val.ID])
+    created = serializers.DateTimeField(validators=[val.DATE]) # TODO: ask Son
+    updated = serializers.DateTimeField(validators=[val.DATE]) # TODO: ask Son
 
     class Meta:
         model = Program
@@ -46,12 +49,54 @@ class ProgramSerializer(serializers.ModelSerializer):
 
 
 class DonorSerializer(serializers.ModelSerializer):
+    submitter_donor_id = serializers.PrimaryKeyRelatedField( # TODO: ask Son
+        queryset=Donor.objects.all(), validators=[val.ID]
+    )
+    is_deceased = serializers.BooleanField(validators=[val.BOOLEAN])
+    cause_of_death = serializers.CharField(validators=[val.CAUSE_OF_DEATH])
+    date_of_birth = serializers.CharField(validators=[val.DATE])
+    date_of_death = serializers.CharField(validators=[val.DATE])
+    primary_site = serializers.CharField(validators=[val.PRIMARY_SITE])
+    
     class Meta:
         model = Donor
         fields = "__all__"
 
 
 class SpecimenSerializer(serializers.ModelSerializer):
+    submitter_specimen_id = serializers.PrimaryKeyRelatedField( # TODO: ask Son
+        queryset=Specimen.objects.all(), validators=[val.ID]
+    )
+    pathological_tumour_staging_system = serializers.CharField(
+        validators=[val.TUMOUR_STAGING_SYSTEM]
+    )
+    pathological_t_category = serializers.CharField(validators=[val.T_CATEGORY])
+    pathological_n_category = serializers.CharField(validators=[val.N_CATEGORY])
+    pathological_m_category = serializers.CharField(validators=[val.M_CATEGORY])
+    pathological_stage_group = serializers.CharField(validators=[val.STAGE_GROUP])
+    specimen_collection_date = serializers.CharField(validators=[val.DATE])
+    specimen_storage = serializers.CharField(validators=[val.STORAGE])
+    # tumour_histological_type = serializers.CharField(validators=[val.MORPHOLOGY]) # TODO: Write validator
+    specimen_anatomic_location = serializers.CharField(validators=[val.TOPOGRAPHY])
+    reference_pathology_confirmed_diagnosis = serializers.CharField(
+        validators=[val.CONFIRMED_DIAGNOSIS_TUMOUR]
+    )
+    reference_pathology_confirmed_tumour_presence = serializers.CharField(
+        validators=[val.CONFIRMED_DIAGNOSIS_TUMOUR]
+    )
+    tumour_grading_system = serializers.CharField(
+        validators=[val.TUMOUR_GRADING_SYSTEM]
+    )
+    tumour_grade = serializers.CharField(
+        validators=[val.TUMOUR_GRADE]
+    )
+    percent_tumour_cells_range = serializers.CharField(
+        validators=[val.PERCENT_CELLS_RANGE]
+    )
+    percent_tumour_cells_measurement_method = serializers.CharField(
+        validators=[val.CELLS_MEASUREMENT_METHOD]
+    )
+
     class Meta:
         model = Specimen
         fields = "__all__"
--- chord_metadata_service/mohpackets/validators.py
+++ chord_metadata_service/mohpackets/validators.py
@@ -3,9 +3,64 @@ from django.core.validators import (
     RegexValidator,
 )
 
+from rest_framework.validators import ValidationError
+
+import chord_metadata_service.mohpackets.permissible_values as v
+
+
+class ChoicesValidator:
+    """Creates a validator that checks permissible values (choices)."""
+    def __init__(self, choices):
+        self.choices = choices
+
+    def __call__(self, value):
+        multiple = False
+        if "|" in value: # Check for multiple entries
+            value = value.split("|")
+            multiple = True
+        else:
+            value = [value]
+    
+        for v in value:
+            if v not in self.choices:
+                message = f"All values must be one of {self.choices}" if multiple \
+                else f"The value must be one of {self.choices}"
+                
+                raise ValidationError(message)
+
+# Boolean format
+# A true/false field (Yes/No in MoH model)
+BOOLEAN = ChoicesValidator(choices=["Yes", "No"])
+
+CAUSE_OF_DEATH = ChoicesValidator(choices=v.CAUSE_OF_DEATH)
+
+PRIMARY_SITE = ChoicesValidator(choices=v.PRIMARY_SITE)
+
+TUMOUR_STAGING_SYSTEM = ChoicesValidator(choices=v.TUMOUR_STAGING_SYSTEM)
+
+T_CATEGORY = ChoicesValidator(v.T_CATEGORY)
+
+N_CATEGORY = ChoicesValidator(v.N_CATEGORY)
+
+M_CATEGORY = ChoicesValidator(v.M_CATEGORY)
+
+STAGE_GROUP = ChoicesValidator(v.STAGE_GROUP)
+
+STORAGE = ChoicesValidator(v.STORAGE)
+
+CONFIRMED_DIAGNOSIS_TUMOUR = ChoicesValidator(v.CONFIRMED_DIAGNOSIS_TUMOUR)
+
+TUMOUR_GRADING_SYSTEM = ChoicesValidator(v.TUMOUR_GRADING_SYSTEM)
+
+TUMOUR_GRADE = ChoicesValidator(v.TUMOUR_GRADE)
+
+PERCENT_CELLS_RANGE = ChoicesValidator(v.PERCENT_CELLS_RANGE)
+
+CELLS_MEASUREMENT_METHOD = ChoicesValidator(v.CELLS_MEASURE_METHOD)
+
 # ID format.
 # Examples: 90234, BLD_donor_89, AML-90
-ID_VALIDATOR = RegexValidator(
+ID = RegexValidator(
     regex=r"^[A-Za-z0-9\-\._]{1,64}",
     message=_("The value does not fit the pattern [A-Za-z0-9\-\._]{1,64}"), # noqa
     code="invalid",
@@ -15,7 +70,7 @@ ID_VALIDATOR = RegexValidator(
 # A date, or partial date (e.g. just year or year + month) as used in
 # human communication. The format is YYYY, YYYY-MM, or YYYY-MM-DD,
 # e.g. 2018, 1973-06, or 1905-08-23. There SHALL be no time zone.
-DATE_VALIDATOR = RegexValidator(
+DATE = RegexValidator(
     regex=r"^([0-9]([0-9]([0-9][1-9]|[1-9]0)|[1-9]00)|[1-9]000)(-(0[1-9]|1[0-2])(-(0[1-9]|[1-2][0-9]|3[0-1]))?)?",
     message=_("The value does not fit the pattern YYYY, YYYY-MM, or YYYY-MM-DD"),
     code="invalid",
@@ -23,7 +78,7 @@ DATE_VALIDATOR = RegexValidator(
 
 # ICD-O-3 topography codes
 # Examples: C50.1, C18
-TOPOGRAPHY_VALIDATOR = RegexValidator(
+TOPOGRAPHY = RegexValidator(
     regex=r"^[8,9]{1}[0-9]{3}/[0,1,2,3,6,9]{1}[1-9]{0,1}$",
     message=_(
         "The value does not fit the pattern [8,9]{1}[0-9]{3}/[0,1,2,3,6,9]{1}[1-9]{0,1}"
