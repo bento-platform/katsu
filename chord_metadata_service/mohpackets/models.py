70cad1e6fc2127b3be86bdf081ff4dee5f79ce76
Son Chau <soncqb@gmail.com>
2023-02-02 14:59:08 -0800

    model change one-to-one
    Radiation
    Surgery

--- chord_metadata_service/mohpackets/models.py
+++ chord_metadata_service/mohpackets/models.py
@@ -254,7 +254,7 @@ class Radiation(models.Model):
     submitter_donor_id = models.ForeignKey(
         Donor, on_delete=models.CASCADE, null=False, blank=False
     )
-    submitter_treatment_id = models.ForeignKey(Treatment, on_delete=models.CASCADE)
+    submitter_treatment_id = models.OneToOneField(Treatment, on_delete=models.CASCADE)
     radiation_therapy_modality = models.CharField(
         max_length=255, null=False, blank=False
     )
@@ -304,10 +304,10 @@ class Surgery(models.Model):
     submitter_donor_id = models.ForeignKey(
         Donor, on_delete=models.CASCADE, null=False, blank=False
     )
-    submitter_specimen_id = models.ForeignKey(
-        Specimen, on_delete=models.CASCADE, null=False, blank=False
+    submitter_specimen_id = models.OneToOneField(
+        Specimen, on_delete=models.CASCADE, null=True, blank=True
     )
-    submitter_treatment_id = models.ForeignKey(Treatment, on_delete=models.CASCADE)
+    submitter_treatment_id = models.OneToOneField(Treatment, on_delete=models.CASCADE)
     surgery_type = models.CharField(max_length=255, null=False, blank=False)
     surgery_site = models.CharField(max_length=255, blank=True, default="")
     surgery_location = models.CharField(max_length=128, blank=True, default="")
