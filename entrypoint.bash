bf94d6ca14797ba09dcfe8205d3076018ca3d892
David Lougheed <david.lougheed@gmail.com>
2023-02-22 14:53:43 -0500

    fix: permissions and tmpdir issues

--- entrypoint.bash
+++ entrypoint.bash
@@ -5,8 +5,11 @@ cd /app || exit
 # Create bento_user + home
 source /create_service_user.bash
 
-# Fix permissions on /app - for developing or /app/tmp writing
-chown -R bento_user:bento_user /app
+# Create /app/tmp if it doesn't exist (say, in the local mount of the code)
+mkdir -p /app/tmp
+
+# Fix permissions on /app/tmp and /env
+chown -R bento_user:bento_user /app/tmp /env
 chmod -R o-rwx /app/tmp  # Remove all access from others
 
 # Drop into bento_user from root and execute the CMD specified for the image
