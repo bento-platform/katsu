9ba4f505ad0c95696a9dd0dc15fcb5479dea0ed7
Son Chau <soncqb@gmail.com>
2023-02-22 16:41:12 -0800

    Create hooks.py

---
+++ config/hooks.py
@@ -0,0 +1,12 @@
+def preprocessing_filter_path(endpoints):
+    """
+    This function used by drf_spectacular to filter out unwanted endpoints.
+    For reference, see
+    https://drf-spectacular.readthedocs.io/en/latest/customization.html?highlight=hook#step-7-preprocessing-hooks
+    """
+    filtered = []
+    for (path, path_regex, method, callback) in endpoints:
+        # only include endpoints that start with discovery and model
+        if path.startswith("/moh/v1/discovery") or path.startswith("/moh/v1/model"):
+            filtered.append((path, path_regex, method, callback))
+    return filtered
