8cf50bc8fc0b130332158f63e779c0832eb34fa8
Son Chau <soncqb@gmail.com>
2023-03-13 15:24:57 -0700

    Update manage.py

--- manage.py
+++ manage.py
@@ -11,7 +11,7 @@ def main():
     # )
 
     # CANDIG settings
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
 
     try:
         from django.core.management import execute_from_command_line
