60e3e8f444a4ab76b27642207824f8dec7ab5866
Son Chau <soncqb@gmail.com>
2023-03-17 13:39:37 -0700

    Update base.py

--- config/settings/base.py
+++ config/settings/base.py
@@ -169,11 +169,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # ------------
 
 REST_FRAMEWORK = {
-    "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest_framework.authentication.BasicAuthentication",
-        "rest_framework.authentication.SessionAuthentication",
-        "rest_framework.authentication.TokenAuthentication",
-    ],
+    "DEFAULT_AUTHENTICATION_CLASSES": [],
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticatedOrReadOnly"
     ],
