d11ff27d0881b7441be983a3499e37d38e9a5714
Son Chau <soncqb@gmail.com>
2023-03-20 11:35:11 -0700

    Update authentication.py

--- chord_metadata_service/mohpackets/authentication.py
+++ chord_metadata_service/mohpackets/authentication.py
@@ -15,37 +15,27 @@ class TokenAuthentication(BaseAuthentication):
     def authenticate(self, request):
         auth = get_authorization_header(request).split()
         if not auth:
-            raise exceptions.AuthenticationFailed("Authorization required")
+            raise AuthenticationFailed("Authorization required")
         else:
-            # opa_url = settings.CANDIG_OPA_URL
             opa_secret = settings.CANDIG_OPA_SECRET
-            # opa_url = "http://docker.localhost:8181"
-            # opa_secret = "IWyF4ST38829tuyxLYyYQ"
-            # Call the get_opa_datasets function to get the authorized datasets
             try:
-                # authorized_datasets = get_opa_datasets(
-                #     request, opa_url=opa_url, admin_secret=opa_secret
-                # )
-                # todo: see why this didn't work
                 authorized_datasets = get_opa_datasets(request, admin_secret=opa_secret)
             except Exception as e:
                 logger.exception(
                     f"An error occurred in OPA get_authorized_datasets: {e}"
                 )
-                raise AuthenticationFailed(
-                    f"Error retrieving authorized datasets from OPA."
-                )
+                raise AuthenticationFailed(f"Error retrieving datasets from OPA.")
 
             # Check if the user is authorized to access any datasets.
             # By default, user has 2 or 5 datasets, so it has to be more than 5.
             if len(authorized_datasets) < 6:
-                # todo: put this in the log
                 logger.exception(
                     f"Retrieved {authorized_datasets}. "
-                    "User should have access to additional datasets. "
-                    "Either token is expired or user is not authorized."
+                    "Either token is expired or user doesn't have any datasets added"
                 )
-                raise Exception(f"User is not authorized to access any datasets. ")
+                raise AuthenticationFailed(
+                    f"User is not authorized to access any datasets."
+                )
             else:
                 # add dataset to request
                 request.authorized_datasets = authorized_datasets
