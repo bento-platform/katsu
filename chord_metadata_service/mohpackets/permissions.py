0aa4c44127b4f2dad1aa1cfb152b7aac7fccb345
Son Chau <soncqb@gmail.com>
2023-03-20 16:19:23 -0700

    Update permissions.py

--- chord_metadata_service/mohpackets/permissions.py
+++ chord_metadata_service/mohpackets/permissions.py
@@ -1,9 +1,11 @@
+import logging
 import os
 
 from authx.auth import is_site_admin
 from django.conf import settings
 from rest_framework.permissions import SAFE_METHODS, BasePermission
 
+logger = logging.getLogger(__name__)
 """
     This module contains custom permission classes for the API.
     By default, only allow superusers (site_admin) to make changes, and
@@ -28,8 +30,13 @@ class CanDIGAdminOrReadOnly(BasePermission):
             settings_module = os.environ.get("DJANGO_SETTINGS_MODULE")
             if "dev" in settings_module or "prod" in settings_module:
                 opa_secret = settings.CANDIG_OPA_SECRET
-                return is_site_admin(
-                    request,
-                    admin_secret=opa_secret,
-                )
+                try:
+                    is_admin = is_site_admin(
+                        request,
+                        admin_secret=opa_secret,
+                    )
+                    return is_admin
+                except Exception as e:
+                    logger.exception(f"An error occurred in OPA is_site_admin: {e}")
+                    raise Exception("Error checking roles from OPA.")
             return True
