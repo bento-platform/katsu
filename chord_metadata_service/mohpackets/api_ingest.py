fb175854f4de15a36b9e8e47d9ee52a16741e9bc
Son Chau <soncqb@gmail.com>
2023-04-04 15:10:24 -0700

    Update api_ingest.py
    bump ingest version to 1.1.0

--- chord_metadata_service/mohpackets/api_ingest.py
+++ chord_metadata_service/mohpackets/api_ingest.py
@@ -465,4 +465,4 @@ def delete_all(request):
 @api_view(["GET"])
 @permission_classes([CanDIGAdminOrReadOnly])
 def version_check(_request):
-    return JsonResponse({"version": "1.0.0"}, status=status.HTTP_200_OK)
+    return JsonResponse({"version": "1.1.0"}, status=status.HTTP_200_OK)
