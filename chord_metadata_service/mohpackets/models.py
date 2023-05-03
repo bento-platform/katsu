418145d111d7225ea1dc4f88fab5b3181cb411fd
Son Chau <soncqb@gmail.com>
2023-05-03 11:27:25 -0700

    Update models.py
     Renamed "chemotherapy_drug_dosage_units" to "chemotherapy_drug_dose_units" (replaced
    the word "dosage" with "dose")
    - Renamed "cumulative_prescribed_drug_dosage" to "prescribed_cumulative_drug_dose" for
    consistency with ARGO data model.
    - Renamed "cumulative_actual_drug_dosage" to "actual_cumulative_drug_dose" for
    consistency with ARGO data model.

--- chord_metadata_service/mohpackets/models.py
+++ chord_metadata_service/mohpackets/models.py
@@ -221,12 +221,14 @@ class Chemotherapy(models.Model):
     )
     drug_reference_database = models.CharField(max_length=64, null=True, blank=True)
     drug_name = models.CharField(max_length=255, null=True, blank=True)
-    drug_rxnormcui = models.CharField(max_length=64, null=True, blank=True)
-    chemotherapy_dosage_units = models.CharField(max_length=64, null=True, blank=True)
-    cumulative_drug_dosage_prescribed = models.PositiveSmallIntegerField(
+    drug_reference_identifier = models.CharField(max_length=64, null=True, blank=True)
+    chemotherapy_drug_dose_units = models.CharField(
+        max_length=64, null=True, blank=True
+    )
+    prescribed_cumulative_drug_dose = models.PositiveSmallIntegerField(
         blank=True, null=True
     )
-    cumulative_drug_dosage_actual = models.PositiveSmallIntegerField(
+    actual_cumulative_drug_dose = models.PositiveSmallIntegerField(
         blank=True, null=True
     )
 
