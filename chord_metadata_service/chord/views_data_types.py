922ea02e48fb8dd5c17526879a60b7f28368bbcb
David Lougheed <david.lougheed@gmail.com>
2023-05-15 14:42:41 -0400

    fix: handle data type endpoint bad requests (validation errors)

--- chord_metadata_service/chord/views_data_types.py
+++ chord_metadata_service/chord/views_data_types.py
@@ -1,6 +1,7 @@
 from bento_lib.responses import errors
-from django.http import HttpRequest
+from django.core.exceptions import ValidationError
 from django.db.models import QuerySet
+from django.http import HttpRequest
 
 from adrf.decorators import api_view
 from rest_framework.decorators import permission_classes
@@ -42,16 +43,28 @@ async def get_count_for_data_type(
     if data_type in (dt.DATA_TYPE_PHENOPACKET, dt.DATA_TYPE_EXPERIMENT):
         q = (Phenopacket if data_type == dt.DATA_TYPE_PHENOPACKET else Experiment).objects.all()
         if dataset:
-            q = q.filter(table__ownership_record__dataset_id=dataset)
+            try:
+                q = q.filter(table__ownership_record__dataset_id=dataset)
+            except ValidationError:
+                raise ValueError("Dataset ID must be a UUID")
         elif project:
-            q = q.filter(table__ownership_record__dataset__project_id=project)
+            try:
+                q = q.filter(table__ownership_record__dataset__project_id=project)
+            except ValidationError:
+                raise ValueError("Project ID must be a UUID")
 
     elif data_type == dt.DATA_TYPE_EXPERIMENT_RESULT:
         q = ExperimentResult.objects.all()
         if dataset:
-            q = q.filter(experiment_set__table__ownership_record__dataset_id=dataset)
+            try:
+                q = q.filter(experiment_set__table__ownership_record__dataset_id=dataset)
+            except ValidationError:
+                raise ValueError("Dataset ID must be a UUID")
         elif project:
-            q = q.filter(experiment_set__table__ownership_record__dataset__project_id=project)
+            try:
+                q = q.filter(experiment_set__table__ownership_record__dataset__project_id=project)
+            except ValidationError:
+                raise ValueError("Project ID must be a UUID")
 
     if q is None:
         raise ValueError(f"Unsupported data type for count function: {data_type}")
@@ -80,7 +93,10 @@ async def data_type_list(request: HttpRequest):
 
     dt_response = []
     for dt_id, dt_d in dt.DATA_TYPES.items():
-        dt_response.append(await make_data_type_response_object(dt_id, dt_d, project, dataset))
+        try:
+            dt_response.append(await make_data_type_response_object(dt_id, dt_d, project, dataset))
+        except ValueError as e:
+            return Response(errors.bad_request_error(str(e)), status=400)
 
     dt_response.sort(key=lambda d: d["id"])
     return Response(dt_response)
@@ -95,7 +111,10 @@ async def data_type_detail(request: HttpRequest, data_type: str):
     project = request.GET.get("project", "").strip() or None
     dataset = request.GET.get("dataset", "").strip() or None
 
-    return Response(await make_data_type_response_object(data_type, dt.DATA_TYPES[data_type], project, dataset))
+    try:
+        return Response(await make_data_type_response_object(data_type, dt.DATA_TYPES[data_type], project, dataset))
+    except ValueError as e:
+        return Response(errors.bad_request_error(str(e)), status=400)
 
 
 @api_view(["GET"])
