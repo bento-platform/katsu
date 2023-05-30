44cef693e469daa640ce9208e431a14971d59adc
yavyx <javiercastilloar@gmail.com>
2023-05-29 18:13:18 -0700

    fixed null values

--- chord_metadata_service/mohpackets/api_discovery.py
+++ chord_metadata_service/mohpackets/api_discovery.py
@@ -1,5 +1,6 @@
 from collections import Counter, defaultdict
 from datetime import datetime as dt
+from itertools import chain
 
 from drf_spectacular.utils import (
     extend_schema,
@@ -238,7 +239,7 @@ def gender_count(_request):
     """
     Return the count for every gender in the database.
     """
-    genders = SampleRegistration.objects.values_list("gender", flat=True)
+    genders = Donor.objects.values_list("gender", flat=True)
     return Response(count_terms(genders))
 
 
@@ -259,7 +260,13 @@ def cancer_type_count(_request):
     """
     Return the count for every cancer type in the database.
     """
-    cancer_types = Donor.objects.values_list("primary_site", flat=True)
+    cancer_types = list(Donor.objects.values_list("primary_site", flat=True))
+    
+    # Handle missing values as empty arrays
+    for i in range(len(cancer_types)):
+        if cancer_types[i] is None:
+            cancer_types[i] = [None]
+    
     return Response(count_terms(cancer_types))
 
 
@@ -280,7 +287,13 @@ def treatment_type_count(_request):
     """
     Return the count for every treatment type in the database.
     """
-    treatment_types = Treatment.objects.values_list("treatment_type", flat=True)
+    treatment_types = list(Treatment.objects.values_list("treatment_type", flat=True))
+
+    # Handle missing values as empty arrays
+    for i in range(len(treatment_types)):
+        if treatment_types[i] is None:
+            treatment_types[i] = [None]
+
     return Response(count_terms(treatment_types))
 
 
@@ -310,17 +323,17 @@ def diagnosis_age_count(_request):
         donor = date["submitter_donor_id"]
         cur_date = (
             dt.strptime(date["date_of_diagnosis"], "%Y-%m").date()
-            if date["date_of_diagnosis"] != ""
-            else ""
+            if date["date_of_diagnosis"] != None
+            else None
         )
         if donor not in min_dates.keys():
             min_dates[donor] = cur_date
         else:
-            if (min_dates[donor] != "" and cur_date < min_dates[donor]) or (
-                min_dates[donor] == "" and cur_date != ""
+            if (min_dates[donor] != None and cur_date < min_dates[donor]) or (
+                min_dates[donor] == None and cur_date != None
             ):
                 min_dates[donor] = cur_date
-
+                
     # Calculate donor's age of diagnosis
     birth_dates = Donor.objects.values("submitter_donor_id", "date_of_birth")
     birth_dates = {
@@ -328,16 +341,16 @@ def diagnosis_age_count(_request):
     }
     ages = {}
     for donor, diagnosis_date in min_dates.items():
-        if birth_dates[donor] != "" and diagnosis_date != "":
+        if birth_dates[donor] != None and diagnosis_date != None:
             birth_date = dt.strptime(birth_dates[donor], "%Y-%m").date()
             ages[donor] = (diagnosis_date - birth_date).days // 365.25
         else:
-            ages[donor] = ""
+            ages[donor] = None
 
     age_counts = defaultdict(int)
     for age in ages.values():
-        if age == "":
-            age_counts["NA"] += 1
+        if age == None:
+            age_counts["null"] += 1
         elif age <= 19:
             age_counts["0-19"] += 1
         elif age <= 29:
