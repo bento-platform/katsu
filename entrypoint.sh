90ce1cc911148ccfbe519c2492ded4d9c42d91bc
Son Chau <soncqb@gmail.com>
2023-06-12 12:14:49 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,5 +11,5 @@ done
 # Run migrations and start the server
 python manage.py makemigrations
 python manage.py migrate
-# python manage.py runserver 0.0.0.0:8000
+python manage.py collectstatic
 uwsgi --ini katsu_wsgi.ini
