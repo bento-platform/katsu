d643247067a98122827c46315f25e8a9a0864620
Son Chau <soncqb@gmail.com>
2023-07-10 15:17:52 -0700

    Update prod.py

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,5 +1,24 @@
+#####################################################################
+#                   PRODUCTION SETTINGS                             #
+# Inherit from setting base.py. Contain only necessary settings to  #
+# make the project run. Following:                                  #
+# https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/ #
+#####################################################################
+
 from .dev import *
 
 DEBUG = False
 
-SECRET_KEY = ""
+SECRET_KEY = os.environ.get("DJANGO_SECRET")
+
+SECURE_HSTS_SECONDS = 3600  # Once confirm that all assets are served securely(i.e. HSTS didn’t break anything), increase this value
+
+SECURE_SSL_REDIRECT = True
+
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+
+CSRF_COOKIE_SECURE = True
+
+SESSION_COOKIE_SECURE = True
+
+SECURE_HSTS_PRELOAD = True
