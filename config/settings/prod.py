9c7afb31fc8d3780454c6825159cb186bbe27d40
Son Chau <soncqb@gmail.com>
2023-07-12 11:14:08 -0700

    Update prod.py

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -25,15 +25,15 @@ SECURE_CONTENT_TYPE_NOSNIFF = True
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 SESSION_COOKIE_SECURE = True
 SECURE_HSTS_PRELOAD = True
-SECURE_HSTS_SECONDS = 3600  # Once confirm that all assets are served securely(i.e. HSTS didn’t break anything), increase this value
+SECURE_HSTS_SECONDS = 60  # Once confirm that all assets are served securely(i.e. HSTS didn’t break anything), increase this value
 # SECURE_HSTS_SECONDS = 60 * 60 * 24 * 7 * 52  # one year
 
 # import sentry_sdk
 # from sentry_sdk.integrations.django import DjangoIntegration
 
-# # ==============================================================================
-# # THIRD-PARTY APPS SETTINGS
-# # ==============================================================================
+# ==============================================================================
+# THIRD-PARTY APPS SETTINGS
+# ==============================================================================
 
 # sentry_sdk.init(
 #     dsn=config("SENTRY_DSN", default=""),
