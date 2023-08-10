dbb0cd8b7ba1a1c64ebd9ce8807dc5798566ea71
Son Chau <soncqb@gmail.com>
2023-08-10 09:36:20 -0700

    Create test_program.py

---
+++ chord_metadata_service/mohpackets/tests/endpoints/test_program.py
@@ -0,0 +1,186 @@
+from django.conf import settings
+from rest_framework import status
+
+from chord_metadata_service.mohpackets.serializers import ProgramSerializer
+from chord_metadata_service.mohpackets.tests.endpoints.base import BaseTestCase
+from chord_metadata_service.mohpackets.tests.endpoints.factories import ProgramFactory
+
+
+# INGEST API
+# ----------
+class IngestAPITestCase(BaseTestCase):
+    def setUp(self):
+        super().setUp()
+        self.ingest_url = "/v2/ingest/programs"
+
+    def test_ingest_201_created(self):
+        """
+        Test that an admin user can ingest and receive 201 Created response
+
+        Testing Strategy:
+        - An authorized user (user_2) with admin permission.
+        - User can perform a POST request for program ingestion.
+        """
+        ingest_programs = ProgramFactory.build_batch(2)
+        serialized_data = ProgramSerializer(ingest_programs, many=True).data
+        response = self.client.post(
+            self.ingest_url,
+            data=serialized_data,
+            format="json",
+            HTTP_AUTHORIZATION=f"Bearer {self.user_2.token}",
+        )
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_ingest_403_unauthorized(self):
+        """
+        Test that an non-admin user attempting to ingest programs receives a 403 Forbidden response.
+
+        Testing Strategy:
+        - An unauthorized user (user_0) with no permission.
+        - User cannot perform a POST request for program ingestion.
+        """
+        ingest_programs = ProgramFactory.build_batch(2)
+        serialized_data = ProgramSerializer(ingest_programs, many=True).data
+        response = self.client.post(
+            self.ingest_url,
+            data=serialized_data,
+            format="json",
+            HTTP_AUTHORIZATION=f"Bearer {self.user_0.token}",
+        )
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
+# DELETE API
+# ----------
+class DeleteAPITestCase(BaseTestCase):
+    def setUp(self):
+        super().setUp()
+        self.authorized_url = "/v2/authorized/programs/"
+
+    def test_delete_204_no_content(self):
+        """
+        Test an authorized DELETE request to the 'authorized/programs/{program_id}/' endpoint.
+
+        Testing Strategy:
+        - Create a new program to delete
+        - Admin (user_2) can delete
+        - The request should receive a 204 no content response.
+        """
+        program_to_delete = ProgramFactory()
+        response = self.client.delete(
+            f"{self.authorized_url}{program_to_delete.program_id}/",
+            HTTP_AUTHORIZATION=f"Bearer {self.user_2.token}",
+        )
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_delete_403_forbidden(self):
+        """
+        Test an unauthorized DELETE request 'authorized/programs/{program_id}/' endpoint.
+
+        Testing Strategy:
+        - Create a new program to delete
+        - Non Admin (user_1) cannot delete
+        - The request should receive a 403 forbidden response.
+        """
+        program_to_delete = ProgramFactory()
+        response = self.client.delete(
+            f"{self.authorized_url}{program_to_delete.program_id}/",
+            HTTP_AUTHORIZATION=f"Bearer {self.user_1.token}",
+        )
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
+# GET API
+# -------
+class GETAPITestCase(BaseTestCase):
+    def setUp(self):
+        super().setUp()
+        self.authorized_url = "/v2/authorized/programs/"
+
+    def test_get_200_ok(self):
+        """
+        Test a successful GET request to the 'authorized/programs/' endpoint.
+
+        Testing Strategy:
+        - An authorized user (user_1) attempts a GET request for authorized programs.
+        - The request should receive a 200 OK response.
+        """
+        response = self.client.get(
+            self.authorized_url,
+            HTTP_AUTHORIZATION=f"Bearer {self.user_1.token}",
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_301_redirect(self):
+        """
+        Test a GET request endpoint with a 301 redirection.
+
+        Testing Strategy:
+        - Send a GET request to the '/v2/authorized/programs' endpoint.
+        - The request should receive a 301 redirection response.
+        """
+        response = self.client.get(
+            "/v2/authorized/programs", HTTP_AUTHORIZATION=f"Bearer {self.user_1.token}"
+        )
+        self.assertEqual(response.status_code, status.HTTP_301_MOVED_PERMANENTLY)
+
+    def test_get_404_not_found(self):
+        """
+        Test a GET request for a 404 Not Found response.
+
+        Testing Strategy:
+        - Send a GET request to a non-existent endpoint ('/v2/authorized/programz').
+        - The request should receive a 404 Not Found response.
+        """
+        response = self.client.get(
+            "/v2/authorized/programz", HTTP_AUTHORIZATION=f"Bearer {self.user_1.token}"
+        )
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_get_datasets_match_permission(self):
+        """
+        Test that the response datasets match the authorized datasets for each user.
+
+        Testing Strategy:
+        - Verify that the response datasets match the datasets in the authorized dataset
+          for each of the test users.
+        """
+        for user in [self.user_0, self.user_1, self.user_2]:
+            response = self.client.get(
+                self.authorized_url,
+                HTTP_AUTHORIZATION=f"Bearer {user.token}",
+            )
+            self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+            authorized_datasets = next(
+                user_data["datasets"]
+                for user_data in settings.LOCAL_AUTHORIZED_DATASET
+                if user_data["token"] == user.token
+            )
+            response_datasets = [
+                program["program_id"] for program in response.data["results"]
+            ]
+            self.assertEqual(response_datasets, authorized_datasets)
+
+    def test_405_method_not_allowed(self):
+        """
+        Test requests for a 405 Method Not Allowed response.
+
+        Testing Strategy:
+        - Send a POST, PUT, PATCH request to the '/authorized/programs/' endpoint.
+        - The request should receive a 405 Method Not Allowed response.
+        """
+        response = self.client.post(
+            self.authorized_url, HTTP_AUTHORIZATION=f"Bearer {self.user_2.token}"
+        )
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+        response = self.client.put(
+            self.authorized_url, HTTP_AUTHORIZATION=f"Bearer {self.user_2.token}"
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+        response = self.client.patch(
+            self.authorized_url, HTTP_AUTHORIZATION=f"Bearer {self.user_2.token}"
+        )
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
