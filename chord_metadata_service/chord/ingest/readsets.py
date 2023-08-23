542d3616e51674ad34d86ffe32ff16c5247abdf6
Victor Rocheleau <victor.rocheleau@mcgill.ca>
2023-08-23 17:11:45 -0400

    readset workflow todo

--- chord_metadata_service/chord/ingest/readsets.py
+++ chord_metadata_service/chord/ingest/readsets.py
@@ -5,6 +5,7 @@ from .utils import get_output_or_raise, workflow_file_output_to_path
 # the table_id is required to fit the bento_ingest.schema.json in bento_lib
 # it can be any existing table_id which can be validated
 # the workflow only performs copying files over to the DRS
+# TODO: make a workflow to deposit files on DRS
 def ingest_readset_workflow(workflow_outputs, dataset_id):
     logger.info(f"Current workflow outputs : {workflow_outputs}")
     for readset_file in get_output_or_raise(workflow_outputs, "readset_files"):
