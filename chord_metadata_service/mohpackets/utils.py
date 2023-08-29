fe80191d75546e4b4a62f10355c6ce40c87e4b89
Son Chau <soncqb@gmail.com>
2023-08-29 15:41:34 -0700

    Update utils.py

--- chord_metadata_service/mohpackets/utils.py
+++ chord_metadata_service/mohpackets/utils.py
@@ -8,7 +8,7 @@ import subprocess
 #####################################################
 
 # Format: major.minor.patch.status
-VERSION = (2, 2, 2, "stable")
+VERSION = (2, 3, 0, "stable")
 
 
 def get_version():
