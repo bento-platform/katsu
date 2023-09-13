03ea68c87629b6b71363bce923bf990f6783bb6d
Son Chau <soncqb@gmail.com>
2023-09-13 11:50:04 -0700

    Update utils.py
    add docstring

--- chord_metadata_service/mohpackets/utils.py
+++ chord_metadata_service/mohpackets/utils.py
@@ -1,18 +1,29 @@
+import re
+
 import yaml
 from django.core.cache import cache
 
 
-def get_schema_version():
-    schema_version = cache.get("schema_version")
-    if schema_version is None:
+def get_schema_url():
+    """
+    Retrieve the schema URL either from cached or by parsing a YAML file.
+    It first checks if the URL is cached in "schema_url".
+    If not cached, it reads the YAML file and extracts the URL from the "description".
+    """
+
+    schema_url = cache.get("schema_url")
+    url_pattern = r"https://[^\s]+"  # get everything after https
+
+    if schema_url is None:
         try:
             with open(
                 "chord_metadata_service/mohpackets/docs/schema.yml", "r"
             ) as yaml_file:
                 data = yaml.safe_load(yaml_file)
-                schema_version = data["info"]["version"]
+                desc_str = data["info"]["description"]
+                schema_url = re.search(url_pattern, desc_str).group()
                 # Cache the schema_version so we won't read it each time
-                cache.set("schema_version", schema_version)
+                cache.set("schema_url", schema_url)
         except Exception as e:
-            schema_version = None
-    return schema_version
+            schema_url = None
+    return schema_url
