7172d4515643fc6428969a6b66f91a924be1d989
Son Chau <soncqb@gmail.com>
2023-10-31 14:58:49 -0700

    Update utils.py

--- chord_metadata_service/mohpackets/utils.py
+++ chord_metadata_service/mohpackets/utils.py
@@ -1,10 +1,49 @@
 import logging
+import os
 import re
 
 import yaml
+from authx.auth import get_opa_datasets, is_site_admin
+from django.conf import settings
 from django.core.cache import cache
+from ninja.security import HttpBearer
 
 logger = logging.getLogger(__name__)
+
+SAFE_METHODS = ("GET", "HEAD", "OPTIONS")
+
+
+class TokenAuthentication(HttpBearer):
+    def authenticate(self, request, token):
+        try:
+            opa_secret = settings.CANDIG_OPA_SECRET
+            authorized_datasets = get_opa_datasets(request, admin_secret=opa_secret)
+            request.authorized_datasets = authorized_datasets
+            request.has_permission = request.method in SAFE_METHODS or is_site_admin(
+                request, admin_secret=opa_secret
+            )
+        except Exception as e:
+            logger.exception(f"An error occurred in OPA: {e}")
+            raise Exception("Error with OPA authentication.")
+
+
+class LocalAuthentication(HttpBearer):
+    def authenticate(self, request, token):
+        authorized_datasets = [
+            dataset
+            for d in settings.LOCAL_AUTHORIZED_DATASET
+            if d["token"] == token
+            for dataset in d["datasets"]
+        ]
+        request.authorized_datasets = authorized_datasets
+
+        request.has_permission = request.method in SAFE_METHODS or any(
+            d.get("is_admin", False)
+            for d in settings.LOCAL_AUTHORIZED_DATASET
+            if d["token"] == token
+        )
+
+        return token
 
 
 def get_schema_url():
