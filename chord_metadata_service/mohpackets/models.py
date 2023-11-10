87e3d86476fae23b1efd7e490c6eb2ef49d31334
Son Chau <soncqb@gmail.com>
2023-11-09 16:50:07 -0800

    Update models.py

--- chord_metadata_service/mohpackets/models.py
+++ chord_metadata_service/mohpackets/models.py
@@ -2,8 +2,6 @@ import uuid
 
 from django.contrib.postgres.fields import ArrayField
 from django.db import models
-from django.db.models.signals import pre_save
-from django.dispatch import receiver
 from django.utils import timezone
 
 """ 
