d2267c7a2e50b427fd64e1021b9fc41ed32888d4
Son Chau <soncqb@gmail.com>
2023-11-21 16:45:49 -0800

    Update utils.py

--- chord_metadata_service/mohpackets/utils.py
+++ chord_metadata_service/mohpackets/utils.py
@@ -1,14 +1,7 @@
 import logging
-import re
 from enum import Enum
 
-import yaml
-from django.core.cache import cache
-
 logger = logging.getLogger(__name__)
-
-
-SAFE_METHODS = ("GET", "HEAD", "OPTIONS")
 
 
 def get_schema_url():
@@ -18,24 +11,25 @@ def get_schema_url():
     If not cached, it reads the YAML file and extracts the URL from the "description".
     """
 
-    schema_url = cache.get("schema_url")
-    url_pattern = r"https://[^\s]+"  # get everything after https
+    # schema_url = cache.get("schema_url")
+    # url_pattern = r"https://[^\s]+"  # get everything after https
 
-    if schema_url is None:
-        try:
-            with open(
-                "chord_metadata_service/mohpackets/docs/schema.yml", "r"
-            ) as yaml_file:
-                data = yaml.safe_load(yaml_file)
-                desc_str = data["info"]["description"]
-                schema_url = re.search(url_pattern, desc_str).group()
-                # Cache the schema_version so we won't read it each time
-                cache.set("schema_url", schema_url)
-        except Exception as e:
-            logger.debug(
-                f"An error occurred while fetching the schema URL. Details: {str(e)}"
-            )
-            schema_url = None
+    # if schema_url is None:
+    #     try:
+    #         with open(
+    #             "chord_metadata_service/mohpackets/docs/schema.yml", "r"
+    #         ) as yaml_file:
+    #             data = yaml.safe_load(yaml_file)
+    #             desc_str = data["info"]["description"]
+    #             schema_url = re.search(url_pattern, desc_str).group()
+    #             # Cache the schema_version so we won't read it each time
+    #             cache.set("schema_url", schema_url)
+    #     except Exception as e:
+    #         logger.debug(
+    #             f"An error occurred while fetching the schema URL. Details: {str(e)}"
+    #         )
+    #         schema_url = None
+    schema_url = None
     return schema_url
 
 
