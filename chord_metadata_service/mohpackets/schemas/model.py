62b94f296b7b7d4ee035c5e9bde8b119aa591b72
Son Chau <soncqb@gmail.com>
2024-01-17 13:00:35 -0800

    Update model.py
    update model based on "BaseSchema"

--- chord_metadata_service/mohpackets/schemas/model.py
+++ chord_metadata_service/mohpackets/schemas/model.py
@@ -1,100 +1,30 @@
-from typing import List, Optional
+from typing import Optional
 
-from ninja import Field, ModelSchema, Schema
-from ninja.orm import create_schema
+from ninja import Field
 
-from chord_metadata_service.mohpackets.models import (
-    Biomarker,
-    Chemotherapy,
-    Comorbidity,
-    Donor,
-    Exposure,
-    FollowUp,
-    HormoneTherapy,
-    Immunotherapy,
-    PrimaryDiagnosis,
-    Program,
-    Radiation,
-    SampleRegistration,
-    Specimen,
-    Surgery,
-    Treatment,
-)
-from chord_metadata_service.mohpackets.permissible_values import (
-    COMORBIDITY_REGEX_PATTERNS,
-    DATE_REGEX_PATTERNS,
-    ID_REGEX_PATTERNS,
-    MORPHOLOGY_REGEX_PATTERNS,
-    TOPOGRAPHY_REGEX_PATTERNS,
-    BasisOfDiagnosisEnum,
-    CauseOfDeathEnum,
-    CellsMeasureMethodEnum,
-    ConfirmedDiagnosisTumourEnum,
-    DiseaseStatusFollowupEnum,
-    DosageUnitsEnum,
-    DrugReferenceDbEnum,
-    ErPrHpvStatusEnum,
-    GenderEnum,
-    Her2StatusEnum,
-    HpvStrainEnum,
-    ImmunotherapyTypeEnum,
-    LostToFollowupReasonEnum,
-    LymphNodeMethodEnum,
-    LymphNodeStatusEnum,
-    LymphovascularInvasionEnum,
-    MalignancyLateralityEnum,
-    MarginTypesEnum,
-    MCategoryEnum,
-    NCategoryEnum,
-    PercentCellsRangeEnum,
-    PerineuralInvasionEnum,
-    PrimaryDiagnosisLateralityEnum,
-    PrimarySiteEnum,
-    ProgressionStatusMethodEnum,
-    RadiationAnatomicalSiteEnum,
-    RadiationTherapyModalityEnum,
-    RelapseTypeEnum,
-    SampleTypeEnum,
-    SexAtBirthEnum,
-    SmokingStatusEnum,
-    SpecimenLateralityEnum,
-    SpecimenProcessingEnum,
-    SpecimenTissueSourceEnum,
-    SpecimenTypeEnum,
-    StageGroupEnum,
-    StorageEnum,
-    SurgeryLocationEnum,
-    SurgeryTypeEnum,
-    TCategoryEnum,
-    TherapyTypeEnum,
-    TobaccoTypeEnum,
-    TreatmentIntentEnum,
-    TreatmentResponseEnum,
-    TreatmentResponseMethodEnum,
-    TreatmentSettingEnum,
-    TreatmentStatusEnum,
-    TreatmentTypeEnum,
-    TumourClassificationEnum,
-    TumourDesginationEnum,
-    TumourFocalityEnum,
-    TumourGradeEnum,
-    TumourGradingSystemEnum,
-    TumourStagingSystemEnum,
-    uBooleanEnum,
-)
 from chord_metadata_service.mohpackets.schemas.base import (
+    BaseBiomarkerSchema,
+    BaseChemotherapySchema,
+    BaseComorbiditySchema,
     BaseDonorSchema,
+    BaseExposureSchema,
+    BaseFollowUpSchema,
+    BaseHormoneTherapySchema,
+    BaseImmunotherapySchema,
     BasePrimaryDiagnosisSchema,
     BaseProgramSchema,
+    BaseRadiationSchema,
+    BaseSampleRegistrationSchema,
     BaseSpecimenSchema,
+    BaseSurgerySchema,
+    BaseTreatmentSchema,
 )
 
 
 """
-Module containing schemas for clinical data models.
+Schemas for clinical models, inherted from base schemas.
 
-All fields are derived from the model, excluding 'uuid'.
-Custom validations, such as regex and permissible choices, have been included.
+Added "foreign keys" to link between models
 
 Author: Son Chau
 """
@@ -114,142 +44,142 @@ Author: Son Chau
 #         model_fields = "__all__"
 
 
-class ExposureModelSchema(ModelSchema):
-    tobacco_smoking_status: Optional[SmokingStatusEnum] = None
-    tobacco_type: Optional[List[TobaccoTypeEnum]] = None
+# class ExposureModelSchema(ModelSchema):
+#     tobacco_smoking_status: Optional[SmokingStatusEnum] = None
+#     tobacco_type: Optional[List[TobaccoTypeEnum]] = None
 
-    class Config:
-        model = Exposure
-        model_exclude = ["uuid", "donor_uuid"]
-
-
-class ComorbidityModelSchema(ModelSchema):
-    prior_malignancy: Optional[uBooleanEnum] = None
-    laterality_of_prior_malignancy: Optional[MalignancyLateralityEnum] = None
-    comorbidity_type_code: Optional[str] = Field(
-        None, pattern=COMORBIDITY_REGEX_PATTERNS, max_length=64
-    )
-    comorbidity_treatment_status: Optional[uBooleanEnum] = None
-    comorbidity_treatment: Optional[str] = Field(None, max_length=255)
-
-    class Config:
-        model = Comorbidity
-        model_exclude = ["uuid", "donor_uuid"]
+#     class Config:
+#         model = Exposure
+#         model_exclude = ["uuid", "donor_uuid"]
 
 
-class BiomarkerModelSchema(ModelSchema):
-    er_status: Optional[ErPrHpvStatusEnum] = None
-    pr_status: Optional[ErPrHpvStatusEnum] = None
-    her2_ihc_status: Optional[Her2StatusEnum] = None
-    her2_ish_status: Optional[Her2StatusEnum] = None
-    hpv_ihc_status: Optional[ErPrHpvStatusEnum] = None
-    hpv_pcr_status: Optional[ErPrHpvStatusEnum] = None
-    hpv_strain: Optional[List[HpvStrainEnum]] = None
+# class ComorbidityModelSchema(ModelSchema):
+#     prior_malignancy: Optional[uBooleanEnum] = None
+#     laterality_of_prior_malignancy: Optional[MalignancyLateralityEnum] = None
+#     comorbidity_type_code: Optional[str] = Field(
+#         None, pattern=COMORBIDITY_REGEX_PATTERNS, max_length=64
+#     )
+#     comorbidity_treatment_status: Optional[uBooleanEnum] = None
+#     comorbidity_treatment: Optional[str] = Field(None, max_length=255)
 
-    class Config:
-        model = Biomarker
-        model_exclude = ["uuid", "donor_uuid"]
-
-
-class FollowUpModelSchema(ModelSchema):
-    submitter_follow_up_id: str = Field(pattern=ID_REGEX_PATTERNS, max_length=64)
-    disease_status_at_followup: Optional[DiseaseStatusFollowupEnum] = None
-    relapse_type: Optional[RelapseTypeEnum] = None
-    date_of_followup: Optional[str] = Field(
-        None, pattern=DATE_REGEX_PATTERNS, max_length=32
-    )
-    date_of_relapse: Optional[str] = Field(
-        None, pattern=DATE_REGEX_PATTERNS, max_length=32
-    )
-    method_of_progression_status: Optional[List[ProgressionStatusMethodEnum]] = None
-    anatomic_site_progression_or_recurrence: Optional[List[str]] = None
-    recurrence_tumour_staging_system: Optional[TumourStagingSystemEnum] = None
-    recurrence_t_category: Optional[TCategoryEnum] = None
-    recurrence_n_category: Optional[NCategoryEnum] = None
-    recurrence_m_category: Optional[MCategoryEnum] = None
-    recurrence_stage_group: Optional[StageGroupEnum] = None
-
-    class Config:
-        model = FollowUp
-        model_exclude = ["uuid", "donor_uuid"]
+#     class Config:
+#         model = Comorbidity
+#         model_exclude = ["uuid", "donor_uuid"]
 
 
-class SurgeryModelSchema(ModelSchema):
-    surgery_type: Optional[SurgeryTypeEnum] = None
-    surgery_site: Optional[str] = Field(
-        None, pattern=TOPOGRAPHY_REGEX_PATTERNS, max_length=255
-    )
-    surgery_location: Optional[SurgeryLocationEnum] = None
-    tumour_focality: Optional[TumourFocalityEnum] = None
-    residual_tumour_classification: Optional[TumourClassificationEnum] = None
-    margin_types_involved: Optional[List[MarginTypesEnum]] = None
-    margin_types_not_involved: Optional[List[MarginTypesEnum]] = None
-    margin_types_not_assessed: Optional[List[MarginTypesEnum]] = None
-    lymphovascular_invasion: Optional[LymphovascularInvasionEnum] = None
-    perineural_invasion: Optional[PerineuralInvasionEnum] = None
+# class BiomarkerModelSchema(ModelSchema):
+#     er_status: Optional[ErPrHpvStatusEnum] = None
+#     pr_status: Optional[ErPrHpvStatusEnum] = None
+#     her2_ihc_status: Optional[Her2StatusEnum] = None
+#     her2_ish_status: Optional[Her2StatusEnum] = None
+#     hpv_ihc_status: Optional[ErPrHpvStatusEnum] = None
+#     hpv_pcr_status: Optional[ErPrHpvStatusEnum] = None
+#     hpv_strain: Optional[List[HpvStrainEnum]] = None
 
-    class Config:
-        model = Surgery
-        model_exclude = ["uuid", "donor_uuid", "treatment_uuid"]
+#     class Config:
+#         model = Biomarker
+#         model_exclude = ["uuid", "donor_uuid"]
 
 
-class ImmunotherapyModelSchema(ModelSchema):
-    immunotherapy_type: Optional[ImmunotherapyTypeEnum] = None
-    drug_reference_database: Optional[DrugReferenceDbEnum] = None
-    immunotherapy_drug_dose_units: Optional[DosageUnitsEnum] = None
+# class FollowUpModelSchema(ModelSchema):
+#     submitter_follow_up_id: str = Field(pattern=ID_REGEX_PATTERNS, max_length=64)
+#     disease_status_at_followup: Optional[DiseaseStatusFollowupEnum] = None
+#     relapse_type: Optional[RelapseTypeEnum] = None
+#     date_of_followup: Optional[str] = Field(
+#         None, pattern=DATE_REGEX_PATTERNS, max_length=32
+#     )
+#     date_of_relapse: Optional[str] = Field(
+#         None, pattern=DATE_REGEX_PATTERNS, max_length=32
+#     )
+#     method_of_progression_status: Optional[List[ProgressionStatusMethodEnum]] = None
+#     anatomic_site_progression_or_recurrence: Optional[List[str]] = None
+#     recurrence_tumour_staging_system: Optional[TumourStagingSystemEnum] = None
+#     recurrence_t_category: Optional[TCategoryEnum] = None
+#     recurrence_n_category: Optional[NCategoryEnum] = None
+#     recurrence_m_category: Optional[MCategoryEnum] = None
+#     recurrence_stage_group: Optional[StageGroupEnum] = None
 
-    class Config:
-        model = Immunotherapy
-        model_exclude = ["uuid", "donor_uuid", "treatment_uuid"]
-
-
-class RadiationModelSchema(ModelSchema):
-    radiation_therapy_modality: Optional[RadiationTherapyModalityEnum] = None
-    radiation_therapy_type: Optional[TherapyTypeEnum] = None
-    anatomical_site_irradiated: Optional[RadiationAnatomicalSiteEnum] = None
-
-    class Config:
-        model = Radiation
-        model_exclude = ["uuid", "donor_uuid", "treatment_uuid"]
-
-
-class HormoneTherapyModelSchema(ModelSchema):
-    hormone_drug_dose_units: Optional[DosageUnitsEnum] = None
-    drug_reference_database: Optional[DrugReferenceDbEnum] = None
-
-    class Config:
-        model = HormoneTherapy
-        model_exclude = ["uuid", "donor_uuid", "treatment_uuid"]
+#     class Config:
+#         model = FollowUp
+#         model_exclude = ["uuid", "donor_uuid"]
 
 
-class ChemotherapyModelSchema(ModelSchema):
-    chemotherapy_drug_dose_units: Optional[DosageUnitsEnum] = None
-    drug_reference_database: Optional[DrugReferenceDbEnum] = None
+# class SurgeryModelSchema(ModelSchema):
+#     surgery_type: Optional[SurgeryTypeEnum] = None
+#     surgery_site: Optional[str] = Field(
+#         None, pattern=TOPOGRAPHY_REGEX_PATTERNS, max_length=255
+#     )
+#     surgery_location: Optional[SurgeryLocationEnum] = None
+#     tumour_focality: Optional[TumourFocalityEnum] = None
+#     residual_tumour_classification: Optional[TumourClassificationEnum] = None
+#     margin_types_involved: Optional[List[MarginTypesEnum]] = None
+#     margin_types_not_involved: Optional[List[MarginTypesEnum]] = None
+#     margin_types_not_assessed: Optional[List[MarginTypesEnum]] = None
+#     lymphovascular_invasion: Optional[LymphovascularInvasionEnum] = None
+#     perineural_invasion: Optional[PerineuralInvasionEnum] = None
 
-    class Config:
-        model = Chemotherapy
-        model_exclude = ["uuid", "donor_uuid", "treatment_uuid"]
+#     class Config:
+#         model = Surgery
+#         model_exclude = ["uuid", "donor_uuid", "treatment_uuid"]
 
 
-class TreatmentModelSchema(ModelSchema):
-    submitter_treatment_id: str = Field(pattern=ID_REGEX_PATTERNS, max_length=64)
-    treatment_type: Optional[List[TreatmentTypeEnum]] = None
-    is_primary_treatment: Optional[uBooleanEnum] = None
-    treatment_start_date: Optional[str] = Field(
-        None, pattern=DATE_REGEX_PATTERNS, max_length=32
-    )
-    treatment_end_date: Optional[str] = Field(
-        None, pattern=DATE_REGEX_PATTERNS, max_length=32
-    )
-    treatment_setting: Optional[TreatmentSettingEnum] = None
-    treatment_intent: Optional[TreatmentIntentEnum] = None
-    response_to_treatment_criteria_method: Optional[TreatmentResponseMethodEnum] = None
-    response_to_treatment: Optional[TreatmentResponseEnum] = None
-    status_of_treatment: Optional[TreatmentStatusEnum] = None
+# class ImmunotherapyModelSchema(ModelSchema):
+#     immunotherapy_type: Optional[ImmunotherapyTypeEnum] = None
+#     drug_reference_database: Optional[DrugReferenceDbEnum] = None
+#     immunotherapy_drug_dose_units: Optional[DosageUnitsEnum] = None
 
-    class Config:
-        model = Treatment
-        model_exclude = ["uuid", "donor_uuid", "primary_diagnosis_uuid"]
+#     class Config:
+#         model = Immunotherapy
+#         model_exclude = ["uuid", "donor_uuid", "treatment_uuid"]
+
+
+# class RadiationModelSchema(ModelSchema):
+#     radiation_therapy_modality: Optional[RadiationTherapyModalityEnum] = None
+#     radiation_therapy_type: Optional[TherapyTypeEnum] = None
+#     anatomical_site_irradiated: Optional[RadiationAnatomicalSiteEnum] = None
+
+#     class Config:
+#         model = Radiation
+#         model_exclude = ["uuid", "donor_uuid", "treatment_uuid"]
+
+
+# class HormoneTherapyModelSchema(ModelSchema):
+#     hormone_drug_dose_units: Optional[DosageUnitsEnum] = None
+#     drug_reference_database: Optional[DrugReferenceDbEnum] = None
+
+#     class Config:
+#         model = HormoneTherapy
+#         model_exclude = ["uuid", "donor_uuid", "treatment_uuid"]
+
+
+# class ChemotherapyModelSchema(ModelSchema):
+#     chemotherapy_drug_dose_units: Optional[DosageUnitsEnum] = None
+#     drug_reference_database: Optional[DrugReferenceDbEnum] = None
+
+#     class Config:
+#         model = Chemotherapy
+#         model_exclude = ["uuid", "donor_uuid", "treatment_uuid"]
+
+
+# class TreatmentModelSchema(ModelSchema):
+#     submitter_treatment_id: str = Field(pattern=ID_REGEX_PATTERNS, max_length=64)
+#     treatment_type: Optional[List[TreatmentTypeEnum]] = None
+#     is_primary_treatment: Optional[uBooleanEnum] = None
+#     treatment_start_date: Optional[str] = Field(
+#         None, pattern=DATE_REGEX_PATTERNS, max_length=32
+#     )
+#     treatment_end_date: Optional[str] = Field(
+#         None, pattern=DATE_REGEX_PATTERNS, max_length=32
+#     )
+#     treatment_setting: Optional[TreatmentSettingEnum] = None
+#     treatment_intent: Optional[TreatmentIntentEnum] = None
+#     response_to_treatment_criteria_method: Optional[TreatmentResponseMethodEnum] = None
+#     response_to_treatment: Optional[TreatmentResponseEnum] = None
+#     status_of_treatment: Optional[TreatmentStatusEnum] = None
+
+#     class Config:
+#         model = Treatment
+#         model_exclude = ["uuid", "donor_uuid", "primary_diagnosis_uuid"]
 
 
 # class PrimaryDiagnosisModelSchema(ModelSchema):
@@ -275,16 +205,16 @@ class TreatmentModelSchema(ModelSchema):
 #         exclude = ["uuid", "donor_uuid"]
 
 
-class SampleRegistrationModelSchema(ModelSchema):
-    submitter_sample_id: str = Field(pattern=ID_REGEX_PATTERNS, max_length=64)
-    specimen_tissue_source: Optional[SpecimenTissueSourceEnum] = None
-    tumour_normal_designation: Optional[TumourDesginationEnum] = None
-    specimen_type: Optional[SpecimenTypeEnum] = None
-    sample_type: Optional[SampleTypeEnum] = None
+# class SampleRegistrationModelSchema(ModelSchema):
+#     submitter_sample_id: str = Field(pattern=ID_REGEX_PATTERNS, max_length=64)
+#     specimen_tissue_source: Optional[SpecimenTissueSourceEnum] = None
+#     tumour_normal_designation: Optional[TumourDesginationEnum] = None
+#     specimen_type: Optional[SpecimenTypeEnum] = None
+#     sample_type: Optional[SampleTypeEnum] = None
 
-    class Config:
-        model = SampleRegistration
-        model_exclude = ["uuid", "donor_uuid", "specimen_uuid"]
+#     class Config:
+#         model = SampleRegistration
+#         model_exclude = ["uuid", "donor_uuid", "specimen_uuid"]
 
 
 # class SpecimenModelSchema(ModelSchema):
@@ -351,11 +281,85 @@ class ProgramModelSchema(BaseProgramSchema):
 class DonorModelSchema(BaseDonorSchema):
     program_id: str = Field(..., alias="program_id_id")
 
+
 class PrimaryDiagnosisModelSchema(BasePrimaryDiagnosisSchema):
     program_id: str = Field(..., alias="program_id_id")
-    submitter_donor_id: str = Field(..., alias="submitter_donor_id_id") 
+    submitter_donor_id: str = Field(..., alias="submitter_donor_id_id")
+
 
 class SpecimenModelSchema(BaseSpecimenSchema):
     program_id: str = Field(..., alias="program_id_id")
-    submitter_donor_id: str = Field(..., alias="submitter_donor_id_id") 
-    submitter_primary_diagnosis_id : str = Field(..., alias="submitter_primary_diagnosis_id_id") 
+    submitter_donor_id: str = Field(..., alias="submitter_donor_id_id")
+    submitter_primary_diagnosis_id: str = Field(
+        ..., alias="submitter_primary_diagnosis_id_id"
+    )
+
+
+class SampleRegistrationModelSchema(BaseSampleRegistrationSchema):
+    program_id: str = Field(..., alias="program_id_id")
+    submitter_donor_id: str = Field(..., alias="submitter_donor_id_id")
+    submitter_specimen_id: str = Field(..., alias="submitter_specimen_id_id")
+
+
+class TreatmentModelSchema(BaseTreatmentSchema):
+    program_id: str = Field(..., alias="program_id_id")
+    submitter_donor_id: str = Field(..., alias="submitter_donor_id_id")
+    submitter_primary_diagnosis_id: str = Field(
+        ..., alias="submitter_primary_diagnosis_id_id"
+    )
+
+
+class SurgeryModelSchema(BaseSurgerySchema):
+    program_id: str = Field(..., alias="program_id_id")
+    submitter_donor_id: str = Field(..., alias="submitter_donor_id_id")
+    submitter_treatment_id: str = Field(..., alias="submitter_treatment_id_id")
+
+
+class ImmunotherapyModelSchema(BaseImmunotherapySchema):
+    program_id: str = Field(..., alias="program_id_id")
+    submitter_donor_id: str = Field(..., alias="submitter_donor_id_id")
+    submitter_treatment_id: str = Field(..., alias="submitter_treatment_id_id")
+
+
+class RadiationModelSchema(BaseRadiationSchema):
+    program_id: str = Field(..., alias="program_id_id")
+    submitter_donor_id: str = Field(..., alias="submitter_donor_id_id")
+    submitter_treatment_id: str = Field(..., alias="submitter_treatment_id_id")
+
+
+class HormoneTherapyModelSchema(BaseHormoneTherapySchema):
+    program_id: str = Field(..., alias="program_id_id")
+    submitter_donor_id: str = Field(..., alias="submitter_donor_id_id")
+    submitter_treatment_id: str = Field(..., alias="submitter_treatment_id_id")
+
+
+class ChemotherapyModelSchema(BaseChemotherapySchema):
+    program_id: str = Field(..., alias="program_id_id")
+    submitter_donor_id: str = Field(..., alias="submitter_donor_id_id")
+    submitter_treatment_id: str = Field(..., alias="submitter_treatment_id_id")
+
+
+class FollowUpModelSchema(BaseFollowUpSchema):
+    program_id: str = Field(..., alias="program_id_id")
+    submitter_donor_id: str = Field(..., alias="submitter_donor_id_id")
+    submitter_primary_diagnosis_id: Optional[str] = Field(
+        ..., alias="submitter_primary_diagnosis_id_id"
+    )
+    submitter_treatment_id: Optional[str] = Field(
+        ..., alias="submitter_treatment_id_id"
+    )
+
+
+class BiomarkerModelSchema(BaseBiomarkerSchema):
+    program_id: str = Field(..., alias="program_id_id")
+    submitter_donor_id: str = Field(..., alias="submitter_donor_id_id")
+
+
+class ExposureModelSchema(BaseExposureSchema):
+    program_id: str = Field(..., alias="program_id_id")
+    submitter_donor_id: str = Field(..., alias="submitter_donor_id_id")
+
+
+class ComorbidityModelSchema(BaseComorbiditySchema):
+    program_id: str = Field(..., alias="program_id_id")
+    submitter_donor_id: str = Field(..., alias="submitter_donor_id_id")
