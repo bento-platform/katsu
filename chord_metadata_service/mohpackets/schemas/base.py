d3fcd36135653c044175cbf8744ed2ba08e3af11
Son Chau <soncqb@gmail.com>
2024-01-17 11:59:11 -0800

    Update base.py
    add the rest of base models

--- chord_metadata_service/mohpackets/schemas/base.py
+++ chord_metadata_service/mohpackets/schemas/base.py
@@ -166,17 +166,266 @@ BaseSpecimenSchema = create_schema(
         ("pathological_n_category", Optional[NCategoryEnum], None),
         ("pathological_m_category", Optional[MCategoryEnum], None),
         ("pathological_stage_group", Optional[StageGroupEnum], None),
-        ("specimen_collection_date", Optional[str], Field(None, pattern=DATE_REGEX_PATTERNS, max_length=32)),
+        (
+            "specimen_collection_date",
+            Optional[str],
+            Field(None, pattern=DATE_REGEX_PATTERNS, max_length=32),
+        ),
         ("specimen_storage", Optional[StorageEnum], None),
-        ("tumour_histological_type", Optional[str], Field(None, max_length=128, pattern=MORPHOLOGY_REGEX_PATTERNS)),
-        ("specimen_anatomic_location", Optional[str], Field(None, max_length=32, pattern=TOPOGRAPHY_REGEX_PATTERNS)),
-        ("reference_pathology_confirmed_diagnosis", Optional[ConfirmedDiagnosisTumourEnum], None),
-        ("reference_pathology_confirmed_tumour_presence", Optional[ConfirmedDiagnosisTumourEnum], None),
+        (
+            "tumour_histological_type",
+            Optional[str],
+            Field(None, max_length=128, pattern=MORPHOLOGY_REGEX_PATTERNS),
+        ),
+        (
+            "specimen_anatomic_location",
+            Optional[str],
+            Field(None, max_length=32, pattern=TOPOGRAPHY_REGEX_PATTERNS),
+        ),
+        (
+            "reference_pathology_confirmed_diagnosis",
+            Optional[ConfirmedDiagnosisTumourEnum],
+            None,
+        ),
+        (
+            "reference_pathology_confirmed_tumour_presence",
+            Optional[ConfirmedDiagnosisTumourEnum],
+            None,
+        ),
         ("tumour_grading_system", Optional[TumourGradingSystemEnum], None),
         ("tumour_grade", Optional[TumourGradeEnum], None),
         ("percent_tumour_cells_range", Optional[PercentCellsRangeEnum], None),
-        ("percent_tumour_cells_measurement_method", Optional[CellsMeasureMethodEnum], None),
+        (
+            "percent_tumour_cells_measurement_method",
+            Optional[CellsMeasureMethodEnum],
+            None,
+        ),
         ("specimen_processing", Optional[SpecimenProcessingEnum], None),
         ("specimen_laterality", Optional[SpecimenLateralityEnum], None),
+    ],
+)
+
+BaseSampleRegistrationSchema = create_schema(
+    SampleRegistration,
+    name="BaseSampleRegistrationSchema",
+    exclude=["uuid", "donor_uuid", "specimen_uuid", "submitter_donor_id", "program_id"],
+    custom_fields=[
+        ("submitter_sample_id", str, Field(pattern=ID_REGEX_PATTERNS, max_length=64)),
+        ("specimen_tissue_source", Optional[SpecimenTissueSourceEnum], None),
+        ("tumour_normal_designation", Optional[TumourDesginationEnum], None),
+        ("specimen_type", Optional[SpecimenTypeEnum], None),
+        ("sample_type", Optional[SampleTypeEnum], None),
+    ],
+)
+
+BaseTreatmentSchema = create_schema(
+    Treatment,
+    name="BaseTreatmentSchema",
+    exclude=[
+        "uuid",
+        "donor_uuid",
+        "primary_diagnosis_uuid",
+        "submitter_donor_id",
+        "program_id",
+    ],
+    custom_fields=[
+        (
+            "submitter_treatment_id",
+            str,
+            Field(pattern=ID_REGEX_PATTERNS, max_length=64),
+        ),
+        ("treatment_type", Optional[List[TreatmentTypeEnum]], None),
+        ("is_primary_treatment", Optional[uBooleanEnum], None),
+        (
+            "treatment_start_date",
+            Optional[str],
+            Field(None, pattern=DATE_REGEX_PATTERNS, max_length=32),
+        ),
+        (
+            "treatment_end_date",
+            Optional[str],
+            Field(None, pattern=DATE_REGEX_PATTERNS, max_length=32),
+        ),
+        ("treatment_setting", Optional[TreatmentSettingEnum], None),
+        ("treatment_intent", Optional[TreatmentIntentEnum], None),
+        (
+            "response_to_treatment_criteria_method",
+            Optional[TreatmentResponseMethodEnum],
+            None,
+        ),
+        ("response_to_treatment", Optional[TreatmentResponseEnum], None),
+        ("status_of_treatment", Optional[TreatmentStatusEnum], None),
+    ],
+)
+
+BaseChemotherapySchema = create_schema(
+    Chemotherapy,
+    name="BaseChemotherapySchema",
+    exclude=[
+        "uuid",
+        "donor_uuid",
+        "treatment_uuid",
+        "submitter_donor_id",
+        "program_id",
+    ],
+    custom_fields=[
+        ("chemotherapy_drug_dose_units", Optional[DosageUnitsEnum], None),
+        ("drug_reference_database", Optional[DrugReferenceDbEnum], None),
+    ],
+)
+
+BaseHormoneTherapySchema = create_schema(
+    HormoneTherapy,
+    name="BaseHormoneTherapySchema",
+    exclude=[
+        "uuid",
+        "donor_uuid",
+        "treatment_uuid",
+        "submitter_donor_id",
+        "program_id",
+    ],
+    custom_fields=[
+        ("hormone_drug_dose_units", Optional[DosageUnitsEnum], None),
+        ("drug_reference_database", Optional[DrugReferenceDbEnum], None),
+    ],
+)
+
+BaseRadiationSchema = create_schema(
+    Radiation,
+    name="BaseRadiationSchema",
+    exclude=[
+        "uuid",
+        "donor_uuid",
+        "treatment_uuid",
+        "submitter_donor_id",
+        "program_id",
+    ],
+    custom_fields=[
+        ("radiation_therapy_modality", Optional[RadiationTherapyModalityEnum], None),
+        ("radiation_therapy_type", Optional[TherapyTypeEnum], None),
+        ("anatomical_site_irradiated", Optional[RadiationAnatomicalSiteEnum], None),
+    ],
+)
+
+BaseImmunotherapySchema = create_schema(
+    Immunotherapy,
+    name="BaseImmunotherapySchema",
+    exclude=[
+        "uuid",
+        "donor_uuid",
+        "treatment_uuid",
+        "submitter_donor_id",
+        "program_id",
+    ],
+    custom_fields=[
+        ("immunotherapy_type", Optional[ImmunotherapyTypeEnum], None),
+        ("drug_reference_database", Optional[DrugReferenceDbEnum], None),
+        ("immunotherapy_drug_dose_units", Optional[DosageUnitsEnum], None),
+    ],
+)
+
+BaseSurgerySchema = create_schema(
+    Surgery,
+    name="BaseSurgerySchema",
+    exclude=[
+        "uuid",
+        "donor_uuid",
+        "treatment_uuid",
+        "submitter_donor_id",
+        "program_id",
+    ],
+    custom_fields=[
+        ("surgery_type", Optional[SurgeryTypeEnum], None),
+        (
+            "surgery_site",
+            Optional[str],
+            Field(None, pattern=TOPOGRAPHY_REGEX_PATTERNS, max_length=255),
+        ),
+        ("surgery_location", Optional[SurgeryLocationEnum], None),
+        ("tumour_focality", Optional[TumourFocalityEnum], None),
+        ("residual_tumour_classification", Optional[TumourClassificationEnum], None),
+        ("margin_types_involved", Optional[List[MarginTypesEnum]], None),
+        ("margin_types_not_involved", Optional[List[MarginTypesEnum]], None),
+        ("margin_types_not_assessed", Optional[List[MarginTypesEnum]], None),
+        ("lymphovascular_invasion", Optional[LymphovascularInvasionEnum], None),
+        ("perineural_invasion", Optional[PerineuralInvasionEnum], None),
+    ],
+)
+
+BaseFollowUpSchema = create_schema(
+    FollowUp,
+    name="BaseFollowUpSchema",
+    exclude=["uuid", "donor_uuid", "submitter_donor_id", "program_id"],
+    custom_fields=[
+        (
+            "submitter_follow_up_id",
+            str,
+            Field(pattern=ID_REGEX_PATTERNS, max_length=64),
+        ),
+        ("disease_status_at_followup", Optional[DiseaseStatusFollowupEnum], None),
+        ("relapse_type", Optional[RelapseTypeEnum], None),
+        (
+            "date_of_followup",
+            Optional[str],
+            Field(None, pattern=DATE_REGEX_PATTERNS, max_length=32),
+        ),
+        (
+            "date_of_relapse",
+            Optional[str],
+            Field(None, pattern=DATE_REGEX_PATTERNS, max_length=32),
+        ),
+        (
+            "method_of_progression_status",
+            Optional[List[ProgressionStatusMethodEnum]],
+            None,
+        ),
+        ("anatomic_site_progression_or_recurrence", Optional[List[str]], None),
+        ("recurrence_tumour_staging_system", Optional[TumourStagingSystemEnum], None),
+        ("recurrence_t_category", Optional[TCategoryEnum], None),
+        ("recurrence_n_category", Optional[NCategoryEnum], None),
+        ("recurrence_m_category", Optional[MCategoryEnum], None),
+        ("recurrence_stage_group", Optional[StageGroupEnum], None),
+    ],
+)
+
+BaseBiomarkerSchema = create_schema(
+    Biomarker,
+    name="BaseBiomarkerSchema",
+    exclude=["uuid", "donor_uuid", "submitter_donor_id", "program_id"],
+    custom_fields=[
+        ("er_status", Optional[ErPrHpvStatusEnum], None),
+        ("pr_status", Optional[ErPrHpvStatusEnum], None),
+        ("her2_ihc_status", Optional[Her2StatusEnum], None),
+        ("her2_ish_status", Optional[Her2StatusEnum], None),
+        ("hpv_ihc_status", Optional[ErPrHpvStatusEnum], None),
+        ("hpv_pcr_status", Optional[ErPrHpvStatusEnum], None),
+        ("hpv_strain", Optional[List[HpvStrainEnum]], None),
+    ],
+)
+
+BaseComorbiditySchema = create_schema(
+    Comorbidity,
+    name="BaseComorbiditySchema",
+    exclude=["uuid", "donor_uuid", "submitter_donor_id", "program_id"],
+    custom_fields=[
+        ("prior_malignancy", Optional[uBooleanEnum], None),
+        ("laterality_of_prior_malignancy", Optional[MalignancyLateralityEnum], None),
+        (
+            "comorbidity_type_code",
+            Optional[str],
+            Field(None, pattern=COMORBIDITY_REGEX_PATTERNS, max_length=64),
+        ),
+        ("comorbidity_treatment_status", Optional[uBooleanEnum], None),
+        ("comorbidity_treatment", Optional[str], Field(None, max_length=255)),
+    ],
+)
+
+BaseExposureSchema = create_schema(
+    Exposure,
+    name="BaseExposureSchema",
+    exclude=["uuid", "donor_uuid", "submitter_donor_id", "program_id"],
+    custom_fields=[
+        ("tobacco_smoking_status", Optional[SmokingStatusEnum], None),
+        ("tobacco_type", Optional[List[TobaccoTypeEnum]], None),
     ],
 )
