ab6085f0d11151b2072b2a1e95dd055ade71bb01
Son Chau <soncqb@gmail.com>
2024-01-17 18:39:44 -0800

    Update model.py
    remove inaccurate field alias

--- chord_metadata_service/mohpackets/schemas/model.py
+++ chord_metadata_service/mohpackets/schemas/model.py
@@ -284,82 +284,73 @@ class DonorModelSchema(BaseDonorSchema):
 
 class PrimaryDiagnosisModelSchema(BasePrimaryDiagnosisSchema):
     program_id: str = Field(..., alias="program_id_id")
-    submitter_donor_id: str = Field(..., alias="submitter_donor_id_id")
+    submitter_donor_id: str
 
 
 class SpecimenModelSchema(BaseSpecimenSchema):
     program_id: str = Field(..., alias="program_id_id")
-    submitter_donor_id: str = Field(..., alias="submitter_donor_id_id")
-    submitter_primary_diagnosis_id: str = Field(
-        ..., alias="submitter_primary_diagnosis_id_id"
-    )
+    submitter_donor_id: str
+    submitter_primary_diagnosis_id: str
 
 
 class SampleRegistrationModelSchema(BaseSampleRegistrationSchema):
     program_id: str = Field(..., alias="program_id_id")
-    submitter_donor_id: str = Field(..., alias="submitter_donor_id_id")
-    submitter_specimen_id: str = Field(..., alias="submitter_specimen_id_id")
+    submitter_donor_id: str
+    submitter_specimen_id: str
 
 
 class TreatmentModelSchema(BaseTreatmentSchema):
     program_id: str = Field(..., alias="program_id_id")
-    submitter_donor_id: str = Field(..., alias="submitter_donor_id_id")
-    submitter_primary_diagnosis_id: str = Field(
-        ..., alias="submitter_primary_diagnosis_id_id"
-    )
+    submitter_donor_id: str
+    submitter_primary_diagnosis_id: str
 
 
 class SurgeryModelSchema(BaseSurgerySchema):
     program_id: str = Field(..., alias="program_id_id")
-    submitter_donor_id: str = Field(..., alias="submitter_donor_id_id")
-    submitter_treatment_id: str = Field(..., alias="submitter_treatment_id_id")
+    submitter_donor_id: str
+    submitter_treatment_id: str
 
 
 class ImmunotherapyModelSchema(BaseImmunotherapySchema):
     program_id: str = Field(..., alias="program_id_id")
-    submitter_donor_id: str = Field(..., alias="submitter_donor_id_id")
-    submitter_treatment_id: str = Field(..., alias="submitter_treatment_id_id")
+    submitter_donor_id: str
+    submitter_treatment_id: str
 
 
 class RadiationModelSchema(BaseRadiationSchema):
     program_id: str = Field(..., alias="program_id_id")
-    submitter_donor_id: str = Field(..., alias="submitter_donor_id_id")
-    submitter_treatment_id: str = Field(..., alias="submitter_treatment_id_id")
+    submitter_donor_id: str
+    submitter_treatment_id: str
 
 
 class HormoneTherapyModelSchema(BaseHormoneTherapySchema):
     program_id: str = Field(..., alias="program_id_id")
-    submitter_donor_id: str = Field(..., alias="submitter_donor_id_id")
-    submitter_treatment_id: str = Field(..., alias="submitter_treatment_id_id")
+    submitter_donor_id: str
+    submitter_treatment_id: str
 
 
 class ChemotherapyModelSchema(BaseChemotherapySchema):
     program_id: str = Field(..., alias="program_id_id")
-    submitter_donor_id: str = Field(..., alias="submitter_donor_id_id")
-    submitter_treatment_id: str = Field(..., alias="submitter_treatment_id_id")
+    submitter_donor_id: str
+    submitter_treatment_id: str
 
 
 class FollowUpModelSchema(BaseFollowUpSchema):
     program_id: str = Field(..., alias="program_id_id")
-    submitter_donor_id: str = Field(..., alias="submitter_donor_id_id")
-    submitter_primary_diagnosis_id: Optional[str] = Field(
-        ..., alias="submitter_primary_diagnosis_id_id"
-    )
-    submitter_treatment_id: Optional[str] = Field(
-        ..., alias="submitter_treatment_id_id"
-    )
-
+    submitter_donor_id: str
+    submitter_primary_diagnosis_id: Optional[str] = None
+    submitter_treatment_id: Optional[str] = None
 
 class BiomarkerModelSchema(BaseBiomarkerSchema):
     program_id: str = Field(..., alias="program_id_id")
-    submitter_donor_id: str = Field(..., alias="submitter_donor_id_id")
+    submitter_donor_id: str
 
 
 class ExposureModelSchema(BaseExposureSchema):
     program_id: str = Field(..., alias="program_id_id")
-    submitter_donor_id: str = Field(..., alias="submitter_donor_id_id")
+    submitter_donor_id: str
 
 
 class ComorbidityModelSchema(BaseComorbiditySchema):
     program_id: str = Field(..., alias="program_id_id")
-    submitter_donor_id: str = Field(..., alias="submitter_donor_id_id")
+    submitter_donor_id: str
