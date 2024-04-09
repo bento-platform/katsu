9652c941027378f62366fbfebed30c85876642e5
David Lougheed <david.lougheed@gmail.com>
2024-04-09 15:45:24 -0400

    test(discovery): add test for get_string_options hard-coded list

--- chord_metadata_service/discovery/tests/test_fields.py
+++ chord_metadata_service/discovery/tests/test_fields.py
@@ -1,17 +1,17 @@
 from django.db.models.base import ModelBase
-from django.test import override_settings
+from django.test import TransactionTestCase, override_settings
 from rest_framework.test import APITestCase
-from unittest import TestCase
 
 from .constants import CONFIG_PUBLIC_TEST
 from ..fields import (
     get_model_and_field,
+    get_field_options,
     get_date_stats,
     get_month_date_range
 )
 
 
-class TestModelField(TestCase):
+class TestModelField(TransactionTestCase):
 
     def test_get_model_field_basic(self):
         model, field = get_model_and_field("individual/age_numeric")
@@ -30,11 +30,27 @@ class TestModelField(TestCase):
         self.assertRaises(NotImplementedError, get_model_and_field, "junk/age_numeric")
 
 
+class TestGetFieldOptions(TransactionTestCase):
+
+    async def test_get_string_options(self):
+        self.assertListEqual(await get_field_options({
+            "datatype": "string",
+            "mapping": "individual/extra_properties/some_prop",
+            "title": "Some Prop",
+            "description": "Some property",
+            "config": {
+                "enum": ["a", "b"],
+            },
+        }, low_counts_censored=False), ["a", "b"])
+
+
 class TestDateStatsExcept(APITestCase):
 
     @override_settings(CONFIG_PUBLIC=CONFIG_PUBLIC_TEST)
     async def test_wrong_bin_config(self):
         fp = {
+            "title": "Date of Consent",
+            "description": "Date of consent for study",
             "mapping": "individual/extra_properties/date_of_consent",
             "datatype": "date",
             "config": {
@@ -51,6 +67,8 @@ class TestDateStatsExcept(APITestCase):
     @override_settings(CONFIG_PUBLIC=CONFIG_PUBLIC_TEST)
     async def test_wrong_field_config(self):
         fp = {
+            "title": "Date of Consent",
+            "description": "Date of consent for study",
             "mapping": "individual/date_of_consent",
             "datatype": "date",
             "config": {
