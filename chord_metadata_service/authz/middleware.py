44f4c9a8e5f6e7f759ffee040867a0615707a02e
David Lougheed <david.lougheed@gmail.com>
2024-08-07 12:37:51 -0400

    chore(authz): allow more authz-enabled endpoints

--- chord_metadata_service/authz/middleware.py
+++ chord_metadata_service/authz/middleware.py
@@ -13,17 +13,25 @@ __all__ = [
 pattern_get = re.compile(r"^GET$")
 
 include_pattern_public = (
-    re.compile(r"^(GET|POST)$"),
-    re.compile(r"^/api/(projects|public|public_overview|public_search_fields|public_dataset|public_rules)$"),
+    re.compile(r"^(GET|POST|PUT|DELETE)$"),
+    re.compile(r"^/api/(projects|datasets|public|public_overview|public_search_fields|public_dataset|public_rules)$"),
 )
 include_pattern_workflows = (pattern_get, re.compile(r"^(/workflows$|/workflows/)"))
 include_pattern_si = (pattern_get, re.compile(r"^/service-info"))
+include_pattern_schemas = (pattern_get, re.compile(r"^/schemas/.+$"))
+include_pattern_schema_types = (pattern_get, re.compile(r"^/extra_properties_schema_types$"))
 
 authz_middleware = DjangoAuthMiddleware(
     bento_authz_service_url=settings.BENTO_AUTHZ_SERVICE_URL,
     debug_mode=settings.DEBUG,
     enabled=settings.BENTO_AUTHZ_ENABLED,
-    include_request_patterns=(include_pattern_public, include_pattern_workflows, include_pattern_si),
+    include_request_patterns=(
+        include_pattern_public,
+        include_pattern_workflows,
+        include_pattern_si,
+        include_pattern_schemas,
+        include_pattern_schema_types,
+    ),
     logger=logger,
 )
 
