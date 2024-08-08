003a61cedf8bdbaf65d6d22ea95a0a3898a6ef91
David Lougheed <david.lougheed@gmail.com>
2024-08-08 12:05:28 -0400

    lint(authz): rm unused helper functions (might be reverted for future use)

--- chord_metadata_service/authz/helpers.py
+++ chord_metadata_service/authz/helpers.py
@@ -6,21 +6,17 @@ from bento_lib.auth.permissions import (
     P_QUERY_PROJECT_LEVEL_COUNTS,
     P_QUERY_DATA,
 )
-from bento_lib.auth.middleware.django import DjangoAuthMiddleware
-from bento_lib.auth.resources import RESOURCE_EVERYTHING, build_resource
+from bento_lib.auth.resources import RESOURCE_EVERYTHING
 from django.http import HttpRequest
 from rest_framework.request import Request
 
 from .middleware import authz_middleware
-from .types import Bools, DataTypeDiscoveryPermissions
+from .types import DataTypeDiscoveryPermissions
 
 
 __all__ = [
     "get_bool_permission",
     "get_counts_permission",
-    "can_see_bool",
-    "can_see_counts",
-    "has_query_data_permission_for_data_types",
     "get_data_type_query_permissions",
 ]
 
@@ -31,44 +27,6 @@ def get_bool_permission(dataset_level: bool) -> Permission:
 
 def get_counts_permission(dataset_level: bool) -> Permission:
     return P_QUERY_DATASET_LEVEL_COUNTS if dataset_level else P_QUERY_PROJECT_LEVEL_COUNTS
-
-
-async def _can_see_censored(
-    request: HttpRequest,
-    authz: DjangoAuthMiddleware,
-    resources: list[dict],
-    censored_permission: Permission,
-) -> Bools:
-    # P_QUERY_DATA as a fallback for any censored data query
-    assert censored_permission in P_QUERY_DATA.gives  # must be a given permission from query:data
-    return tuple(map(any, (
-        await authz.async_evaluate(request, resources, (censored_permission, P_QUERY_DATA))
-    )))
-
-
-async def can_see_bool(request: HttpRequest, resources: list[dict], dataset_level: bool) -> Bools:
-    return await _can_see_censored(request, authz_middleware, resources, get_bool_permission(dataset_level))
-
-
-async def can_see_counts(request: HttpRequest, resources: list[dict], dataset_level: bool) -> Bools:
-    return await _can_see_censored(request, authz_middleware, resources, get_counts_permission(dataset_level))
-
-
-async def can_query_data(request: Request | HttpRequest, resource: dict) -> bool:
-    return await authz_middleware.async_evaluate_one(request, resource, P_QUERY_DATA)
-
-
-async def has_query_data_permission_for_data_types(
-    request: Request | HttpRequest, project_id: str | None, dataset_id: str | None, data_types: list[str]
-) -> Bools:
-    has_permission: bool = await can_query_data(request, build_resource(project_id, dataset_id))
-
-    if has_permission:
-        return tuple([True] * len(data_types))
-
-    resources = [build_resource(project_id, dataset_id, dt_id) for dt_id in data_types]
-    res = await authz_middleware.async_evaluate(request, resources, (P_QUERY_DATA,))
-    return tuple(ps[0] for ps in res)
 
 
 async def get_data_type_query_permissions(
