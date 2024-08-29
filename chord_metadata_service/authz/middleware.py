54482fbb3e9112e2a77e78b92988b2dd8ecec8db
David Lougheed <david.lougheed@gmail.com>
2024-08-29 12:58:24 -0400

    chore(authz): comment note about middlware request patterns

--- chord_metadata_service/authz/middleware.py
+++ chord_metadata_service/authz/middleware.py
@@ -12,6 +12,9 @@ __all__ = [
 
 pattern_get = re.compile(r"^GET$")
 
+# --- List of patterns to apply authz middleware to --------------------------------------------------------------------
+#  - Note: as we gradually roll out authz across Katus, this list will expand. Anything not covered here is assumed to
+#          be protected by the gateway.
 include_pattern_public = (
     re.compile(r"^(GET|POST|PUT|DELETE)$"),
     re.compile(r"^/api/(projects|datasets|public|public_overview|public_search_fields|public_dataset|public_rules)$"),
@@ -20,6 +23,7 @@ include_pattern_workflows = (pattern_get, re.compile(r"^(/workflows$|/workflows/
 include_pattern_si = (pattern_get, re.compile(r"^/service-info"))
 include_pattern_schemas = (pattern_get, re.compile(r"^/schemas/.+$"))
 include_pattern_schema_types = (pattern_get, re.compile(r"^/extra_properties_schema_types$"))
+# ----------------------------------------------------------------------------------------------------------------------
 
 authz_middleware = DjangoAuthMiddleware(
     bento_authz_service_url=settings.BENTO_AUTHZ_SERVICE_URL,
