3967cafb642bb9024cb6e10b0e12ec92d9984c25
Julian <ornatus30@gmail.com>
2024-10-29 11:32:32 -0400

    lint

--- chord_metadata_service/experiments/schemas.py
+++ chord_metadata_service/experiments/schemas.py
@@ -86,8 +86,8 @@ EXPERIMENT_RESULT_SCHEMA = tag_ids_and_describe({
         "file_format": {
             "type": "string",
             "enum": ["SAM", "BAM", "CRAM", "VCF", "BCF", "MAF", "GVCF", "BigWig", "BigBed", "FASTA", "FASTQ", "TAB",
-                     "SRA", "SRF", "SFF", "GFF", "PDF", "CSV", "TSV", "JPEG", "PNG", "GIF", "HTML", "MARKDOWN", "MP3", "M4A",
-                     "MP4", "DOCX", "XLS", "XLSX", "UNKNOWN", "OTHER"]
+                     "SRA", "SRF", "SFF", "GFF", "PDF", "CSV", "TSV", "JPEG", "PNG", "GIF", "HTML", "MARKDOWN",
+                     "MP3", "M4A", "MP4", "DOCX", "XLS", "XLSX", "UNKNOWN", "OTHER"]
         },
         "data_output_type": {
             "type": "string",
