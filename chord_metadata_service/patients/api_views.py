084fccf2d8b5265ca1b2f278208f9ab1f63dee8f
David Lougheed <david.lougheed@gmail.com>
2024-12-20 15:09:11 -0500

    fix: individuals scoping

--- chord_metadata_service/patients/api_views.py
+++ chord_metadata_service/patients/api_views.py
@@ -98,12 +98,6 @@ class IndividualViewSet(BentoAuthzModelViewSet):
     @async_to_sync
     async def get_queryset(self):
         scope = await get_request_discovery_scope(self.request)
-
-        if "project" in self.request.query_params:
-            del self.request.query_params["project"]
-        if "dataset" in self.request.query_params:
-            del self.request.query_params["dataset"]
-
         return (
             Individual.get_model_scoped_queryset(scope)
             .prefetch_related(
