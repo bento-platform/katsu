6d6ad60693ca9176a639d2fda02af65c92ebaf38
David Lougheed <david.lougheed@gmail.com>
2025-01-07 16:24:16 -0500

    chore(phenopackets): rm unused scope filters for MetaData this model doesn't have an associated view set anymore.

--- chord_metadata_service/phenopackets/models.py
+++ chord_metadata_service/phenopackets/models.py
@@ -41,22 +41,7 @@ class MetaData(BaseTimeStamp):
     """
     Class to store structured definitions of the resources
     and ontologies used within the phenopacket
-
-    FHIR: Metadata
     """
-
-    @staticmethod
-    def get_scope_filters() -> ModelScopeFilters:
-        return {
-            "project": {
-                "filter": "phenopacket_set__dataset__project__identifier",
-                "prefetch_related": ("phenopacket_set__dataset__project",),
-            },
-            "dataset": {
-                "filter": "phenopacket_set__dataset__identifier",
-                "prefetch_related": ("phenopacket_set__dataset",),
-            },
-        }
 
     created_by = models.CharField(max_length=200, blank=True, null=True, default=None,
                                   help_text=rec_help(d.META_DATA, "created_by"))
