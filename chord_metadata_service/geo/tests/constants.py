d0454821f64a1c39b388ce87751699cda5c42d34
David Lougheed <david.lougheed@gmail.com>
2025-01-30 14:48:33 -0500

    oops

--- chord_metadata_service/geo/tests/constants.py
+++ chord_metadata_service/geo/tests/constants.py
@@ -2,18 +2,23 @@ from django.contrib.gis.geos import Point
 from django.test import TestCase
 from ..models import GeoLocation
 
-__all__ = ["KINGSTON_GEOM_JSON", "GeoLocationTestCase"]
+__all__ = ["KINGSTON_GEOM_JSON", "GEO_LOCATION_1", "GeoLocationTestCase"]
 
 KINGSTON_GEOM_JSON = {
     "type": "Point",
     "coordinates": [44.2380626, -76.512335],
 }
 
+GEO_LOCATION_1 = {
+    "point": Point(44.2380626, -76.512335),
+    "label": "Kingston",
+}
+
 
 class GeoLocationTestCase(TestCase):
 
     def setUp(self):
-        self.loc_1 = GeoLocation.objects.create(point=Point(44.2380626, -76.512335), label="Kingston")
+        self.loc_1 = GeoLocation.objects.create(**GEO_LOCATION_1)
         self.loc_2 = GeoLocation.objects.create(
             point=Point(44.2380626, -76.512335),
             label="Kingston",
