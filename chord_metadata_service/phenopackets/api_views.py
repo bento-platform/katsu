bb115d36b583efa1848e6ea78c153f3622b5a391
David Lougheed <david.lougheed@gmail.com>
2025-04-28 15:50:47 -0400

    fix(phenopackets): mixed-up select/prefetch

--- chord_metadata_service/phenopackets/api_views.py
+++ chord_metadata_service/phenopackets/api_views.py
@@ -129,20 +129,20 @@ class BiosampleBatchViewSet(BentoAuthzScopedModelGenericListViewSet):
 
 PHENOPACKET_PREFETCH = (
     *(f"biosamples__{p}" for p in BIOSAMPLE_PREFETCH),
+    *(f"biosamples__{p}" for p in BIOSAMPLE_SELECT_REL),
     "meta_data__resources",
     "diseases",
     "phenotypic_features",
     "interpretations",
-    "interpretations__diagnosis__genomic_interpretations",
-)
-
-PHENOPACKET_SELECT_REL = (
-    *(f"biosamples__{p}" for p in BIOSAMPLE_SELECT_REL),
     "interpretations__diagnosis",
+    "interpretations__diagnosis__genomic_interpretations",
     "interpretations__diagnosis__genomic_interpretations__biosample",
     "interpretations__diagnosis__genomic_interpretations__subject",
     "interpretations__diagnosis__genomic_interpretations__gene_descriptor",
     "interpretations__diagnosis__genomic_interpretations__variant_interpretation__variation_descriptor",
+)
+
+PHENOPACKET_SELECT_REL = (
     "subject",
     "meta_data",
 )
