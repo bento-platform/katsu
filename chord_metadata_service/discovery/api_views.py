d77908e2867bbe644332ef79304aa18fb6575eb9
David Lougheed <david.lougheed@gmail.com>
2025-05-07 10:14:39 -0400

    commenting + variable renaming

--- chord_metadata_service/discovery/api_views.py
+++ chord_metadata_service/discovery/api_views.py
@@ -147,9 +147,10 @@ async def public_overview(request: DrfRequest):
     counts: dict[PublicModelName, int] = dict(
         await asyncio.gather(*map(_counts_for_scoped_model_name, PUBLIC_MODEL_NAMES_TO_MODEL.items())))
 
-    counts_res: dict[str, int | bool] = {}
+    # for each 'public model', we generate either a count (0/count-if-above-threshold) or a boolean (count > threshold)
+    count_or_bools_res: dict[str, int | bool] = {}
 
-    # Set counts to 0 if they're under the count threshold and the threshold is positive.
+    # Set counts to 0 (or bool to False) if they're under the count threshold and the threshold is positive.
     for public_model_name in counts:
         dt = PUBLIC_MODEL_NAMES_TO_DATA_TYPE[public_model_name]
         model_permissions = dt_permissions[dt]
@@ -172,12 +173,13 @@ async def public_overview(request: DrfRequest):
             # if we only have boolean permissions, store a Boolean "count" (yes or no to above-threshold count) if we
             # didn't get censored down to 0 above.
             # - hacky pluralize - works for current public model names
-            counts_res[f"{public_model_name}s"] = model_count if model_permissions["counts"] else (model_count > 0)
+            count_or_bools_res[f"{public_model_name}s"] = model_count if model_permissions["counts"] else (model_count > 0)
 
     response = {
         "layout": [cd.model_dump(mode="json") for cd in discovery.overview],
         "fields": {},
-        "counts": counts_res,  # permissions-dependent: dictionary of {entity plural: counts or bool if above threshold}
+        # permissions-dependent: dictionary of {entity plural: counts or True if above threshold, 0/False otherwise}:
+        "counts": count_or_bools_res,
     }
 
     # Parse the public config to gather data for each field defined in the overview
