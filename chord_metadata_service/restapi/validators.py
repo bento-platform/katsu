8fdffd7da757dcc3d30ce0f8cffed288de10767c
Victor Rocheleau <victor.rocheleau@mcgill.ca>
2025-07-31 16:32:51 +0000

    lint and typo

--- chord_metadata_service/restapi/validators.py
+++ chord_metadata_service/restapi/validators.py
@@ -18,18 +18,19 @@ __all__ = [
     "base_extra_properties_validator",
 ]
 
-###### Migrations shenanigans ######
+# Migrations shenanigans:
 # Passing full schemas to JsonSchemaValidator can cause huge migrations as soon as a couple of schemas change.
 # These migrations include JsonSchemaValidator constructors with the full schema as a param.
 # To get around this, David B had the idea of using a schema resolving mechanism instead, where we only pass a
 # string ref of the schema to the constructor, this is a small POC of the idea.
 SCHEMA_REFS: dict[str, dict] = {
     "AGE_OR_AGE_RANGE": AGE_OR_AGE_RANGE,
-    "ONTOLOGY_CLASS": AGE_OR_AGE_RANGE,
+    "ONTOLOGY_CLASS": ONTOLOGY_CLASS,
     "ONTOLOGY_CLASS_LIST": ONTOLOGY_CLASS_LIST,
     "KEY_VALUE_OBJECT": KEY_VALUE_OBJECT,
     "EXTRA_PROPERTIES_SCHEMA": EXTRA_PROPERTIES_SCHEMA,
 }
+
 
 class JsonSchemaValidator:
     """ Custom class based validator to validate against Json schema for JSONField """
@@ -37,12 +38,10 @@ class JsonSchemaValidator:
     def __init__(self, schema=None, schema_ref=None, formats=None, registry=None):
         """
         Validators can be constructed from a full `schema` object, or a `schema_ref` string.
-        
-
         """
         if (not schema and not schema_ref) or (schema and schema_ref):
             raise ValueError("Must provide a schema OR a schema_ref argument.")
-        
+
         # Retrieve schema data from dict when schema_ref is passed
         if schema_ref:
             if schema_ref not in SCHEMA_REFS:
@@ -86,11 +85,15 @@ class JsonSchemaValidator:
             {"formats": self.formats}
         )
 
+
+# New way of constructing JsonSchemaValidators using schema refs
 age_or_age_range_validator = JsonSchemaValidator(schema_ref="AGE_OR_AGE_RANGE")
 ontology_validator = JsonSchemaValidator(schema_ref="ONTOLOGY_CLASS")
 ontology_list_validator = JsonSchemaValidator(schema_ref="ONTOLOGY_CLASS_LIST")
 key_value_validator = JsonSchemaValidator(schema_ref="KEY_VALUE_OBJECT")
 base_extra_properties_validator = JsonSchemaValidator(schema_ref="EXTRA_PROPERTIES_SCHEMA")
+
+# TODO: remove, left as an example of schema ref replacement
 # age_or_age_range_validator = JsonSchemaValidator(AGE_OR_AGE_RANGE)
 # ontology_validator = JsonSchemaValidator(ONTOLOGY_CLASS)
 # ontology_list_validator = JsonSchemaValidator(ONTOLOGY_CLASS_LIST)
