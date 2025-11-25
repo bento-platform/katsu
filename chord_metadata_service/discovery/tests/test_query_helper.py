75c02bc0185e2c750ca7dffb4d59f95b86953837
David Lougheed <david.lougheed@gmail.com>
2025-11-25 11:33:16 -0500

    test: add wip tests for QueryHelper directly

---
+++ chord_metadata_service/discovery/tests/test_query_helper.py
@@ -0,0 +1,44 @@
+from django.test import TransactionTestCase, override_settings
+from structlog.stdlib import get_logger
+
+from chord_metadata_service.authz.types import DataPermissions
+from chord_metadata_service.chord.data_types import DATA_TYPE_PHENOPACKET, DATA_TYPE_EXPERIMENT
+from chord_metadata_service.discovery.api_views import QueryHelper
+from chord_metadata_service.discovery.pydantic_models import DiscoveryQuery
+from chord_metadata_service.discovery.scope import ValidatedDiscoveryScope
+
+from .constants import DISCOVERY_CONFIG_TEST
+
+
+class QueryHelperTest(TransactionTestCase):
+
+    def setUp(self):
+        self.fts_query = DiscoveryQuery(fts="cancer", filters={})
+        self.filters_query = DiscoveryQuery(fts=None, filters={"smoking": "Smoker"})
+        self.scope = ValidatedDiscoveryScope(project=None, dataset=None)
+        self.dt_permissions = {
+            DATA_TYPE_PHENOPACKET: DataPermissions(bool_=True, counts=True, data=False),
+            DATA_TYPE_EXPERIMENT: DataPermissions(bool_=True, counts=True, data=False),
+        }
+        self.logger = get_logger("query-helper-test")
+
+    def test_property_methods(self):
+        qh = QueryHelper(self.fts_query, self.scope, self.dt_permissions, self.logger)
+        self.assertEqual(qh.scope, self.scope)
+        self.assertEqual(qh.dt_permissions, self.dt_permissions)
+
+    @override_settings(CONFIG_PUBLIC=DISCOVERY_CONFIG_TEST)
+    async def test_empty_querying_fts(self):
+        # nothing here
+        qh = QueryHelper(self.fts_query, self.scope, self.dt_permissions, self.logger)
+        qs, qe = await qh.get_query_queryset_and_queried_entities("phenopacket")
+        # TODO: queried entities for FTS?
+        self.assertEqual(qe, frozenset({"TODO"}))
+
+    @override_settings(CONFIG_PUBLIC=DISCOVERY_CONFIG_TEST)
+    async def test_empty_querying_filters(self):
+        # nothing here
+        qh = QueryHelper(self.filters_query, self.scope, self.dt_permissions, self.logger)
+        qs, qe = await qh.get_query_queryset_and_queried_entities("phenopacket")
+        print(qs, qe)
+        self.assertEqual(qe, frozenset({"individual"}))
