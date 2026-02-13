074f211d278a15228bf213afee0513432612daef
David Lougheed <david.lougheed@gmail.com>
2026-02-13 15:06:00 -0500

    lint

--- chord_metadata_service/restapi/urls.py
+++ chord_metadata_service/restapi/urls.py
@@ -1,6 +1,4 @@
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
 from rest_framework import routers
 
 from chord_metadata_service.chord import api_views as chord_views
