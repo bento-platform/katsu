c3bd277775220184fdc4907e8fdd445a02922f7f
SanjeevLakhwani <lakhwani.sanjeev691@gmail.com>
2026-03-11 17:03:34 -0400

    refact: Simplified migration

--- chord_metadata_service/metadata/settings.py
+++ chord_metadata_service/metadata/settings.py
@@ -207,10 +207,16 @@ LOGGING = {
         'daphne.server': _logging_propagate_to_root,
         'django': _logging_propagate_to_root,
         # Log SQL queries to diagnose N+1 / other inefficient query issues when we're in debug mode:
-        **({'django.db.backends': {'level': 'DEBUG'}} if DEBUG else {}),
+        # **({'django.db.backends': {'level': 'DEBUG'}} if DEBUG else {}),
+        'django.db.backends': {
+            'level': 'WARNING',
+            'propagate': False,
+        },
         'django.request': _logging_propagate_to_root,
         'django.channels.server': {'level': 'WARNING'},  # silence in favour of custom access middleware
-        'katsu': _logging_propagate_to_root
+        'katsu': {
+            "level": "INFO"
+        }
     },
 }
 
