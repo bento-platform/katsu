cf06e3bbd3dbca78d2c20c58e6e1a1866eb74ea2
Sanjeev Lakhwani <lakhwani.sanjeev691@gmail.com>
2026-05-26 12:05:15 -0400

    fix/test: fix liceses typo in distributions_to_jsonld; 100% coverage for restapi/jsonld_utils

--- chord_metadata_service/restapi/tests/test_jsonld.py
+++ chord_metadata_service/restapi/tests/test_jsonld.py
@@ -1,1 +1,39 @@
-# JSON-LD and RDF renderers to come for DatasetV2
+from django.test import SimpleTestCase
+
+from chord_metadata_service.restapi import jsonld_utils as jlu
+
+
+class JsonLdUtilsTest(SimpleTestCase):
+    def test_dataset_to_jsonld_empty_dataset(self):
+        result = jlu.dataset_to_jsonld({})
+        self.assertIn("@context", result)
+        self.assertEqual(result["@type"], "Dataset")
+
+    def test_dataset_to_jsonld_full(self):
+        dataset = {
+            "dates": [{"type": {}}],
+            "stored_in": {},
+            "creators": [{"name": "Org"}, {}],
+            "types": [{"information": {}}, {}],
+            "licenses": [{}],
+            "extra_properties": [{"values": [{}]}, {"values": []}],
+            "alternate_identifiers": [{}],
+            "related_identifiers": [{}],
+            "spatial_coverage": [{
+                "identifier": {},
+                "alternate_identifiers": [{}],
+                "related_identifiers": [{}],
+            }],
+            "distributions": [
+                {"stored_in": {}, "dates": [{"type": {}}], "licenses": [{}]},
+                {},
+            ],
+            "dimensions": [{}],
+            "primary_publications": [
+                {"identifier": {}, "authors": [{"name": "Author"}, {}], "dates": [{"type": {}}]},
+                {},
+            ],
+        }
+        result = jlu.dataset_to_jsonld(dataset)
+        self.assertIn("@context", result)
+        self.assertEqual(result["@type"], "Dataset")
